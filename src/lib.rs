a6bed280673d69a890bbaa2f725652b401277578
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-company.com>
2017-03-24 17:37:59 +0300

    * Splitted API parts in different structures

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,49 @@ mod services;
 
 use commands::{Command, CommandExecutor};
 use std::error;
+use std::sync::Arc;
 
-pub struct SovrinClient {
-    command_executor: CommandExecutor
+struct AnoncredsAPI {
+    command_executor: Arc<CommandExecutor>,
 }
 
-impl SovrinClient {
-    pub fn new() -> SovrinClient {
-        SovrinClient {
-            command_executor: CommandExecutor::new()
+impl AnoncredsAPI {
+    pub fn new(command_executor: Arc<CommandExecutor>) -> AnoncredsAPI {
+        AnoncredsAPI { command_executor: command_executor }
+    }
+
+    fn dummy() {}
+}
+
+struct SovrinAPI {
+    command_executor: Arc<CommandExecutor>,
+}
+
+impl SovrinAPI {
+    pub fn new(command_executor: Arc<CommandExecutor>) -> SovrinAPI {
+        SovrinAPI {
+            command_executor: command_executor
         }
     }
 
     pub fn set_did(&self, did: String, cb: Box<Fn(Result<(), Box<error::Error>>) + Send>) {
         self.command_executor.send(Command::SetDidCommand(did, cb));
+    }
+}
+
+
+pub struct SovrinClient {
+    sovrin: SovrinAPI,
+    anoncreds: AnoncredsAPI
+}
+
+impl SovrinClient {
+    pub fn new() -> SovrinClient {
+        let command_executor = Arc::new(CommandExecutor::new());
+        SovrinClient {
+            anoncreds: AnoncredsAPI::new(command_executor.clone()),
+            sovrin: SovrinAPI::new(command_executor.clone())
+        }
     }
 }
 
@@ -31,7 +60,7 @@ mod tests {
     #[test]
     fn sovrin_client_can_be_created() {
         let sovrin_client = SovrinClient::new();
-        assert!(true, "No crashes on SovrinClient::new");
+        assert! (true, "No crashes on SovrinClient::new");
     }
 
     #[test]
@@ -41,7 +70,7 @@ mod tests {
         }
 
         drop_test();
-        assert!(true, "No crashes on SovrinClient::drop");
+        assert! (true, "No crashes on SovrinClient::drop");
     }
 
     #[test]
@@ -56,11 +85,11 @@ mod tests {
         });
 
         let sovrin_client = SovrinClient::new();
-        sovrin_client.set_did("DID0".to_string(), cb);
+        sovrin_client.sovrin.set_did("DID0".to_string(), cb);
 
         match receiver.recv() {
             Ok(result) => {
-                assert_eq!("OK", result);
+                assert_eq! ("OK", result);
             }
             Err(err) => {
                 panic!("Error on result recv: {:?}", err);
