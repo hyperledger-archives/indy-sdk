45f3e3283abcd7f7d3c12a7dc4e8d6509f1146b0
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-03-27 15:03:23 +0300

    Added documentation

--- src/api/anoncreds.rs
+++ src/api/anoncreds.rs
@@ -12,27 +12,69 @@ impl AnoncredsAPI {
         AnoncredsAPI { command_executor: command_executor }
     }
 
+    /// Creates master secret key for prover.
+    ///
+    /// #Params
+    /// cb: Callback that takes command result as a parameter.
+    ///
+    /// #Returns
+    /// Master secret key as a String.
+    ///
+    /// #Errors
+    /// No method specific errors.
+    /// See `AnoncredsError` docs for common errors description.
     pub fn create_master_secret(cb: Box<Fn(Result<(String), AnoncredsError>) + Send>) {
-
+        unimplemented!();
     }
 
-    pub fn create_keys(schema: String,
-                       cb: Box<Fn(Result<((String, String)), AnoncredsError>) + Send>) {
-
+    /// Creates public/private keys pair for issuer.
+    ///
+    /// #Params
+    /// schema: claim definition schema as a byte array.
+    /// cb: Callback that takes command result as a parameter.
+    ///
+    /// #Returns
+    /// Tuple of
+    /// (Public primary key, Secret Key, Public non-revocation key, Secret non-revocation key)
+    /// as a String.
+    ///
+    /// #Errors
+    /// No method specific errors.
+    /// See `AnoncredsError` docs for common errors description.
+    pub fn create_key_pair(schema: &[&u8],
+                       cb: Box<Fn(
+                           Result<((String, String, String, String)), AnoncredsError>) + Send>) {
+        unimplemented!();
     }
+
 
     pub fn create_context_attribute(i_a: String, user_id: String,
                                     cb: Box<Fn(Result<(String), AnoncredsError>) + Send>) {
 
     }
 
-    pub fn issue_accumulator(schema: String, i_a: String, l: String,
-                             public_key_revocation: String,
+    /// Issues accumulator.
+    ///
+    /// #Params
+    /// schema: claim definition schema as a byte array.
+    /// accumulator_id: accumulator id as a byte array.
+    /// max_claims: maximum number of claims within accumulator as a byte array.
+    /// public_key_non_revocation: non-revocation public key as a byte array.
+    /// cb: Callback that takes command result as a parameter.
+    ///
+    /// #Returns
+    /// Tuple of (Accumulator, Tails, Accumulator public key, Accumulator secret key) as a String.
+    ///
+    /// #Errors
+    /// No method specific errors.
+    /// See `AnoncredsError` docs for common errors description.
+    pub fn issue_accumulator(schema: &[&u8], accumulator_id: &[&u8], max_claims: &[&u8],
+                             public_key_non_revocation: &[&u8],
                              cb: Box<Fn(
                                  Result<((String, String, String, String)),
                                      AnoncredsError>
                              ) + Send>) {
-
+        unimplemented!();
     }
 
     pub fn issue_claim(attributes: String, accumulator: String,i_a: String, i: String,
@@ -44,10 +86,25 @@ impl AnoncredsAPI {
 
     }
 
-    pub fn create_claim_request(master_secret: String, public_key: String,
-                                public_key_revocation: String, request_non_revocation: String,
+    /// Creates claim request.
+    ///
+    /// #Params
+    /// master_secret: prover master secret as a byte array.
+    /// public_key: issuer public_key as a byte array.
+    /// public_key_non_revocation: issuer non-revocation public key as a byte array.
+    /// request_non_revocation: whether to request non-revocation claim as a byte array.
+    /// cb: Callback that takes command result as a parameter.
+    ///
+    /// #Returns
+    /// Claim request as a String.
+    ///
+    /// #Errors
+    /// No method specific errors.
+    /// See `AnoncredsError` docs for common errors description.
+    pub fn create_claim_request(master_secret: &[&u8], public_key: &[&u8],
+                                public_key_non_revocation: &[&u8], request_non_revocation: &[&u8],
                                 cb: Box<Fn(Result<(String), AnoncredsError>) + Send>) {
-
+        unimplemented!();
     }
 
     pub fn create_proof(proof_input: String, nonce: String, claims: String,
