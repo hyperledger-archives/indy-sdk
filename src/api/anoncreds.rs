c40299ec9abcd9e701f8756273ce125091a7ab8d
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-company.com>
2017-03-30 19:18:55 +0300

    * Removed public Rust API at all
    * Updated C API

--- src/api/anoncreds.rs
+++ src/api/anoncreds.rs
@@ -1,222 +1,79 @@
-use commands::{Command, CommandExecutor};
-use std::error;
-use std::sync::Arc;
-use errors::anoncreds::AnoncredsError;
+extern crate libc;
 
-pub struct AnoncredsAPI {
-    command_executor: Arc<CommandExecutor>,
+use self::libc::{c_char, c_uchar};
+
+#[no_mangle]
+pub extern fn anoncreds_create_master_secret(client_id: i32, command_id: i32,
+                                             cb: extern fn(xcommand_id: i32, err: i32,
+                                                           master_secret: *const c_char)) {
+    unimplemented!();
 }
 
-impl AnoncredsAPI {
-    pub fn new(command_executor: Arc<CommandExecutor>) -> AnoncredsAPI {
-        AnoncredsAPI { command_executor: command_executor }
-    }
-
-    /// Creates master secret key for prover.
-    ///
-    /// #Params
-    /// cb: Callback that takes command result as a parameter.
-    ///
-    /// #Returns
-    /// Master secret key as a String.
-    ///
-    /// #Errors
-    /// No method specific errors.
-    /// See `AnoncredsError` docs for common errors description.
-    pub fn create_master_secret(cb: Box<Fn(Result<String, AnoncredsError>) + Send>) {
-        unimplemented!();
-    }
-
-    /// Creates public/private keys pair for issuer.
-    ///
-    /// #Params
-    /// schema: claim definition schema as a byte array.
-    /// cb: Callback that takes command result as a parameter.
-    ///
-    /// #Returns
-    /// Tuple of
-    /// (Public primary key, Secret Key, Public non-revocation key, Secret non-revocation key)
-    /// as a String.
-    ///
-    /// #Errors
-    /// No method specific errors.
-    /// See `AnoncredsError` docs for common errors description.
-    pub fn create_key_pair(schema: &[&u8],
-                       cb: Box<Fn(
-                           Result<((String, String, String, String)), AnoncredsError>) + Send>) {
-        unimplemented!();
-    }
-
-    /// Issues accumulator.
-    ///
-    /// #Params
-    /// schema: claim definition schema as a byte array.
-    /// accumulator_id: accumulator id as a byte array.
-    /// max_claims: maximum number of claims within accumulator as a byte array.
-    /// public_key_non_revocation: issuer's non-revocation public key as a byte array.
-    /// cb: Callback that takes command result as a parameter.
-    ///
-    /// #Returns
-    /// Tuple of (Accumulator, Tails, Accumulator public key, Accumulator secret key) as a String.
-    ///
-    /// #Errors
-    /// No method specific errors.
-    /// See `AnoncredsError` docs for common errors description.
-    pub fn issue_accumulator(schema: &[&u8], accumulator_id: &[&u8], max_claims: &[&u8],
-                             public_key_non_revocation: &[&u8],
-                             cb: Box<Fn(
-                                 Result<((String, String, String, String)),
-                                     AnoncredsError>
-                             ) + Send>) {
-        unimplemented!();
-    }
-
-
-    /// Issues claim.
-    ///
-    /// #Params
-    /// attributes:  all names of attributes as a byte array.
-    /// accumulator: accumulator as a byte array.
-    /// sequence_number: claim's sequence number within accumulator as a byte array.
-    /// claim_request:
-    ///     A claim request containing prover ID and prover-generated values as a byte array.
-    /// public_key: issuer's public_key as a byte array.
-    /// secret_key: issuer's secret_key as a byte array.
-    /// public_key_non_revocation: issuer's non-revocation public key as a byte array.
-    /// secret_key_non_revocation: issuer's non-revocation secret key as a byte array.
-    /// tails: tails as a byte array.
-    /// secret_key_accumulator: accumulator's secret key as a byte array.
-    /// cb: Callback that takes command result as a parameter.
-    ///
-    /// #Returns
-    /// The claim (both primary and non-revocation) as a String.
-    ///
-    /// #Errors
-    /// No method specific errors.
-    /// See `AnoncredsError` docs for common errors description.
-    pub fn issue_claim(attributes: &[&u8], accumulator: &[&u8], sequence_number: &[&u8],
-                       claim_request: &[&u8], public_key: &[&u8],
-                       secret_key: &[&u8], public_key_non_revocation: &[&u8],
-                       secret_key_non_revocation: &[&u8], tails: &[&u8],
-                       secret_key_accumulator: &[&u8],
-                       cb: Box<Fn(Result<String, AnoncredsError>) + Send>) {
-        unimplemented!();
-    }
-
-    /// Creates claim request.
-    ///
-    /// #Params
-    /// master_secret: prover's master secret as a byte array.
-    /// public_key: issuer's public_key as a byte array.
-    /// public_key_non_revocation: issuer's non-revocation public key as a byte array.
-    /// request_non_revocation: whether to request non-revocation claim as a byte array.
-    /// cb: Callback that takes command result as a parameter.
-    ///
-    /// #Returns
-    /// Claim request as a String.
-    ///
-    /// #Errors
-    /// No method specific errors.
-    /// See `AnoncredsError` docs for common errors description.
-    pub fn create_claim_request(master_secret: &[&u8], public_key: &[&u8],
-                                public_key_non_revocation: &[&u8], request_non_revocation: &[&u8],
-                                cb: Box<Fn(Result<String, AnoncredsError>) + Send>) {
-        unimplemented!();
-    }
-
-
-    /// Creates proof.
-    ///
-    /// #Params
-    /// proof_input: description of a proof to be presented
-    ///     (revealed attributes, predicates, timestamps for non-revocation) as a byte array.
-    /// nonce: verifier's nonce as a byte array.
-    /// claims: necessary claims for proof as a byte array.
-    /// master_secret: prover's master secret key as a byte array.
-    /// public_key: issuer's public key as a byte array.
-    /// public_key_non_revocation: issuer's non-revocation public key as a byte array.
-    /// accumulator: accumulator as a byte array.
-    /// cb: Callback that takes command result as a parameter.
-    ///
-    /// #Returns
-    /// Typle of (
-    ///         Proof (both primary and non-revocation),
-    ///         Revealed attributes (initial non-encoded values)
-    ///     ) as a String.
-    ///
-    /// #Errors
-    /// No method specific errors.
-    /// See `AnoncredsError` docs for common errors description.
-    pub fn create_proof(proof_input: &[&u8], nonce: &[&u8], claims: &[&u8],
-                        public_key_non_revocation: &[&u8], accumulator: &[&u8], public_key: &[&u8],
-                        master_secret: &[&u8],
-                        cb: Box<Fn(Result<(String, String), AnoncredsError>) + Send>) {
-        unimplemented!();
-    }
-
-    /// Verifies proof.
-    ///
-    /// #Params
-    /// proof_input: description of a proof to be presented
-    ///     (revealed attributes, predicates, timestamps for non-revocation) as a byte array.
-    /// proof: a proof as a byte array.
-    /// revealed_attributes:
-    ///     values of revealed attributes (initial values, non-encoded) as a byte array.
-    /// nonce: verifier's nonce as a byte array.
-    /// public_key_non_revocation: issuer's non-revocation public key as a byte array.
-    /// accumulator: accumulator as a byte array.
-    /// public_key_accumulator: accumulator's public key as a byte array.
-    /// public_key: issuer's public key as a byte array.
-    /// attributes:  all names of attributes as a byte array.
-    /// cb: Callback that takes command result as a parameter.
-    ///
-    /// #Returns
-    /// True if verified successfully and false otherwise as a String.
-    ///
-    /// #Errors
-    /// No method specific errors.
-    /// See `AnoncredsError` docs for common errors description.
-    pub fn verify_proof(proof_input: &[&u8], proof: &[&u8], revealed_attributes: &[&u8],
-                        nonce: &[&u8], public_key_non_revocation: &[&u8],
-                        accumulator: &[&u8], public_key_accumulator: &[&u8],
-                        public_key: &[&u8], attributes: &[&u8],
-                        cb: Box<Fn(Result<String, AnoncredsError>) + Send>) {
-        unimplemented!();
-    }
-
-    /// Creates verifier's nonce.
-    ///
-    /// #Params
-    /// cb: Callback that takes command result as a parameter.
-    ///
-    /// #Returns
-    /// Nonce as a String.
-    ///
-    /// #Errors
-    /// No method specific errors.
-    /// See `AnoncredsError` docs for common errors description.
-    pub fn create_nonce(cb: Box<Fn(Result<String, AnoncredsError>) + Send>) {
-        unimplemented!();
-    }
+#[no_mangle]
+pub extern fn anoncreds_create_key_pair(client_id: i32, command_id: i32, schema: *const c_uchar,
+                                        cb: extern fn(xcommand_id: i32, err: i32,
+                                                      pk: *const c_char, sk: *const c_char,
+                                                      pnrk: *const c_char, snrk: *const c_char)) {
+    unimplemented!();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[no_mangle]
+pub extern fn anoncreds_issue_accumulator(client_id: i32, command_id: i32, schema: *const c_uchar,
+                                          accumulator_id: *const c_uchar,
+                                          max_claims: *const c_uchar,
+                                          public_key_non_revocation: *const c_uchar,
+                                          cb: extern fn(xcommand_id: i32, err: i32,
+                                                        acc: *const c_char, tails: *const c_char,
+                                                        acc_pk: *const c_char, acc_sk: *const c_char
+                                          )) {
+    unimplemented!();
+}
 
-    #[test]
-    fn anoncreds_api_can_be_created() {
-        let anoncreds_api = AnoncredsAPI::new(Arc::new(CommandExecutor::new()));
-        assert! (true, "No crashes on AnoncredsAPI::new");
-    }
+#[no_mangle]
+pub extern fn anoncreds_issue_claim(client_id: i32, command_id: i32, attributes: *const c_uchar,
+                                    accumulator: *const c_uchar, sequence_number: *const c_uchar,
+                                    claim_request: *const c_uchar, public_key: *const c_uchar,
+                                    secret_key: *const c_uchar,
+                                    public_key_non_revocation: *const c_uchar,
+                                    secret_key_non_revocation: *const c_uchar,
+                                    tails: *const c_uchar, secret_key_accumulator: *const c_uchar,
+                                    cb: extern fn(xcommand_id: i32, err: i32, claim: *const c_char)) {
+    unimplemented!();
+}
 
-    #[test]
-    fn anoncredsn_api_can_be_dropped() {
-        fn drop_test() {
-            let anoncreds_api = AnoncredsAPI::new(Arc::new(CommandExecutor::new()));
-        }
+#[no_mangle]
+pub extern fn anoncreds_create_claim_request(client_id: i32, command_id: i32, master_secret: *const c_uchar,
+                                             public_key: *const c_uchar,
+                                             public_key_non_revocation: *const c_uchar,
+                                             request_non_revocation: *const c_uchar,
+                                             cb: extern fn(xcommand_id: i32, err: i32,
+                                                           claim_req: *const c_char)) {
+    unimplemented!();
+}
 
-        drop_test();
-        assert! (true, "No crashes on AnoncredsAPI::drop");
-    }
+#[no_mangle]
+pub extern fn anoncreds_create_proof(proof_input: *const c_uchar, nonce: *const c_uchar,
+                                     claims: *const c_uchar,
+                                     public_key_non_revocation: *const c_uchar,
+                                     accumulator: *const c_uchar, public_key: *const c_uchar,
+                                     master_secret: *const c_uchar,
+                                     cb: extern fn(xcommand_id: i32, err: i32,
+                                                   proof: *const c_char, attrs: *const c_char)) {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub extern fn anoncreds_verify_proof(proof_input: *const c_uchar, proof: *const c_uchar,
+                                     revealed_attributes: *const c_uchar, nonce: *const c_uchar,
+                                     public_key_non_revocation: *const c_uchar,
+                                     accumulator: *const c_uchar,
+                                     public_key_accumulator: *const c_uchar,
+                                     public_key: *const c_uchar, attributes: *const c_uchar,
+                                     cb: extern fn(xcommand_id: i32, err: i32)) {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub extern fn anoncreds_create_nonce(cb: extern fn(client_id: i32, command_id: i32, err: i32, nonce: *const c_char)) {
+    unimplemented!();
 }
