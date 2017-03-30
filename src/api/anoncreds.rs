02a3655a8a8f76deee6bd22c4399702620cebdf4
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-03-30 20:52:44 +0300

    removed unnecessary parameter

--- src/api/anoncreds.rs
+++ src/api/anoncreds.rs
@@ -14,7 +14,6 @@ pub extern fn anoncreds_create_claim(client_id: i32, command_id: i32,
 #[no_mangle]
 pub extern fn anoncreds_create_claim_request(client_id: i32, command_id: i32,
                                              schema_id: *const c_char,
-                                             request_non_revocation: bool,
                                              cb: extern fn(xcommand_id: i32, err: i32,
                                                            claim_req: *const c_char)) {
     unimplemented!();
