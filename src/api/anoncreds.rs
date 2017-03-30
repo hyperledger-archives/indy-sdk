770dfeddc1f9ca7a1a3c71236d6909f8673d34ae
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-03-30 21:09:41 +0300

    added documentation

--- src/api/anoncreds.rs
+++ src/api/anoncreds.rs
@@ -2,7 +2,23 @@ extern crate libc;
 
 use self::libc::{c_char, c_uchar};
 
-
+/// Issues claim.
+///
+/// #Params
+/// client_id: id of sovrin client instance.
+/// command_id: command id to map of callback to user context.
+/// schema_id: id of schema.
+/// attributes:  all names of attributes as a byte array.
+/// claim_request:
+///     A claim request containing prover ID and prover-generated values as a byte array.
+/// cb: Callback that takes command result as parameter.
+///
+/// #Returns
+/// Returns Claim.
+///
+/// #Errors
+/// No method specific errors.
+/// See `AnoncredsError` docs for common errors description.
 #[no_mangle]
 pub extern fn anoncreds_create_claim(client_id: i32, command_id: i32,
                                      schema_id: *const c_char, attributes: *const c_char,
@@ -11,6 +27,20 @@ pub extern fn anoncreds_create_claim(client_id: i32, command_id: i32,
     unimplemented!();
 }
 
+/// Creates claim request.
+///
+/// #Params
+/// client_id: id of sovrin client instance.
+/// command_id: command id to map of callback to user context.
+/// schema_id: id of schema.
+/// cb: Callback that takes command result as parameter.
+///
+/// #Returns
+/// Claim request.
+///
+/// #Errors
+/// No method specific errors.
+/// See `AnoncredsError` docs for common errors description.
 #[no_mangle]
 pub extern fn anoncreds_create_claim_request(client_id: i32, command_id: i32,
                                              schema_id: *const c_char,
@@ -19,6 +49,26 @@ pub extern fn anoncreds_create_claim_request(client_id: i32, command_id: i32,
     unimplemented!();
 }
 
+/// Creates proof.
+///
+/// #Params
+/// client_id: id of sovrin client instance.
+/// command_id: command id to map of callback to user context.
+/// proof_input: description of a proof to be presented
+///     (revealed attributes, predicates, timestamps for non-revocation).
+///     For example: {
+///         names:['name', 'height'],
+///         predicates: [{predicate: 'gt', value: 18, attr: age}]
+///     }
+/// nonce: verifier's nonce.
+/// cb: Callback that takes command result as parameter.
+///
+/// #Returns
+/// Proof.
+///
+/// #Errors
+/// No method specific errors.
+/// See `AnoncredsError` docs for common errors description.
 #[no_mangle]
 pub extern fn anoncreds_create_proof(client_id: i32, command_id: i32,
                                      proof_input: *const c_char,
@@ -28,6 +78,29 @@ pub extern fn anoncreds_create_proof(client_id: i32, command_id: i32,
     unimplemented!();
 }
 
+/// Verifies proof.
+///
+/// #Params
+/// client_id: id of sovrin client instance.
+/// command_id: command id to map of callback to user context.
+/// proof_input: description of a proof to be presented
+///     (revealed attributes, predicates, timestamps for non-revocation).
+///     For example: {
+///         names:['name', 'height'],
+///         predicates: [{predicate: 'gt', value: 18, attr: age}]
+///     }
+/// proof: a proof.
+/// revealed_attributes:
+///     values of revealed attributes (initial values, non-encoded).
+/// nonce: verifier's nonce.
+/// cb: Callback that takes command result as parameter.
+///
+/// #Returns
+/// Claim request.
+///
+/// #Errors
+/// No method specific errors.
+/// See `AnoncredsError` docs for common errors description.
 #[no_mangle]
 pub extern fn anoncreds_verify_proof(client_id: i32, command_id: i32,
                                      proof_input: *const c_char,
