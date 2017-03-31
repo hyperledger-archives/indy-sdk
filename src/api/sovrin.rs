382012e715d859ac8d2260bf452a11d44e3b6daf
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-03-31 15:35:34 +0300

    changes for api

--- src/api/sovrin.rs
+++ src/api/sovrin.rs
@@ -2,16 +2,32 @@ extern crate libc;
 
 use self::libc::{c_char, c_uchar};
 
-/// Sends transaction message to Sovrin network.
+#[no_mangle]
+pub extern fn ledger_check_verkey(client_handle: i32, command_handle: i32,
+                                   did: *const c_char, verkey: *const c_char,
+                                   cb: extern fn(xcommand_handle: i32, err: i32,
+                                                 xverkey: *const c_char)) {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub extern fn ledger_read_ddo(client_handle: i32, command_handle: i32,
+                               submitter_did: *const c_char, target_did: *const c_char,
+                               cb: extern fn(xcommand_handle: i32, err: i32,
+                                             txn_result_json: *const c_char)) {
+    unimplemented!();
+}
+
+/// Sends transaction message to Ledger network.
 ///
 /// Adds issuer information to passed transaction json, signs it with issuer sign key
-/// and sends signed transaction message to Sovring network.
+/// and sends signed transaction message to Ledger network.
 ///
 /// #Params
-/// client_id: id of sovrin client instance.
-/// command_id: command id to map of callback to user context.
-/// issuer: Id of Identity stored in secured Wallet.
-/// tx_json: Transaction data json.
+/// client_handle: id of Ledger client instance.
+/// command_handle: command id to map of callback to user context.
+/// submitter_did: Id of Identity stored in secured Wallet.
+/// txn_json: Transaction data json.
 /// cb: Callback that takes command result as parameter.
 ///
 /// #Returns
@@ -19,97 +35,97 @@ use self::libc::{c_char, c_uchar};
 ///
 /// #Errors
 /// No method specific errors.
-/// See `SovrinError` docs for common errors description.
+/// See `LedgerError` docs for common errors description.
 #[no_mangle]
-pub  extern fn sovrin_send_tx(client_id: i32, command_id: i32,
-                              issuer: *const c_char, tx_json: *const c_char,
-                              cb: extern fn(xcommand_id: i32, err: i32,
-                                            tx_result_json: *const c_char)) {
+pub extern fn ledger_write_txn(client_handle: i32, command_handle: i32,
+                                submitter_did: *const c_char, txn_json: *const c_char,
+                                cb: extern fn(xcommand_handle: i32, err: i32,
+                                              txn_result_json: *const c_char)) {
     unimplemented!();
 }
 
-// Creates NYM transaction message and sends it to Sovrin network.
+// Creates NYM transaction message and sends it to Ledger network.
 #[no_mangle]
-pub extern fn sovrin_send_nym_tx(client_id: i32, command_id: i32,
-                                 issuer: *const c_char,
-                                 dest: *const c_char, verkey: *const c_char, xref: *const c_char,
-                                 data: *const c_char, role: *const c_char,
-                                 cb: extern fn(xcommand_id: i32, err: i32,
-                                               tx_result_json: *const c_char)) {
+pub extern fn ledger_write_nym_txn(client_handle: i32, command_handle: i32,
+                                   submitter_did: *const c_char,
+                                   target_did: *const c_char,
+                                   verkey: *const c_char, xref: *const c_char,
+                                   data: *const c_char, role: *const c_char,
+                                   cb: extern fn(xcommand_handle: i32, err: i32,
+                                                 txn_result_json: *const c_char)) {
     unimplemented!();
 }
 
-// Creates ATTRIB transaction message and sends it to Sovrin network.
+// Creates ATTRIB transaction message and sends it to Ledger network.
 #[no_mangle]
-pub extern fn sovrin_send_attrib_tx(client_id: i32, command_id: i32,
-                                    issuer: *const c_char, dest: *const c_char,
-                                    hash: *const c_char, raw: *const c_char, enc: *const c_char,
-                                    cb: extern fn(xcommand_id: i32, err: i32,
-                                                  tx_result_json: *const c_char)) {
+pub extern fn ledger_write_attrib_txn(client_handle: i32, command_handle: i32,
+                                      submitter_did: *const c_char, target_did: *const c_char,
+                                      hash: *const c_char, raw: *const c_char, enc: *const c_char,
+                                      cb: extern fn(xcommand_handle: i32, err: i32,
+                                                    txn_result_json: *const c_char)) {
     unimplemented!();
 }
 
-// Creates GET_ATTRIB transaction message and sends it to Sovrin network.
-#[no_mangle]
-pub extern fn sovrin_send_get_attrib_tx(client_id: i32, command_id: i32,
-                                        issuer: *const c_char, dest: *const c_char,
-                                        data: *const c_char,
-                                        cb: extern fn(xcommand_id: i32, err: i32,
-                                                      tx_result_json: *const c_char)) {
+// Creates GET_ATTRIB transaction message and sends it to Ledger network.
+pub extern fn ledger_read_attrib(client_handle: i32, command_handle: i32,
+                                 submitter_did: *const c_char, target_did: *const c_char,
+                                 data: *const c_char,
+                                 cb: extern fn(xcommand_handle: i32, err: i32,
+                                               txn_result_json: *const c_char)) {
     unimplemented!();
 }
 
-// Creates GET_NYM transaction message and sends it to Sovrin network.
+// Creates GET_NYM transaction message and sends it to Ledger network.
 #[no_mangle]
-pub extern fn sovrin_send_get_nym_tx(client_id: i32, command_id: i32,
-                                     issuer: *const c_char, dest: *const c_char,
-                                     cb: extern fn(xcommand_id: i32, err: i32,
-                                                   tx_result_json: *const c_char)) {
+pub extern fn ledger_read_nym(client_handle: i32, command_handle: i32,
+                              submitter_did: *const c_char, target_did: *const c_char,
+                              cb: extern fn(xcommand_handle: i32, err: i32,
+                                            txn_result_json: *const c_char)) {
     unimplemented!();
 }
 
-// Creates SCHEMA transaction message and sends it to Sovrin network.
+// Creates SCHEMA transaction message and sends it to Ledger network.
 #[no_mangle]
-pub extern fn sovrin_send_schema_tx(client_id: i32, command_id: i32,
-                                    issuer: *const c_char, data: *const c_char,
-                                    cb: extern fn(xcommand_id: i32, err: i32,
-                                                  tx_result_json: *const c_char)) {
+pub extern fn ledger_write_schema_txn(client_handle: i32, command_handle: i32,
+                                      submitter_did: *const c_char, data: *const c_char,
+                                      cb: extern fn(xcommand_handle: i32, err: i32,
+                                                    txn_result_json: *const c_char)) {
     unimplemented!();
 }
 
-// Creates GET_SCHEMA transaction message and sends it to Sovrin network.
+// Creates GET_SCHEMA transaction message and sends it to Ledger network.
 #[no_mangle]
-pub extern fn sovrin_send_get_schema_tx(client_id: i32, command_id: i32,
-                                        issuer: *const c_char, data: *const c_char,
-                                        cb: extern fn(xcommand_id: i32, err: i32,
-                                                      tx_result_json: *const c_char)) {
+pub extern fn ledger_read_schema(client_handle: i32, command_handle: i32,
+                                 submitter_did: *const c_char, data: *const c_char,
+                                 cb: extern fn(xcommand_handle: i32, err: i32,
+                                               txn_result_json: *const c_char)) {
     unimplemented!();
 }
 
-// Creates ISSUER_KEY transaction message and sends it to Sovrin network.
+// Creates ISSUER_KEY transaction message and sends it to Ledger network.
 #[no_mangle]
-pub extern fn sovrin_send_issuer_key_tx(client_id: i32, command_id: i32,
-                                        issuer: *const c_char, xref: *const c_char,
-                                        data: *const c_char,
-                                        cb: extern fn(xcommand_id: i32, err: i32,
-                                                      tx_result_json: *const c_char)) {
+pub extern fn ledger_write_issuer_key_txn(client_handle: i32, command_handle: i32,
+                                          submitter_did: *const c_char, xref: *const c_char,
+                                          data: *const c_char,
+                                          cb: extern fn(xcommand_handle: i32, err: i32,
+                                                        txn_result_json: *const c_char)) {
     unimplemented!();
 }
 
-// Creates GET_ISSUER_KEY transaction message and sends it to Sovrin network.
-#[no_mangle]
-pub extern fn sovrin_send_get_issuer_key_tx(client_id: i32, command_id: i32,
-                                            issuer: *const c_char, xref: *const c_char,
-                                            cb: extern fn(xcommand_id: i32, err: i32,
-                                                          tx_result_json: *const c_char)) {
+// Creates GET_ISSUER_KEY transaction message and sends it to Ledger network.
+pub extern fn ledger_read_issuer_key(client_handle: i32, command_handle: i32,
+                                     submitter_did: *const c_char, xref: *const c_char,
+                                     cb: extern fn(xcommand_handle: i32, err: i32,
+                                                   txn_result_json: *const c_char)) {
     unimplemented!();
 }
 
-// Creates NODE transaction message and sends it to Sovrin network.
+// Creates NODE transaction message and sends it to Ledger network.
 #[no_mangle]
-pub extern fn sovrin_send_node_tx(client_id: i32, command_id: i32,
-                                  issuer: *const c_char, dest: *const c_char, data: *const c_char,
-                                  cb: extern fn(xcommand_id: i32, err: i32,
-                                                tx_result_json: *const c_char)) {
+pub extern fn ledger_write_node_txn(client_handle: i32, command_handle: i32,
+                                    submitter_did: *const c_char, target_did: *const c_char,
+                                    data: *const c_char,
+                                    cb: extern fn(xcommand_handle: i32, err: i32,
+                                                  txn_result_json: *const c_char)) {
     unimplemented!();
 }
