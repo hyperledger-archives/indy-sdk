9b82951a3c34fc824d27627bd7ff2af1b938a054
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-04-11 17:52:32 +0300

    added AnoncredsService

--- src/services/crypto/mod.rs
+++ src/services/crypto/mod.rs
@@ -3,25 +3,19 @@ pub mod anoncreds;
 pub mod ed25519;
 pub mod wrappers;
 
+use self::anoncreds::AnoncredsService;
 use self::ed25519::Sodium;
-use self::anoncreds::issuer::Issuer;
-use self::anoncreds::prover::Prover;
-use self::anoncreds::verifier::Verifier;
 
 pub struct CryptoService {
     sodium: Sodium,
-    issuer: Issuer,
-    prover: Prover,
-    verifier: Verifier
+    anoncreds: AnoncredsService
 }
 
 impl CryptoService {
     pub fn new() -> CryptoService {
         CryptoService {
             sodium: Sodium::new(),
-            issuer: Issuer::new(),
-            prover: Prover::new(),
-            verifier: Verifier::new()
+            anoncreds: AnoncredsService::new()
         }
     }
 }
