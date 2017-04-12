dfeef1ab16ccd3ecb03d6e638cd0171b74ff8104
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-04-12 10:13:18 +0300

    refactoring
    txn->request, publish->submit

--- src/api/ledger.rs
+++ src/api/ledger.rs
@@ -4,17 +4,17 @@ use api::ErrorCode;
 
 use self::libc::{c_char, c_uchar};
 
-/// Signs and publishes transaction message to validator pool.
+/// Signs and submits transaction message to validator pool.
 ///
 /// Adds submitter information to passed transaction json, signs it with submitter
 /// sign key (see wallet_sign), and sends signed transaction message
-/// to validator pool (see write_txn).
+/// to validator pool (see write_request).
 ///
 /// #Params
 /// command_handle: command handle to map callback to caller context.
 /// wallet_handle: wallet handle (created by open_wallet).
 /// submitter_did: Id of Identity stored in secured Wallet.
-/// txn_json: Transaction data json.
+/// request_json: Transaction data json.
 /// cb: Callback that takes command result as parameter.
 ///
 /// #Returns
@@ -26,22 +26,22 @@ use self::libc::{c_char, c_uchar};
 /// Ledger*
 /// Crypto*
 #[no_mangle]
-pub extern fn sign_and_publish_txn(command_handle: i32,
+pub extern fn sign_and_submit_request(command_handle: i32,
                                    wallet_handle: i32,
-                                   submitter_did: *const c_char, txn_json: *const c_char,
+                                   submitter_did: *const c_char, request_json: *const c_char,
                                    cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                                 txn_result_json: *const c_char)) -> ErrorCode {
+                                                 request_result_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
-/// Publishes transaction message to validator pool (no signing, unlike sign_and_publish_txn).
+/// Publishes transaction message to validator pool (no signing, unlike sign_and_submit_request).
 ///
 /// The transaction is sent to the validator pool as is. It's assumed that it's already prepared.
 ///
 /// #Params
 /// command_handle: command handle to map callback to caller context.
 /// pool_handle: pool handle (created by open_pool_ledger).
-/// txn_json: Transaction data json.
+/// request_json: Transaction data json.
 /// cb: Callback that takes command result as parameter.
 ///
 /// #Returns
@@ -51,16 +51,16 @@ pub extern fn sign_and_publish_txn(command_handle: i32,
 /// Common*
 /// Ledger*
 #[no_mangle]
-pub extern fn publish_txn(command_handle: i32,
+pub extern fn submit_request(command_handle: i32,
                           pool_handle: i32,
-                          txn_json: *const c_char,
+                          request_json: *const c_char,
                           cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                        txn_result_json: *const c_char)) -> ErrorCode {
+                                        request_result_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
 
-/// Builds a txn to get a DDO.
+/// Builds a request to get a DDO.
 ///
 /// #Params
 /// command_handle: command handle to map callback to caller context.
@@ -74,10 +74,10 @@ pub extern fn publish_txn(command_handle: i32,
 /// #Errors
 /// Common*
 #[no_mangle]
-pub extern fn build_get_ddo_txn(command_handle: i32,
+pub extern fn build_get_ddo_request(command_handle: i32,
                                 submitter_did: *const c_char, target_did: *const c_char,
                                 cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                              txn_json: *const c_char)) -> ErrorCode {
+                                              request_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
@@ -100,13 +100,13 @@ pub extern fn build_get_ddo_txn(command_handle: i32,
 /// #Errors
 /// Common*
 #[no_mangle]
-pub extern fn build_nym_txn(command_handle: i32,
+pub extern fn build_nym_request(command_handle: i32,
                             submitter_did: *const c_char,
                             target_did: *const c_char,
                             verkey: *const c_char, xref: *const c_char,
                             data: *const c_char, role: *const c_char,
                             cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                          txn_json: *const c_char)) -> ErrorCode {
+                                          request_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
@@ -127,11 +127,11 @@ pub extern fn build_nym_txn(command_handle: i32,
 /// #Errors
 /// Common*
 #[no_mangle]
-pub extern fn build_attrib_txn(command_handle: i32,
+pub extern fn build_attrib_request(command_handle: i32,
                                submitter_did: *const c_char, target_did: *const c_char,
                                hash: *const c_char, raw: *const c_char, enc: *const c_char,
                                cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                             txn_json: *const c_char)) -> ErrorCode {
+                                             request_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
@@ -149,11 +149,11 @@ pub extern fn build_attrib_txn(command_handle: i32,
 ///
 /// #Errors
 /// Common*
-pub extern fn build_get_attrib_txn(command_handle: i32,
+pub extern fn build_get_attrib_request(command_handle: i32,
                                    submitter_did: *const c_char, target_did: *const c_char,
                                    data: *const c_char,
                                    cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                                 txn_json: *const c_char)) -> ErrorCode {
+                                                 request_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
@@ -171,10 +171,10 @@ pub extern fn build_get_attrib_txn(command_handle: i32,
 /// #Errors
 /// Common*
 #[no_mangle]
-pub extern fn build_get_nym_txn(command_handle: i32,
+pub extern fn build_get_nym_request(command_handle: i32,
                                 submitter_did: *const c_char, target_did: *const c_char,
                                 cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                              txn_json: *const c_char)) -> ErrorCode {
+                                              request_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
@@ -192,10 +192,10 @@ pub extern fn build_get_nym_txn(command_handle: i32,
 /// #Errors
 /// Common*
 #[no_mangle]
-pub extern fn build_schema_txn(command_handle: i32,
+pub extern fn build_schema_request(command_handle: i32,
                                submitter_did: *const c_char, data: *const c_char,
                                cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                             txn_json: *const c_char)) -> ErrorCode {
+                                             request_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
@@ -213,10 +213,10 @@ pub extern fn build_schema_txn(command_handle: i32,
 /// #Errors
 /// Common*
 #[no_mangle]
-pub extern fn build_get_schema_txn(command_handle: i32,
+pub extern fn build_get_schema_request(command_handle: i32,
                                    submitter_did: *const c_char, data: *const c_char,
                                    cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                                 txn_json: *const c_char)) -> ErrorCode {
+                                                 request_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
@@ -235,11 +235,11 @@ pub extern fn build_get_schema_txn(command_handle: i32,
 /// #Errors
 /// Common*
 #[no_mangle]
-pub extern fn build_issuer_key_txn(command_handle: i32,
+pub extern fn build_issuer_key_request(command_handle: i32,
                                    submitter_did: *const c_char, xref: *const c_char,
                                    data: *const c_char,
                                    cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                                 txn_result_json: *const c_char)) -> ErrorCode {
+                                                 request_result_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
@@ -256,10 +256,10 @@ pub extern fn build_issuer_key_txn(command_handle: i32,
 ///
 /// #Errors
 /// Common*
-pub extern fn build_get_issuer_key_txn(command_handle: i32,
+pub extern fn build_get_issuer_key_request(command_handle: i32,
                                        submitter_did: *const c_char, xref: *const c_char,
                                        cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                                     txn_json: *const c_char)) -> ErrorCode {
+                                                     request_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
 
@@ -278,10 +278,10 @@ pub extern fn build_get_issuer_key_txn(command_handle: i32,
 /// #Errors
 /// Common*
 #[no_mangle]
-pub extern fn build_node_txn(command_handle: i32,
+pub extern fn build_node_request(command_handle: i32,
                              submitter_did: *const c_char, target_did: *const c_char,
                              data: *const c_char,
                              cb: extern fn(xcommand_handle: i32, err: ErrorCode,
-                                           txn_json: *const c_char)) -> ErrorCode {
+                                           request_json: *const c_char)) -> ErrorCode {
     unimplemented!();
 }
