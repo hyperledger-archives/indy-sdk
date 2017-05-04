f0fa005f6d2419f50f4a99f398d0b185be6afb60
Sergey Minaev <sergey.minaev@dsr-company.com>
2017-05-04 16:18:00 +0300

    Update integration test sovrin_submit_request_can_be_called.

--- tests/api_pool.rs
+++ tests/api_pool.rs
@@ -1,6 +1,9 @@
 extern crate sovrin;
 
 #[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+#[macro_use]
 extern crate lazy_static;
 
 #[path = "utils/mod.rs"]
@@ -25,7 +28,18 @@ use utils::callbacks::CallbacksHelpers;
 
 use std::sync::mpsc::{channel};
 use std::ffi::{CString};
+use std::io::Write;
 
+fn _create_default_pool_transaction_file() {
+    let mut f = fs::File::create("./pool_transactions_sandbox").unwrap();
+    let data = format!("{}\n{}\n{}\n{}\n",
+                       "{\"data\":{\"alias\":\"Node1\",\"client_ip\":\"10.0.0.2\",\"client_port\":9702,\"node_ip\":\"10.0.0.2\",\"node_port\":9701,\"services\":[\"VALIDATOR\"]},\"dest\":\"Gw6pDLhcBcoQesN72qfotTgFa7cbuqZpkX3Xo6pLhPhv\",\"identifier\":\"FYmoFw55GeQH7SRFa37dkx1d2dZ3zUF8ckg7wmL7ofN4\",\"txnId\":\"fea82e10e894419fe2bea7d96296a6d46f50f93f9eeda954ec461b2ed2950b62\",\"type\":\"0\"}",
+                       "{\"data\":{\"alias\":\"Node2\",\"client_ip\":\"10.0.0.3\",\"client_port\":9704,\"node_ip\":\"10.0.0.3\",\"node_port\":9703,\"services\":[\"VALIDATOR\"]},\"dest\":\"8ECVSk179mjsjKRLWiQtssMLgp6EPhWXtaYyStWPSGAb\",\"identifier\":\"8QhFxKxyaFsJy4CyxeYX34dFH8oWqyBv1P4HLQCsoeLy\",\"txnId\":\"1ac8aece2a18ced660fef8694b61aac3af08ba875ce3026a160acbc3a3af35fc\",\"type\":\"0\"}",
+                       "{\"data\":{\"alias\":\"Node3\",\"client_ip\":\"10.0.0.4\",\"client_port\":9706,\"node_ip\":\"10.0.0.4\",\"node_port\":9705,\"services\":[\"VALIDATOR\"]},\"dest\":\"DKVxG2fXXTU8yT5N7hGEbXB3dfdAnYv1JczDUHpmDxya\",\"identifier\":\"2yAeV5ftuasWNgQwVYzeHeTuM7LwwNtPR3Zg9N4JiDgF\",\"txnId\":\"7e9f355dffa78ed24668f0e0e369fd8c224076571c51e2ea8be5f26479edebe4\",\"type\":\"0\"}",
+                       "{\"data\":{\"alias\":\"Node4\",\"client_ip\":\"10.0.0.5\",\"client_port\":9708,\"node_ip\":\"10.0.0.5\",\"node_port\":9707,\"services\":[\"VALIDATOR\"]},\"dest\":\"4PS3EDQ3dW1tci1Bp6543CfuuebjFrg36kLAUcskGfaA\",\"identifier\":\"FTE95CVthRtrBnK2PYCBbC9LghTcGwi9Zfi1Gz2dnyNx\",\"txnId\":\"aa5e817d7cc626170eca175822029339a444eb0ee8f0bd20d3b0b76e566fb008\",\"type\":\"0\"}");
+    f.write_all(data.as_bytes()).unwrap();
+    f.flush().unwrap();
+}
 
 #[test]
 fn sovrin_create_pool_ledger_can_be_called() {
@@ -38,8 +52,8 @@ fn sovrin_create_pool_ledger_can_be_called() {
     let (command_handle, callback) = CallbacksHelpers::closure_to_create_pool_ledger_cb(cb);
 
     let pool_name = CString::new("test_open").unwrap();
-    let pool_config = CString::new("{\"genesis_txn\": \"./test_open_src.txn\"}").unwrap();
-    std::fs::File::create("./test_open_src.txn").unwrap();
+    let pool_config = CString::new("{\"genesis_txn\": \"./pool_transactions_sandbox\"}").unwrap();
+    _create_default_pool_transaction_file();
 
     let err = sovrin_create_pool_ledger_config(command_handle,
                                                pool_name.as_ptr(),
@@ -100,7 +114,24 @@ fn sovrin_open_pool_ledger_can_be_called() {
 #[test]
 #[ignore] //required nodes pool available from CI
 fn sovrin_submit_request_can_be_called() {
-    //TODO call create pool ledger before open
+    std::fs::remove_dir_all(EnvironmentUtils::pool_path("test")).unwrap();
+    //create pool
+    let (sender, receiver) = channel();
+    let cb = Box::new(move |err| {
+        sender.send(err).unwrap();
+    });
+    let (command_handle, callback) = CallbacksHelpers::closure_to_create_pool_ledger_cb(cb);
+    let pool_name = CString::new("test").unwrap();
+    let pool_config = CString::new("{\"genesis_txn\": \"./pool_transactions_sandbox\"}").unwrap();
+    _create_default_pool_transaction_file();
+    let err = sovrin_create_pool_ledger_config(command_handle,
+                                               pool_name.as_ptr(),
+                                               pool_config.as_ptr(),
+                                               callback);
+    assert_eq!(ErrorCode::Success, err);
+    let err = receiver.recv_timeout(Duration::from_secs(1)).unwrap();
+    assert_eq!(ErrorCode::Success, err);
+    //open pool
     let (sender, receiver) = channel();
     let cb = Box::new(move |err, handle| {
         sender.send((err, handle)).unwrap();
@@ -115,7 +146,7 @@ fn sovrin_submit_request_can_be_called() {
     let (err, pool_handle) = receiver.recv_timeout(std::time::Duration::from_secs(1)).unwrap();
     assert_eq!(ErrorCode::Success, err);
     std::thread::sleep(std::time::Duration::from_secs(1)); //TODO
-
+    //test communication
     let (sender, receiver) = channel();
     let cb_send = Box::new(move |err, resp| {
         sender.send((err, resp)).unwrap();
@@ -125,7 +156,7 @@ fn sovrin_submit_request_can_be_called() {
             \"identifier\":\"Th7MpTaRZVRYnPiabds81Y\",\
             \"operation\":{\
                 \"type\":\"105\",\
-                \"dest\":\"FYmoFw55GeQH7SRFa37dkx1d2dZ3zUF8ckg7wmL7ofN4\",\
+                \"dest\":\"FYmoFw55GeQH7SRFa37dkx1d2dZ3zUF8ckg7wmL7ofN4\"\
             },\
             \"signature\":\"4o86XfkiJ4e2r3J6Ufoi17UU3W5Zi9sshV6FjBjkVw4sgEQFQov9dxqDEtLbAJAWffCWd5KfAk164QVo7mYwKkiV\"\
         }\
@@ -140,5 +171,26 @@ fn sovrin_submit_request_can_be_called() {
 
     assert_eq!(ErrorCode::Success, err);
     let recv = receiver.recv_timeout(std::time::Duration::from_secs(1));
-    recv.unwrap(); //TODO check data in ok
+
+    #[derive(Deserialize, Eq, PartialEq, Debug)]
+    struct Reply {
+        op: String,
+        result: ReplyResult,
+    }
+    #[derive(Deserialize, Eq, PartialEq, Debug)]
+    #[serde(rename_all = "camelCase")]
+    struct ReplyResult {
+        txn_id: String,
+        req_id: u64,
+    }
+    let exp_reply = Reply {
+        op: "REPLY".to_string(),
+        result: ReplyResult {
+            req_id: 1491566332010860,
+            txn_id: "5511e5493c1d37dfa67b73269a392a7aca5b71e9d10ac106adc7f9e552aee560".to_string(),
+        }
+    };
+    let act_reply: Reply = serde_json::from_str(recv.unwrap().1.as_str()).unwrap();
+    assert_eq!(act_reply, exp_reply);
+    //    std::fs::remove_dir_all(EnvironmentUtils::pool_path("test")).unwrap();
 }
