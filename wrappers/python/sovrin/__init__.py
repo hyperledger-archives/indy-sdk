f7a1dfb0419442d01d669bf34210de93de33caca
Sergej Pupykin <sergej.pupykin@dsr-company.com>
2017-05-04 19:31:31 +0300

    python API draft #2

--- wrappers/python/sovrin/__init__.py
+++ wrappers/python/sovrin/__init__.py
@@ -1,2 +1,9 @@
 from sovrin.error import SovrinError
+
+from sovrin.anoncreds import Anoncreds
+from sovrin.ledger import Ledger
+from sovrin.pool import Pool
+from sovrin.signus import Signus
 from sovrin.wallet import Wallet
+
+__all__ = ["Wallet"]
