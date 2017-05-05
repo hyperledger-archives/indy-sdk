2f1ee90af4ba0f92cfc24c173b96e0e93d97e32c
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-05-05 18:29:09 +0300

    refactoring(static methods)

--- src/utils/crypto/ed25519/sodium.rs
+++ src/utils/crypto/ed25519/sodium.rs
@@ -11,16 +11,12 @@ use std::convert::AsMut;
 pub struct ED25519 {}
 
 impl ED25519 {
-    pub fn new() -> ED25519 {
-        ED25519 {}
-    }
-
-    pub fn create_key_pair(&self) -> (Vec<u8>, Vec<u8>) {
+    pub fn create_key_pair() -> (Vec<u8>, Vec<u8>) {
         let (public_key, private_key) = box_::gen_keypair();
         (public_key[..].to_vec(), private_key[..].to_vec())
     }
 
-    pub fn encrypt(&self, private_key: &[u8], public_key: &[u8], doc: &[u8], nonce: &[u8]) -> Vec<u8> {
+    pub fn encrypt(private_key: &[u8], public_key: &[u8], doc: &[u8], nonce: &[u8]) -> Vec<u8> {
         box_::seal(
             doc,
             &box_::Nonce(ED25519::_clone_into_array(nonce)),
@@ -29,7 +25,7 @@ impl ED25519 {
         )
     }
 
-    pub fn decrypt(&self, private_key: &[u8], public_key: &[u8], doc: &[u8], nonce: &[u8]) -> Result<Vec<u8>, CryptoError> {
+    pub fn decrypt(private_key: &[u8], public_key: &[u8], doc: &[u8], nonce: &[u8]) -> Result<Vec<u8>, CryptoError> {
         box_::open(
             doc,
             &box_::Nonce(ED25519::_clone_into_array(nonce)),
@@ -39,11 +35,11 @@ impl ED25519 {
             .map_err(|_| CryptoError::InvalidStructure("Unable to decrypt data".to_string()))
     }
 
-    pub fn gen_nonce(&self) -> Vec<u8> {
+    pub fn gen_nonce() -> Vec<u8> {
         box_::gen_nonce()[..].to_vec()
     }
 
-    pub fn create_key_pair_for_signature(&self, seed: Option<&[u8]>) -> (Vec<u8>, Vec<u8>) {
+    pub fn create_key_pair_for_signature(seed: Option<&[u8]>) -> (Vec<u8>, Vec<u8>) {
         let (public_key, private_key) =
             sign::keypair_from_seed(
                 &sign::Seed(
@@ -56,7 +52,7 @@ impl ED25519 {
         (public_key[..].to_vec(), private_key[..].to_vec())
     }
 
-    pub fn sign(&self, private_key: &[u8], doc: &[u8]) -> Vec<u8> {
+    pub fn sign(private_key: &[u8], doc: &[u8]) -> Vec<u8> {
         let mut pr_key: [u8; 64] = [0; 64];
         pr_key.clone_from_slice(private_key);
 
@@ -66,7 +62,7 @@ impl ED25519 {
         )
     }
 
-    pub fn verify(&self, public_key: &[u8], doc: &[u8]) -> Result<Vec<u8>, CryptoError> {
+    pub fn verify(public_key: &[u8], doc: &[u8]) -> Result<Vec<u8>, CryptoError> {
         sign::verify(
             doc,
             &sign::PublicKey(ED25519::_clone_into_array(public_key))
@@ -125,33 +121,31 @@ mod tests {
 
     #[test]
     fn encrypt_decrypt_works() {
-        let ed25519 = ED25519::new();
         let text = randombytes::randombytes(16);
-        let nonce = ed25519.gen_nonce();
+        let nonce = ED25519::gen_nonce();
 
-        let (alice_pk, alice_sk) = ed25519.create_key_pair();
-        let (bob_pk, bob_sk) = ed25519.create_key_pair();
+        let (alice_pk, alice_sk) = ED25519::create_key_pair();
+        let (bob_pk, bob_sk) = ED25519::create_key_pair();
 
-        let bob_encrypted_text = ed25519.encrypt(&bob_sk, &alice_pk, &text, &nonce);
-        let bob_decrypt_result = ed25519.decrypt(&alice_sk, &bob_pk, &bob_encrypted_text, &nonce);
+        let bob_encrypted_text = ED25519::encrypt(&bob_sk, &alice_pk, &text, &nonce);
+        let bob_decrypt_result = ED25519::decrypt(&alice_sk, &bob_pk, &bob_encrypted_text, &nonce);
         assert!(bob_decrypt_result.is_ok());
         assert_eq!(text, bob_decrypt_result.unwrap());
 
-        let alice_encrypted_text = ed25519.encrypt(&alice_sk, &bob_pk, &text, &nonce);
-        let alice_decrypted_text = ed25519.decrypt(&bob_sk, &alice_pk, &alice_encrypted_text, &nonce);
+        let alice_encrypted_text = ED25519::encrypt(&alice_sk, &bob_pk, &text, &nonce);
+        let alice_decrypted_text = ED25519::decrypt(&bob_sk, &alice_pk, &alice_encrypted_text, &nonce);
         assert!(alice_decrypted_text.is_ok());
         assert_eq!(text, alice_decrypted_text.unwrap());
     }
 
     #[test]
     fn signin_verify_works() {
-        let ed25519 = ED25519::new();
         let seed = randombytes::randombytes(32);
         let text = randombytes::randombytes(16);
 
-        let (public_key, secret_key) = ed25519.create_key_pair_for_signature(Some(&seed));
-        let alice_signed_text = ed25519.sign(&secret_key, &text);
-        let verified_data = ed25519.verify(&public_key, &alice_signed_text);
+        let (public_key, secret_key) = ED25519::create_key_pair_for_signature(Some(&seed));
+        let alice_signed_text = ED25519::sign(&secret_key, &text);
+        let verified_data = ED25519::verify(&public_key, &alice_signed_text);
 
         assert!(verified_data.is_ok());
         assert_eq!(text, verified_data.unwrap());
