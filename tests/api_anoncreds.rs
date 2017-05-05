dc0585613ad69f1d7d514deb85d760a053145177
artem.ivanov <artem.ivanov@dsr-company.com>
2017-05-05 16:01:52 +0300

    Minor Code refactoring

--- tests/api_anoncreds.rs
+++ tests/api_anoncreds.rs
@@ -3,8 +3,12 @@ extern crate sovrin;
 #[macro_use]
 extern crate lazy_static;
 
+#[macro_use]
 #[path = "utils/mod.rs"]
 mod utils;
+
+use utils::test::TestUtils;
+use utils::timeout::TimeoutUtils;
 
 use sovrin::api::ErrorCode;
 use sovrin::api::anoncreds::{
@@ -32,6 +36,8 @@ use std::ffi::{CString};
 
 #[test]
 fn sovrin_anoncreds_demo() {
+    TestUtils::cleanup_storage();
+
     let (create_wallet_sender, create_wallet_receiver) = channel();
     let (open_wallet_sender, open_wallet_receiver) = channel();
     let (issuer_create_claim_definition_sender, issuer_create_claim_definition_receiver) = channel();
@@ -91,7 +97,7 @@ fn sovrin_anoncreds_demo() {
     let (verifier_verify_proof_handle, verifier_verify_proof_callback) = CallbackUtils::closure_to_verifier_verify_proof_cb(verifier_verify_proof_cb);
 
     let pool_name = "pool1";
-    let name = "wallet79";
+    let name = "wallet";
     let xtype = "default";
 
     let err =
@@ -104,7 +110,7 @@ fn sovrin_anoncreds_demo() {
                              create_wallet_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let err = create_wallet_receiver.recv().unwrap();
+    let err = create_wallet_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     assert_eq!(ErrorCode::Success, err);
 
     let err =
@@ -115,26 +121,27 @@ fn sovrin_anoncreds_demo() {
                            open_wallet_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let (err, wallet_handle) = open_wallet_receiver.recv().unwrap();
+    let (err, wallet_handle) = open_wallet_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     assert_eq!(ErrorCode::Success, err);
 
-    let schema = "{\
-            \"name\":\"gvt\",\
-            \"version\":\"1.0\",\
-            \"attribute_names\":[\"age\",\"sex\",\"height\",\"name\"],\
-            \"seq_no\":1\
-        }";
+    let schema_seq_no = 1;
+    let schema = format!("{{\
+                            \"name\":\"gvt\",\
+                            \"version\":\"1.0\",\
+                            \"attribute_names\":[\"age\",\"sex\",\"height\",\"name\"],\
+                            \"seq_no\":{}\
+                         }}", schema_seq_no);
 
     let err =
         sovrin_issuer_create_and_store_claim_def(issuer_create_claim_definition_command_handle,
                                                  wallet_handle,
-                                                 CString::new(schema).unwrap().as_ptr(),
+                                                 CString::new(schema.clone()).unwrap().as_ptr(),
                                                  null(),
                                                  false,
                                                  create_claim_definition_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let (err, claim_def_json, claim_def_uuid) = issuer_create_claim_definition_receiver.recv().unwrap();
+    let (err, claim_def_json, claim_def_uuid) = issuer_create_claim_definition_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     println!("claim_def_json {:?}", claim_def_json);
     assert_eq!(ErrorCode::Success, err);
 
@@ -147,12 +154,12 @@ fn sovrin_anoncreds_demo() {
                                                  wallet_set_seq_no_for_value_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let err = wallet_set_seq_no_for_value_receiver.recv().unwrap();
+    let err = wallet_set_seq_no_for_value_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     assert_eq!(ErrorCode::Success, err);
 
     //TODO USE ISSUER WALLET AND PROVER WALLET
 
-    let master_secret_name = "name";
+    let master_secret_name = "master_secret";
 
     let err =
         sovrin_prover_create_master_secret(prover_create_master_secret_command_handle,
@@ -161,11 +168,11 @@ fn sovrin_anoncreds_demo() {
                                            prover_create_master_secret_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let err = prover_create_master_secret_receiver.recv().unwrap();
+    let err = prover_create_master_secret_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     assert_eq!(ErrorCode::Success, err);
 
     let prover_did = "some_prover_did";
-    let claim_offer_json = "{\"issuer_did\":\"some_issuer_did\",\"claim_def_seq_no\":1}";
+    let claim_offer_json = format!("{{\"issuer_did\":\"some_issuer_did\",\"claim_def_seq_no\":{}}}", claim_def_seq_no);
 
     let err =
         sovrin_prover_create_and_store_claim_req(prover_create_claim_req_command_handle,
@@ -177,15 +184,16 @@ fn sovrin_anoncreds_demo() {
                                                  prover_create_claim_req_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let (err, claim_req_json) = prover_create_claim_req_receiver.recv().unwrap();
+    let (err, claim_req_json) = prover_create_claim_req_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     println!("claim_req_json {:?}", claim_req_json);
     assert_eq!(ErrorCode::Success, err);
 
-    let claim_json = "{\"sex\":[\"male\",\"5944657099558967239210949258394887428692050081607692519917050011144233115103\"],\
-                       \"name\":[\"Alex\",\"1139481716457488690172217916278103335\"],\
-                       \"height\":[\"175\",\"175\"],\
-                       \"age\":[\"28\",\"28\"]\
-                       }";
+    let claim_json = "{\
+                           \"sex\":[\"male\",\"5944657099558967239210949258394887428692050081607692519917050011144233115103\"],\
+                           \"name\":[\"Alex\",\"1139481716457488690172217916278103335\"],\
+                           \"height\":[\"175\",\"175\"],\
+                           \"age\":[\"28\",\"28\"]\
+                     }";
 
     let err =
         sovrin_issuer_create_claim(issuer_create_claim_command_handle,
@@ -197,7 +205,7 @@ fn sovrin_anoncreds_demo() {
                                    issuer_create_claim_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let (err, revoc_reg_update_json, xclaim_json) = issuer_create_claim_receiver.recv().unwrap();
+    let (err, revoc_reg_update_json, xclaim_json) = issuer_create_claim_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     println!("xclaim_json {:?}", xclaim_json);
     assert_eq!(ErrorCode::Success, err);
 
@@ -208,29 +216,31 @@ fn sovrin_anoncreds_demo() {
                                   prover_store_claim_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let err = prover_store_claim_receiver.recv().unwrap();
+    let err = prover_store_claim_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     assert_eq!(ErrorCode::Success, err);
 
-    let proof_req_json = "{\"nonce\":\"123432421212\",\
-                           \"requested_attrs\":{\"1\":{\"schema_seq_no\":1,\"name\":\"name\"}},\
-                           \"requested_predicates\":{\"1\":{\"attr_name\":\"age\",\"p_type\":\"GE\",\"value\":18}}\
-                         }";
+    let proof_req_json = format!("{{\
+                                   \"nonce\":\"123432421212\",\
+                                   \"requested_attrs\":{{\"attr1_uuid\":{{\"schema_seq_no\":{},\"name\":\"name\"}}}},\
+                                   \"requested_predicates\":{{\"predicate1_uuid\":{{\"attr_name\":\"age\",\"p_type\":\"GE\",\"value\":18}}}}\
+                                }}", schema_seq_no);
 
     let err =
         sovrin_prover_get_claims_for_proof_req(prover_get_claims_for_proof_req_handle,
                                                wallet_handle,
-                                               CString::new(proof_req_json).unwrap().as_ptr(),
+                                               CString::new(proof_req_json.clone()).unwrap().as_ptr(),
                                                prover_get_claims_for_proof_req_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let (err, claims_json) = prover_get_claims_for_proof_req_receiver.recv().unwrap();
+    let (err, claims_json) = prover_get_claims_for_proof_req_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     println!("claims_json {:?}", claims_json);
     assert_eq!(ErrorCode::Success, err);
 
-    let requested_claims_json = "{\"self_attested_attributes\":{},\
-                                  \"requested_attrs\":{\"1\":[\"1\",true]},\
-                                  \"requested_predicates\":{\"1\":\"1\"}\
-                                }";
+    let requested_claims_json = format!("{{\
+                                          \"self_attested_attributes\":{{}},\
+                                          \"requested_attrs\":{{\"attr1_uuid\":[\"{}\",true]}},\
+                                          \"requested_predicates\":{{\"predicate1_uuid\":\"{}\"}}\
+                                        }}", claim_def_seq_no, claim_def_seq_no);
 
     let schemas_json = format!("{{\"{}\":{}}}", claim_def_seq_no, schema);
     let claim_defs_json = format!("{{\"{}\":{}}}", claim_def_seq_no, claim_def_json);
@@ -239,7 +249,7 @@ fn sovrin_anoncreds_demo() {
     let err =
         sovrin_prover_create_proof(prover_create_proof_handle,
                                    wallet_handle,
-                                   CString::new(proof_req_json).unwrap().as_ptr(),
+                                   CString::new(proof_req_json.clone()).unwrap().as_ptr(),
                                    CString::new(requested_claims_json).unwrap().as_ptr(),
                                    CString::new(schemas_json.clone()).unwrap().as_ptr(),
                                    CString::new(master_secret_name).unwrap().as_ptr(),
@@ -248,7 +258,7 @@ fn sovrin_anoncreds_demo() {
                                    prover_create_proof_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let (err, proof_json) = prover_create_proof_receiver.recv().unwrap();
+    let (err, proof_json) = prover_create_proof_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     println!("proof_json {:?}", proof_json);
     assert_eq!(ErrorCode::Success, err);
 
@@ -263,7 +273,9 @@ fn sovrin_anoncreds_demo() {
                                      verifier_verify_proof_callback);
 
     assert_eq!(ErrorCode::Success, err);
-    let (err, result) = verifier_verify_proof_receiver.recv().unwrap();
+    let (err, result) = verifier_verify_proof_receiver.recv_timeout(TimeoutUtils::long_timeout()).unwrap();
     assert_eq!(ErrorCode::Success, err);
     assert!(result);
+
+    TestUtils::cleanup_storage();
 }
