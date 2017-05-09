9a9afa59aaf07d127a8a635e521ebae946dcd023
Kirill Neznamov <kirill.neznamov@dsr-company.com>
2017-05-09 23:18:06 +0300

    fix compile errors

--- include/sovrin_mod.h
+++ include/sovrin_mod.h
@@ -1,7 +1,7 @@
 #ifndef __sovrin__mod_included__
 #define __sovrin__mod_included__
 
-typedef enum sovrin_error_t
+typedef enum
 {
     Success = 0,
     
@@ -117,7 +117,7 @@ typedef enum sovrin_error_t
     AnoncredsMasterSecretDuplicateNameError,
     
     ProofRejected
-}
+} sovrin_error_t;
 
 #endif
 
