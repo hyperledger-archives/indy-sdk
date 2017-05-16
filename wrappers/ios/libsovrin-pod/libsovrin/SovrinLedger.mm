f96a1c50409cb7bcb64977b7e35a7558f0a9886b
Kirill Neznamov <kirill.neznamov@dsr-company.com>
2017-05-16 11:14:20 +0300

    added wrapper for ledger api

---
+++ wrappers/ios/libsovrin-pod/libsovrin/SovrinLedger.mm
@@ -0,0 +1,14 @@
+//
+//  SovrinLedger.m
+//  libsovrin
+//
+//  Created by Kirill Neznamov on 16/05/2017.
+//  Copyright © 2017 Kirill Neznamov. All rights reserved.
+//
+
+#import "SovrinLedger.h"
+
+@implementation SovrinLedger
+
+
+@end
