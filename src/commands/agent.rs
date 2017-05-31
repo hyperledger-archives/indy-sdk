f710aaf4089812c40cffb7c2b9a7f3d29eb4423a
Sergey Minaev <sergey.minaev@dsr-company.com>
2017-05-31 17:18:43 +0300

    Implement AgentCommand::Connect command for case with all data in local wallet.

--- src/commands/agent.rs
+++ src/commands/agent.rs
@@ -1,9 +1,12 @@
 use std::rc::Rc;
 
 use errors::sovrin::SovrinError;
+use errors::common::CommonError;
 use services::agent::AgentService;
 use services::pool::PoolService;
+use services::signus::types::{MyDid, TheirDid};
 use services::wallet::WalletService;
+use utils::json::JsonDecodable;
 
 pub enum AgentCommand {
     Connect(
@@ -35,6 +38,32 @@ impl AgentCommandExecutor {
     }
 
     pub fn execute(&self, agent_cmd: AgentCommand) {
-        unimplemented!();
+        match agent_cmd {
+            AgentCommand::Connect(wallet_handle, sender_did, receiver_did, connect_cb, message_cb) => {
+                info!(target: "agent_command_executor", "Connect command received");
+                self.connect(wallet_handle, sender_did, receiver_did, connect_cb, message_cb)
+            }
+            _ => unimplemented!(),
+        }
+    }
+
+    fn connect(&self, wallet_handle: i32, sender_did: String, receiver_did: String,
+               connect_cb: Box<Fn(Result<i32, SovrinError>) + Send>,
+               message_cb: Box<Fn(Result<(i32, String), SovrinError>) + Send>,
+    ) {
+        self._connect(wallet_handle, sender_did, receiver_did)
+            .and_then(|_| self.agent_service.connect().map_err(From::from))
+            .unwrap_or_else(|err| connect_cb(Err(err)))
+    }
+
+    fn _connect(&self, wallet_handle: i32, sender_did: String, receiver_did: String)
+                -> Result<(), SovrinError> {
+        let my_did_json = self.wallet_service.get(wallet_handle, &format!("my_did::{}", sender_did))?;
+        let my_did: MyDid = MyDid::from_json(&my_did_json).map_err(|_| CommonError::InvalidState((format!("Invalid my did json"))))?;
+
+        let their_did_json = self.wallet_service.get_not_expired(wallet_handle, &format!("their_did::{}", receiver_did))?; //FIXME implement: get from ledger
+        let their_did: TheirDid = TheirDid::from_json(&their_did_json).map_err(|_| CommonError::InvalidState((format!("Invalid their did json"))))?;
+        assert!(their_did.endpoint.is_some()); //FIXME implement: get from ledger
+        Ok(())
     }
 }
--- src/services/agent.rs
+++ src/services/agent.rs
@@ -1,7 +1,13 @@
+use errors::common::CommonError;
+
 pub struct AgentService;
 
 impl AgentService {
     pub fn new() -> AgentService {
         AgentService {}
+    }
+
+    pub fn connect(&self) -> Result<(), CommonError> {
+        unimplemented!();
     }
 }
