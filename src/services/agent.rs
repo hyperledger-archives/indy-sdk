aa182db364fdfaefb79785f56ea7c4d026b23e17
Sergey Minaev <sergey.minaev@dsr-company.com>
2017-06-02 17:33:36 +0300

    Implement AgentWorker::poll.

--- src/services/agent.rs
+++ src/services/agent.rs
@@ -82,16 +82,17 @@ impl AgentService {
 
 impl AgentWorker {
     fn run(&mut self) {
-        loop {
+        'agent_pool_loop: loop {
             trace!("agent worker poll loop >>");
-            self.cmd_socket.poll(zmq::POLLIN, -1).unwrap();
-            let s = self.cmd_socket.recv_string(zmq::DONTWAIT).unwrap().unwrap();
-            let cmd = AgentWorkerCommand::from_json(s.as_str()).unwrap();
-            match cmd {
-                AgentWorkerCommand::Connect(cmd) => self.connect(&cmd).unwrap(),
-                AgentWorkerCommand::Exit => break,
+            let cmds = self.poll().unwrap();
+            for cmd in cmds {
+                info!("received cmd {:?}", cmd);
+                match cmd {
+                    AgentWorkerCommand::Connect(cmd) => self.connect(&cmd).unwrap(),
+                    AgentWorkerCommand::Response(resp) => self.agent_connections[resp.agent_ind].handle_response(resp.msg),
+                    AgentWorkerCommand::Exit => break 'agent_pool_loop,
+                }
             }
-            info!("received cmd {}", s);
             trace!("agent worker poll loop <<");
         }
         trace!("agent poll finished");
@@ -104,6 +105,37 @@ impl AgentWorker {
         ra.connect().map_err(map_err_trace!("RemoteAgent::connect failed"))?;
         self.agent_connections.push(ra);
         Ok(())
+    }
+
+    fn poll(&self) -> Result<Vec<AgentWorkerCommand>, zmq::Error> {
+        let mut result = Vec::new();
+        let mut poll_items: Vec<zmq::PollItem> = Vec::new();
+        poll_items.push(self.cmd_socket.as_poll_item(zmq::POLLIN));
+
+        for agent_conn in &self.agent_connections {
+            poll_items.push(agent_conn.socket.as_poll_item(zmq::POLLIN));
+        }
+
+        zmq::poll(poll_items.as_mut_slice(), -1).map_err(map_err_trace!("agent poll failed"))?;
+
+        if poll_items[0].is_readable() {
+            let msg = self.cmd_socket.recv_string(zmq::DONTWAIT).unwrap().unwrap();
+            info!("Input on cmd socket {}", msg);
+            result.push(AgentWorkerCommand::from_json(msg.as_str()).unwrap());
+        }
+
+        for i in 0..self.agent_connections.len() {
+            if poll_items[1 + i].is_readable() {
+                let msg = self.agent_connections[i].socket.recv_string(zmq::DONTWAIT).unwrap().unwrap();
+                info!("Input on agent socket {}: {}", i, msg);
+                result.push(AgentWorkerCommand::Response(Response {
+                    agent_ind: i,
+                    msg: msg,
+                }))
+            }
+        }
+
+        Ok(result)
     }
 }
 
@@ -136,9 +168,14 @@ impl RemoteAgent {
         self.socket.set_curve_serverkey(zmq::z85_encode(self.server_key.as_slice())?.as_str())
             .map_err(map_err_trace!())?;
         self.socket.set_linger(0).map_err(map_err_trace!())?; //TODO set correct timeout
-        self.socket.connect(self.addr.as_str()).map_err(map_err_trace!())?;
+        self.socket.connect(self.addr.as_str())
+            .map_err(map_err_trace!("RemoteAgent::connect self.socket.connect failed"))?;
         self.socket.send_str("DID", zmq::DONTWAIT).map_err(map_err_trace!())?;
         Ok(())
+    }
+
+    fn handle_response(&self, msg: String) {
+        unimplemented!();
     }
 }
 
@@ -146,6 +183,7 @@ impl RemoteAgent {
 #[derive(Serialize, Deserialize, Debug)]
 enum AgentWorkerCommand {
     Connect(ConnectCmd),
+    Response(Response),
     Exit,
 }
 
@@ -160,6 +198,12 @@ struct ConnectCmd {
     secret_key: String,
     public_key: String,
     server_key: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    agent_ind: usize,
+    msg: String,
 }
 
 fn _create_zmq_socket_pair(address: &str, connect_and_bind: bool) -> Result<(zmq::Socket, zmq::Socket), zmq::Error> {
@@ -251,6 +295,49 @@ mod tests {
         assert_eq!(agent_worker.agent_connections.len(), 1);
         recv_soc.recv_string(0).unwrap().unwrap(); //ignore identity
         assert_eq!(recv_soc.recv_string(zmq::DONTWAIT).unwrap().unwrap(), "DID");
+    }
+
+    #[test]
+    fn agent_worker_poll_works_for_cmd_socket() {
+        let (send_soc, recv_soc) = _create_zmq_socket_pair("aw_poll_cmd", true).unwrap();
+        let agent_worker = AgentWorker {
+            agent_connections: Vec::new(),
+            cmd_socket: recv_soc,
+        };
+        send_soc.send_str(r#"{"cmd": "Exit"}"#, zmq::DONTWAIT).unwrap();
+
+        let cmds = agent_worker.poll().unwrap();
+
+        assert_eq!(cmds.len(), 1);
+        assert_match!(AgentWorkerCommand::Exit, cmds[0]);
+    }
+
+    #[test]
+    fn agent_worker_poll_works_for_agent_socket() {
+        let (send_soc, recv_soc) = _create_zmq_socket_pair("aw_poll_cmd", true).unwrap();
+        let agent_worker = AgentWorker {
+            agent_connections: vec!(RemoteAgent {
+                socket: recv_soc,
+                addr: String::new(),
+                public_key: Vec::new(),
+                secret_key: Vec::new(),
+                server_key: Vec::new(),
+            }),
+            cmd_socket: zmq::Context::new().socket(zmq::SocketType::PAIR).unwrap(),
+        };
+        send_soc.send_str("msg", zmq::DONTWAIT).unwrap();
+
+        let mut cmds = agent_worker.poll().unwrap();
+
+        assert_eq!(cmds.len(), 1);
+        let cmd = cmds.remove(0);
+        match cmd {
+            AgentWorkerCommand::Response(resp) => {
+                assert_eq!(resp.agent_ind, 0);
+                assert_eq!(resp.msg, "msg");
+            }
+            _ => panic!("unexpected cmd {:?}", cmd),
+        }
     }
 
     #[test]
