e2e46b9895012543c87e1daafb8c3d7cef933820
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-company.com>
2017-07-05 15:47:02 +0300

    * Removed indeterministic behavior from test cases

--- src/services/wallet/plugged.rs
+++ src/services/wallet/plugged.rs
@@ -640,6 +640,7 @@ mod tests {
         wallet.set("key1::subkey2", "value2").unwrap();
 
         let mut key_values = wallet.list("key1::").unwrap();
+        key_values.sort();
         assert_eq!(2, key_values.len());
 
         let (key, value) = key_values.pop().unwrap();
