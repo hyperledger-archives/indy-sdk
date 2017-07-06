52340cde6523f7c849d0a98b935be3a27af87cf5
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-company.com>
2017-07-06 10:23:17 +0300

    * Added WalletAlreadyOpenedError error to c headers

--- include/sovrin_mod.h
+++ include/sovrin_mod.h
@@ -71,6 +71,9 @@ typedef enum
     // Trying to use wallet with pool that has different name
     WalletIncompatiblePoolError,
 
+    // Trying to open wallet that was opened already
+    WalletAlreadyOpenedError,
+
     // Ledger errors
     // Trying to open pool ledger that wasn't created before
     PoolLedgerNotCreatedError = 300,
