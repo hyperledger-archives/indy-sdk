d28f4c8eaf7e6111e05a638ffa353e3091789b8c
artem.ivanov <artem.ivanov@dsr-company.com>
2017-07-07 18:44:20 +0300

    debug script

--- ci/rpm-build.sh
+++ ci/rpm-build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 commit=$1
-version=$(wget -q https://raw.githubusercontent.com/hyperledger/indy-sdk/$commit/Cargo.toml -O - | grep -E '^version =' | head -n1 | cut -f2 -d= | tr -d '" ')
 
+echo commit
+echo 000
+version=$(wget -q https://raw.githubusercontent.com/hyperledger/indy-sdk/$commit/Cargo.toml -O - | grep -E '^version =' | head -n1 | cut -f2 -d= | tr -d '" ')
+echo 111
 [ -z $version ] && exit 1
 [ -z $commit ] && exit 2
-
+echo 222
 sed \
 	-e "s|@commit@|$commit|g" \
 	-e "s|@version@|$version.$commit|g" \
 	indy-sdk.spec.in >indy-sdk.spec
-
+echo 333
 spectool -g -R indy-sdk.spec || exit 3
+
+echo 444
 rpmbuild -ba indy-sdk.spec || exit 4
+
+555
