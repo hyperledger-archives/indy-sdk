c6dc7af780f3188544037ccb7b864c8a99264ed6
artem.ivanov <artem.ivanov@dsr-company.com>
2017-07-07 18:24:21 +0300

    test upload rpm script

---
+++ ci/upload-rpm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+if [ "$1" = "--help" ] ; then
+  echo "Usage: $0 <key>"
+fi
+
+key="$1"
+
+echo "Uploading...."
+
+cat <<EOF | sftp -i $key repo@54.187.56.182
+ls -l /var/repository/repos/rpm
+rm /var/repository/repos/rpm/*
+rmdir /var/repository/repos/rpm
+mkdir /var/repository/repos/rpm/
+cd /var/repository/repos/rpm/
+put $WORK_DIR/$POD_FILE_NAME
+put -r -r /usr/src/rpm/
+ls -l /var/repository/repos/rpm/
+EOF
