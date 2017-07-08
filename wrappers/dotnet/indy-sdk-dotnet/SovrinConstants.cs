523bfccc0e82fbfa974707a06cb574cc385ba882
Symon Rottem <s.rottem@gmail.com>
2017-07-08 23:28:07 +0200

    Fix missing code docs.

--- wrappers/dotnet/indy-sdk-dotnet/SovrinConstants.cs
+++ wrappers/dotnet/indy-sdk-dotnet/SovrinConstants.cs
@@ -7,17 +7,59 @@ namespace Indy.Sdk.Dotnet
     /// </summary>
     public static class SovrinConstants
     {
+        /// <summary>
+        /// Role for trustees.
+        /// </summary>
         public const String ROLE_TRUSTEE = "TRUSTEE";
-	    public const String ROLE_STEWARD = "STEWARD";
 
-	    public const String OP_NODE = "0";
-	    public const String OP_NYM = "1";
-	    public const String OP_ATTRIB = "100";
-	    public const String OP_SCHEMA = "101";
-	    public const String OP_CLAIM_DEF = "102";
-	    public const String OP_GET_ATTR = "104";
-	    public const String OP_GET_NYM = "105";
-	    public const String OP_GET_SCHEMA = "107";
-	    public const String OP_GET_CLAIM_DEF = "108";
+        /// <summary>
+        /// Role for stewards.
+        /// </summary>
+        public const String ROLE_STEWARD = "STEWARD";
+
+        /// <summary>
+        /// OP_NODE value.
+        /// </summary>
+        public const String OP_NODE = "0";
+
+        /// <summary>
+        /// OP_NYM value.
+        /// </summary>
+        public const String OP_NYM = "1";
+
+        /// <summary>
+        /// OP_ATTRIB value.
+        /// </summary>
+        public const String OP_ATTRIB = "100";
+
+        /// <summary>
+        /// OP_SCHEMA value.
+        /// </summary>
+        public const String OP_SCHEMA = "101";
+
+        /// <summary>
+        /// OP_CLAIM_DEF value.
+        /// </summary>
+        public const String OP_CLAIM_DEF = "102";
+
+        /// <summary>
+        /// OP_GET_ATTR value.
+        /// </summary>
+        public const String OP_GET_ATTR = "104";
+
+        /// <summary>
+        /// OP_GET_NYM value.
+        /// </summary>
+        public const String OP_GET_NYM = "105";
+
+        /// <summary>
+        /// OP_GET_SCHEMA value.
+        /// </summary>
+        public const String OP_GET_SCHEMA = "107";
+
+        /// <summary>
+        /// OP_GET_CLAIM_DEF value.
+        /// </summary>
+        public const String OP_GET_CLAIM_DEF = "108";
     }
 }
