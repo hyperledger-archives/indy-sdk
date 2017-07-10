818af39493f6bd640140efe31bbb10164b5f92a1
artem.ivanov <artem.ivanov@dsr-company.com>
2017-07-10 15:32:28 +0300

    try

--- ci/upload-rpm.sh
+++ ci/upload-rpm.sh
@@ -10,4 +10,9 @@ echo "Uploading...."
 
 cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
 ls -l /var/repository/repos
+rm /var/repository/repos/rpm/*
+rmdir /var/repository/repos/rpm
+mkdir /var/repository/repos/rpm/
+cd /var/repository/repos/rpm/
+ls -l /var/repository/repos/rpm/
 EOF
