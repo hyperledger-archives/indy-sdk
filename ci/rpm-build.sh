86bb4d022ca046f49d4ea83aad353d6565a41f21
artem.ivanov <artem.ivanov@dsr-company.com>
2017-07-11 09:28:03 +0300

    try

--- ci/rpm-build.sh
+++ ci/rpm-build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 if [ "$1" = "--help" ] ; then
-  echo "Usage: $0 <commit> $1 <file>"
+  echo "Usage: $0 <commit>"
 fi
 
 commit="$1"
-file="$2"
 
 mkdir -p /usr/src/rpm/SOURCES/
 
@@ -21,7 +20,7 @@ sed \
 	-e "s|@version@|$version.$commit|g" \
 	indy-sdk.spec.in >indy-sdk.spec
 
-chown root.root $file
+chown root.root indy-sdk.spec
 
 spectool -g -R indy-sdk.spec || exit 3
 rpmbuild -ba indy-sdk.spec || exit 4
