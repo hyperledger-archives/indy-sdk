fe1a2f2928012b56cc3625641da0fed54aecf4d9
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-07-18 15:38:06 +0300

    added test for sign and submit request

--- wrappers/python/tests/utils/wallet.py
+++ wrappers/python/tests/utils/wallet.py
@@ -1,9 +1,6 @@
 from indy import wallet
 
-async def create_and_open_wallet():
-    pool_name = "indy_open_wallet_works"
-    wallet_name = "indy_open_wallet_works"
-
+async def create_and_open_wallet(pool_name="indy_open_wallet_works", wallet_name="indy_open_wallet_works"):
     await wallet.create_wallet(pool_name, wallet_name, None, None, None)
     wallet_handle = await wallet.open_wallet(wallet_name, None, None)
     assert wallet_handle is not None
