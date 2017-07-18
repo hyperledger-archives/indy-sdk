f1fdea850be1e02efb794eba6767f54c13da734b
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-07-18 16:22:59 +0300

    added test_submit_request

--- wrappers/python/tests/ledger/test_sign_and_submit_request.py
+++ wrappers/python/tests/ledger/test_sign_and_submit_request.py
@@ -26,6 +26,7 @@ async def wallet_handle():
 async def test_sign_and_submit_request_works(wallet_handle):
     pool_handle = await pool.create_and_open_pool_ledger("pool_name")
     (my_did, _, _) = await signus.create_and_store_my_did(wallet_handle, '{"seed":"00000000000000000000000000000My1"}')
-    (trustee_did, _, _) = await signus.create_and_store_my_did(wallet_handle, '{"seed":"000000000000000000000000Trustee1"}')
+    (trustee_did, _, _) = await signus.create_and_store_my_did(wallet_handle,
+                                                               '{"seed":"000000000000000000000000Trustee1"}')
     nym_request = await ledger.build_nym_request(trustee_did.decode(), my_did.decode(), None, None, None)
     await ledger.sign_and_submit_request(pool_handle, wallet_handle, trustee_did.decode(), nym_request.decode())
---
+++ wrappers/python/tests/ledger/test_submit_request.py
@@ -0,0 +1,43 @@
+from tests.utils import pool, storage
+from indy import ledger
+
+import json
+import pytest
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@pytest.fixture(autouse=True)
+def before_after_each():
+    storage.cleanup()
+    yield
+    storage.cleanup()
+
+
+@pytest.mark.asyncio
+async def test_submit_request_works():
+    pool_handle = await pool.create_and_open_pool_ledger("pool_name")
+    request = {
+        "reqId": 1491566332010860,
+        "identifier": "Th7MpTaRZVRYnPiabds81Y",
+        "operation": {
+            "type": "105",
+            "dest": "Th7MpTaRZVRYnPiabds81Y"
+        },
+        "signature": "4o86XfkiJ4e2r3J6Ufoi17UU3W5Zi9sshV6FjBjkVw4sgEQFQov9dxqDEtLbAJAWffCWd5KfAk164QVo7mYwKkiV"
+    }
+
+    expected_response = {
+        "result": {
+            "reqId": 1491566332010860,
+            "identifier": "Th7MpTaRZVRYnPiabds81Y",
+            "dest": "Th7MpTaRZVRYnPiabds81Y",
+            "data": "{\"dest\":\"Th7MpTaRZVRYnPiabds81Y\",\"identifier\":\"V4SGRU86Z58d6TV7PBUe6f\",\"role\":\"2\""
+                    ",\"verkey\":\"~7TYfekw4GUagBnBVCqPjiC\"}",
+            "type": "105",
+        },
+        "op": "REPLY"
+    }
+    response = json.loads((await ledger.submit_request(pool_handle, json.dumps(request))).decode())
+    assert response == expected_response
