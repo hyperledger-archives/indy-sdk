1e3377311040c269ddffbebd844891a57ebb31e2
Symon Rottem <s.rottem@gmail.com>
2017-07-18 15:19:32 +0200

    Added some Signus tests.

---
+++ wrappers/dotnet/indy-sdk-dotnet-test/Wrapper/SignusTests/ReplaceKeysTest.cs
@@ -0,0 +1,77 @@
+﻿using Base58Check;
+using Indy.Sdk.Dotnet.Wrapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indy.Sdk.Dotnet.Test.Wrapper.SignusTests
+{
+    [TestClass]
+    public class ReplaceKeysTest : IndyIntegrationTest
+    {
+        private Wallet _wallet;
+
+        private string _did;
+        private string _verKey;
+
+        [TestInitialize]
+        public void CreateWalletWithDid()
+        {
+            Wallet.CreateWalletAsync("default", "signusWallet", "default", null, null).Wait();
+            _wallet = Wallet.OpenWalletAsync("signusWallet", null, null).Result;
+
+            var result = Signus.CreateAndStoreMyDidAsync(_wallet, "{}").Result;
+
+            _did = result.Did;
+            _verKey = result.VerKey;
+        }
+
+        [TestCleanup]
+        public void DeleteWallet()
+        {
+            _wallet.CloseAsync().Wait();
+            Wallet.DeleteWalletAsync("signusWallet", null).Wait();
+        }
+        
+        [TestMethod]
+        public void TestReplaceKeysWorksForEmptyJson()
+        {
+            var result = Signus.ReplaceKeysAsync(_wallet, _did, "{}").Result;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(32, Base58CheckEncoding.DecodePlain(result.VerKey).Length);
+        }
+
+        [TestMethod]
+        public async Task TestReplaceKeysWorksForInvalidDid()
+        {
+            var ex = await Assert.ThrowsExceptionAsync<IndyException>(() =>
+                Signus.ReplaceKeysAsync(_wallet, "invalid_base58_string", "{}")
+            );
+
+            Assert.AreEqual(ErrorCode.CommonInvalidStructure, ex.ErrorCode);
+        }
+
+        [TestMethod]
+        public void TestReplaceKeysWorksForNotExistsDid()
+        {
+            var result = Signus.ReplaceKeysAsync(_wallet, "8wZcEriaNLNKtteJvx7f8i", "{}").Result;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestReplaceKeysWorksForSeed()
+        {
+            var result = Signus.ReplaceKeysAsync(_wallet, _did, "{\"seed\":\"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"}").Result;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("CnEDk9HrMnmiHXEV1WFgbVCRteYnPqsJwrTdcZaNhFVW", result.VerKey);
+            Assert.AreNotEqual(_verKey, result.VerKey);
+        }
+
+    }
+}
---
+++ wrappers/dotnet/indy-sdk-dotnet-test/Wrapper/SignusTests/SignTest.cs
@@ -0,0 +1,81 @@
+﻿using Base58Check;
+using Indy.Sdk.Dotnet.Wrapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indy.Sdk.Dotnet.Test.Wrapper.SignusTests
+{
+    [TestClass]
+    public class SignTest : IndyIntegrationTest
+    {
+        private Wallet _wallet;
+
+        private string _did;
+
+        [TestInitialize]
+        public void CreateWalletWithDid()
+        {
+            Wallet.CreateWalletAsync("default", "signusWallet", "default", null, null).Wait();
+            _wallet = Wallet.OpenWalletAsync("signusWallet", null, null).Result;
+
+            var result = Signus.CreateAndStoreMyDidAsync(_wallet, "{\"seed\":\"000000000000000000000000Trustee1\"}").Result;
+
+            _did = result.Did;
+        }
+
+        [TestCleanup]
+        public void DeleteWallet()
+        {
+            _wallet.CloseAsync().Wait();
+            Wallet.DeleteWalletAsync("signusWallet", null).Wait();
+        }
+        
+        [TestMethod]
+        public void TestSignWorks()
+        {
+            var msg = "{\n" +
+                "                \"reqId\":1496822211362017764,\n" +
+                "                \"identifier\":\"GJ1SzoWzavQYfNL9XkaJdrQejfztN4XqdsiV4ct3LXKL\",\n" +
+                "                \"operation\":{\n" +
+                "                    \"type\":\"1\",\n" +
+                "                    \"dest\":\"VsKV7grR1BUE29mG2Fm2kX\",\n" +
+                "                    \"verkey\":\"GjZWsBLgZCR18aL468JAT7w9CZRiBnpxUPPgyQxh4voa\"\n" +
+                "                }\n" +
+                "            }";
+
+            var expectedSignature = "\"signature\":\"65hzs4nsdQsTUqLCLy2qisbKLfwYKZSWoyh1C6CU59p5pfG3EHQXGAsjW4Qw4QdwkrvjSgQuyv8qyABcXRBznFKW\"";
+
+            var signedMessage = Signus.SignAsync(_wallet, _did, msg).Result;
+
+            Assert.IsTrue(signedMessage.Contains(expectedSignature));
+        }
+
+        [TestMethod]
+        public async Task TestSignWorksForUnknownDid()
+        {
+            var msg = "{\"reqId\":1496822211362017764}";
+
+            var ex = await Assert.ThrowsExceptionAsync<IndyException>(() =>
+                Signus.SignAsync(_wallet, "8wZcEriaNLNKtteJvx7f8i", msg)
+            );
+
+            Assert.AreEqual(ErrorCode.WalletNotFoundError, ex.ErrorCode);
+        }
+
+        [TestMethod]
+        public async Task TestSignWorksForInvalidMessageFormat()
+        {
+            var msg = "reqId:1495034346617224651";
+
+            var ex = await Assert.ThrowsExceptionAsync<IndyException>(() =>
+                Signus.SignAsync(_wallet, _did, msg)
+            );
+
+            Assert.AreEqual(ErrorCode.CommonInvalidStructure, ex.ErrorCode);
+        }
+    }
+}
