1188147f65fb480bdca739ab7d18cd565df46812
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-07-19 14:17:54 +0300

    added tests for sign_and_submit

--- wrappers/python/tests/ledger/test_sign_and_submit_request.py
+++ wrappers/python/tests/ledger/test_sign_and_submit_request.py
@@ -2,6 +2,7 @@ from tests.utils import pool, storage
 from tests.utils.wallet import create_and_open_wallet
 from indy import wallet, signus, ledger
 from indy.pool import close_pool_ledger
+from indy.error import ErrorCode, IndyError
 import pytest
 import logging
 
@@ -36,3 +37,55 @@ async def test_sign_and_submit_request_works(wallet_handle, pool_handle):
                                                                '{"seed":"000000000000000000000000Trustee1"}')
     nym_request = await ledger.build_nym_request(trustee_did.decode(), my_did.decode(), None, None, None)
     await ledger.sign_and_submit_request(pool_handle, wallet_handle, trustee_did.decode(), nym_request.decode())
+
+
+@pytest.mark.asyncio
+async def test_sign_and_submit_request_works_for_invalid_pool_handle(wallet_handle, pool_handle):
+    (my_did, _, _) = await signus.create_and_store_my_did(wallet_handle, '{"seed":"00000000000000000000000000000My1"}')
+    (trustee_did, _, _) = await signus.create_and_store_my_did(wallet_handle,
+                                                               '{"seed":"000000000000000000000000Trustee1"}')
+    nym_request = await ledger.build_nym_request(trustee_did.decode(), my_did.decode(), None, None, None)
+    invalid_pool_handle = pool_handle + 1
+
+    try:
+        await ledger.sign_and_submit_request(invalid_pool_handle, wallet_handle, trustee_did.decode(),
+                                             nym_request.decode())
+        raise Exception("Failed")
+    except Exception as e:
+        assert type(IndyError(ErrorCode.PoolLedgerInvalidPoolHandle)) == type(e) and \
+               IndyError(ErrorCode.PoolLedgerInvalidPoolHandle).args == e.args
+
+
+@pytest.mark.asyncio
+async def test_sign_and_submit_request_works_for_invalid_wallet_handle(wallet_handle, pool_handle):
+    (my_did, _, _) = await signus.create_and_store_my_did(wallet_handle, '{"seed":"00000000000000000000000000000My1"}')
+    (trustee_did, _, _) = await signus.create_and_store_my_did(wallet_handle,
+                                                               '{"seed":"000000000000000000000000Trustee1"}')
+    nym_request = await ledger.build_nym_request(trustee_did.decode(), my_did.decode(), None, None, None)
+    invalid_wallet_handle = wallet_handle + 1
+
+    try:
+        await ledger.sign_and_submit_request(pool_handle, invalid_wallet_handle, trustee_did.decode(),
+                                             nym_request.decode())
+        raise Exception("Failed")
+    except Exception as e:
+        assert type(IndyError(ErrorCode.WalletInvalidHandle)) == type(e) and \
+               IndyError(ErrorCode.WalletInvalidHandle).args == e.args
+
+
+@pytest.mark.asyncio
+async def test_sign_and_submit_request_works_for_incompatible_wallet_and_pool(pool_handle):
+    wallet_handle = await create_and_open_wallet(pool_name="pool_2")
+    (my_did, _, _) = await signus.create_and_store_my_did(wallet_handle, '{"seed":"00000000000000000000000000000My1"}')
+    (trustee_did, _, _) = await signus.create_and_store_my_did(wallet_handle,
+                                                               '{"seed":"000000000000000000000000Trustee1"}')
+    nym_request = await ledger.build_nym_request(trustee_did.decode(), my_did.decode(), None, None, None)
+
+    try:
+        await ledger.sign_and_submit_request(pool_handle, wallet_handle, trustee_did.decode(),
+                                             nym_request.decode())
+        raise Exception("Failed")
+    except Exception as e:
+        assert type(IndyError(ErrorCode.WalletIncompatiblePoolError)) == type(e) and \
+               IndyError(ErrorCode.WalletIncompatiblePoolError).args == e.args
+    await wallet.close_wallet(wallet_handle)
