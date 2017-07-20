c48df497423e4ab1ee9771adf90b137299aa8a9a
artem.ivanov <artem.ivanov@dsr-company.com>
2017-07-20 08:17:22 +0300

    try

--- ci/upload-rpm.sh
+++ ci/upload-rpm.sh
@@ -7,10 +7,5 @@ fi
 key="$1"
 
 cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
-ls -l /var/repository/repos
-rm /var/repository/repos/rpm/*
-cd /var/repository/repos/rpm/
-put -r /usr/src/rpm/RPMS/
-put -r /usr/src/rpm/SRPMS/
-ls -l /var/repository/repos/rpm/
+rm -rf /var/repository/repos/rpm/*
 EOF
