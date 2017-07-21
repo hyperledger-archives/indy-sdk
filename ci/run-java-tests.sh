d90cac67abaad361cfed400b0cb7c868f109dfb6
artem.ivanov <artem.ivanov@dsr-company.com>
2017-07-21 15:39:28 +0300

    Worked

--- ci/run-java-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Use this script because we can not change directory after docker.inside { }
-# https://issues.jenkins-ci.org/browse/JENKINS-35518
-
-cd wrappers/java
-
-mvn clean test
