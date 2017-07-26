f6f30a9e13ceebdd490b23708b12dd847c4306be
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-07-26 11:59:33 +0300

    added test for delete

--- wrappers/python/tests/pool/test_delete_pool_ledger_config.py
+++ wrappers/python/tests/pool/test_delete_pool_ledger_config.py
@@ -1,20 +1,25 @@
-from tests.utils import pool, storage
+from tests.utils import pool
 from indy.pool import delete_pool_ledger_config
+from indy.error import ErrorCode, IndyError
+
 import pytest
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-@pytest.fixture(autouse=True)
-def before_after_each():
-    storage.cleanup()
-    yield
-    storage.cleanup()
-
-
 @pytest.mark.asyncio
-async def test_delete_pool_ledger_config_works():
+async def test_delete_pool_ledger_config_works(cleanup_storage):
     name = "pool_name"
     await pool.create_pool_ledger_config(name)
     await delete_pool_ledger_config(name)
+
+
+@pytest.mark.asyncio
+async def test_delete_pool_ledger_config_works_for_opened(cleanup_storage):
+    name = "pool_name"
+    await pool.create_and_open_pool_ledger(name)
+    with pytest.raises(IndyError) as e:
+        await delete_pool_ledger_config(name)
+    assert ErrorCode.CommonInvalidState == e.value.error_code
+
