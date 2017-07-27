98830f781972a21da3cb6fdb7574b6a7e80cc86e
artem.ivanov <artem.ivanov@dsr-company.com>
2017-07-27 10:59:34 +0300

    Upload pod file

--- build-libindy-core-ios.sh
+++ build-libindy-core-ios.sh
@@ -1,48 +1,16 @@
-#!/bin/sh 
+#!/bin/bash
 
-export PKG_CONFIG_ALLOW_CROSS=1
-export OPENSSL_DIR=/usr/local/Cellar/openssl/1.0.2k
-export EVERNYM_REPO_KEY=~/Documents/EvernymRepo
-export LIBINDY_POD_VERSION=0.0.3
-export POD_FILE_NAME=libindy-core-ios.tar.gz
-
-echo "\nBuild IOS POD started..."
-cargo lipo
-echo 'Build completed successfully.'
-
-WORK_DIR=`mktemp -d`
-
-echo "Try to create temporary directory: $WORK_DIR"
-
-if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
-  echo "Could not create temp dir $WORK_DIR"
-  exit 1
+if [ "$1" = "--help" ] ; then
+  echo "Usage: $0 <key>"
 fi
 
-echo "Packing...\n\n"
+key="$1"
 
-cp include/*.h $WORK_DIR
-cp target/universal/debug/libindy.a $WORK_DIR
-CUR_DIR=`pwd`
-cd $WORK_DIR
-tar -cvzf $POD_FILE_NAME *
-ls -l $WORK_DIR/$POD_FILE_NAME
+[ -z $key ] && exit 1
 
-echo "\nPacking completed."
-cd $CUR_DIR
 
-echo "Uploading...."
-
-cat <<EOF | sftp -i $EVERNYM_REPO_KEY repo@54.187.56.182
-ls -l /var/repositories/deb/pods-ios/libindy-core/$LIBINDY_POD_VERSION/$POD_FILE_NAME
-rm /var/repositories/deb/pods-ios/libindy-core/$LIBINDY_POD_VERSION/$POD_FILE_NAME
-rmdir /var/repositories/deb/pods-ios/libindy-core/$LIBINDY_POD_VERSION
-mkdir /var/repositories/deb/pods-ios/libindy-core/$LIBINDY_POD_VERSION
-cd /var/repositories/deb/pods-ios/libindy-core/$LIBINDY_POD_VERSION
-put $WORK_DIR/$POD_FILE_NAME
-ls -l /var/repositories/deb/pods-ios/libindy-core/$LIBINDY_POD_VERSION
+cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
+rm /var/repository/repos/deb/pods-ios/libindy-core/0.1.1/libindy-core-ios.tar.gz
+cd /var/repository/repos/deb/pods-ios/libindy-core/0.1.1
+put /var/lib/jenkins/workspace/indy/libindy-core-ios.tar.gz
 EOF
-
-echo "Cleanup temporary directory: $WORK_DIR"
-rm -rf "$WORK_DIR"
-
