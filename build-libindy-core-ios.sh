a3d512049dc88a40afcb8b0f8e3da44c802f2b98
artem.ivanov <artem.ivanov@dsr-company.com>
2017-07-27 11:02:10 +0300

    try

--- build-libindy-core-ios.sh
+++ build-libindy-core-ios.sh
@@ -10,7 +10,8 @@ key="$1"
 
 
 cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
+ls
 rm /var/repository/repos/deb/pods-ios/libindy-core/0.1.1/libindy-core-ios.tar.gz
 cd /var/repository/repos/deb/pods-ios/libindy-core/0.1.1
-put /var/lib/jenkins/workspace/indy/libindy-core-ios.tar.gz
+put /var/lib/jenkins/workspace/libindy-core-ios.tar.gz
 EOF
