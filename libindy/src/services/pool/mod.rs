485e67a8fbe679e6b3981131436a8e5c96d3b6d8
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-company.com>
2017-08-03 16:06:33 +0300

    * Added workaround for races in Pool::drop(). The problem requires better investigation, but it allows us to move forward.

--- libindy/src/services/pool/mod.rs
+++ libindy/src/services/pool/mod.rs
@@ -485,7 +485,12 @@ impl Drop for Pool {
     fn drop(&mut self) {
         let target = format!("pool{}", self.name);
         info!(target: target.as_str(), "Drop started");
-        self.cmd_sock.send("exit".as_bytes(), zmq::DONTWAIT).expect("send exit command"); //TODO
+
+        if let Err(e) = self.cmd_sock.send("exit".as_bytes(), zmq::DONTWAIT) {
+            //TODO
+            error!("Send exit command failed, err {}", e);
+        }
+
         info!(target: target.as_str(), "Drop wait worker");
         // Option worker type and this kludge is workaround for rust
         if let Some(worker) = self.worker.take() {
