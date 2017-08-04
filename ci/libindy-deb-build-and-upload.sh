6635f7273d2792ce7164233678705b06f6ee4807
Sergey Minaev <sergey.minaev@dsr-company.com>
2017-08-04 18:25:21 +0300

    Change source for uploading to relative paths.

--- ci/libindy-deb-build-and-upload.sh
+++ ci/libindy-deb-build-and-upload.sh
@@ -20,7 +20,7 @@ cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
 mkdir /var/repository/repos/deb/indy-sdk
 mkdir /var/repository/repos/deb/indy-sdk/$version-$number
 cd /var/repository/repos/deb/indy-sdk/$version-$number
-put -r /var/lib/jenkins/workspace/indy-sdk-dev_"$version"_amd64.deb
-put -r /var/lib/jenkins/workspace/indy-sdk_"$version"_amd64.deb
+put -r ../indy-sdk-dev_"$version"_amd64.deb
+put -r ../indy-sdk_"$version"_amd64.deb
 ls -l /var/repository/repos/deb/indy-sdk/$version-$number
 EOF
