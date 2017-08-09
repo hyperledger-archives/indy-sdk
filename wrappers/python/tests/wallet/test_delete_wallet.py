1931a637596d4c276291eb24a72f86968d86e4c1
artem.ivanov <artem.ivanov@dsr-company.com>
2017-08-09 15:33:32 +0300

    Fixed merge error

--- wrappers/python/tests/wallet/test_delete_wallet.py
+++ wrappers/python/tests/wallet/test_delete_wallet.py
@@ -1,7 +1,8 @@
+import pytest
+
 from indy import IndyError
 from indy import wallet
 from indy.error import ErrorCode
->>>>>>> master
 
 
 # noinspection PyUnusedLocal
@@ -18,15 +19,10 @@ async def test_delete_wallet_works_for_closed(wallet_name, wallet_handle):
     await wallet.delete_wallet(wallet_name, None)
 
 
-<<<<<<< HEAD
-@pytest.mark.skip(reason="There is BUG in indy_sdk")
-async def test_delete_wallet_works_for_opened(cleanup_storage):
-=======
 # noinspection PyUnusedLocal
 @pytest.mark.skip(reason="TODO: FIXME: Create a bug!!!")
 @pytest.mark.asyncio
 async def test_delete_wallet_works_for_opened(wallet_name, wallet_handle):
->>>>>>> master
     with pytest.raises(IndyError) as e:
         await wallet.delete_wallet(wallet_name, None)
 
