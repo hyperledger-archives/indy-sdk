b26b099db9528daa6e65b8bab970820c3d488a53
Sergey Minaev <sergey.minaev@dsr-company.com>
2017-08-11 18:01:50 +0300

    Draft implementation for publishing windows artifacts.

---
+++ ci/libindy-win-zip-and-upload.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$1" = "--help" ] ; then
+  echo "Usage: $0 <commit> $1 <key> $2 <number>"
+fi
+
+commit="$1"
+key="$2"
+number="$3"
+
+version=$(wget -q https://raw.githubusercontent.com/hyperledger/indy-sdk/$commit/libindy/Cargo.toml -O - | grep -E '^version =' | head -n1 | cut -f2 -d= | tr -d '" ')
+
+[ -z $version ] && exit 1
+[ -z $commit ] && exit 2
+[ -z $key ] && exit 3
+
+mkdir indy-sdk-zip
+cd indy-sdk-zip
+cp -r libindy/include .
+mkdir lib
+cp libindy/target/release/*.dll lib/
+zip ../indy-sdk_"$version".zip ./include/* ./lib/*
+cd ..
+rm -rf ./indy-sdk-zip
+
+cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
+mkdir /var/repository/repos/deb/windows-bins/
+mkdir /var/repository/repos/deb/windows-bins/indy-sdk
+mkdir /var/repository/repos/deb/windows-bins/indy-sdk/$version-$number
+cd /var/repository/repos/deb/windows-bins/indy-sdk/$version-$number
+put -r indy-sdk_"$version".zip
+ls -l /var/repository/repos/deb/windows-bins/indy-sdk/$version-$number
+EOF
