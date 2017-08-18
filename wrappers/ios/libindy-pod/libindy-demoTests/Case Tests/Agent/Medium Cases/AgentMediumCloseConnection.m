9e9d16084ff5637735afa68a3b80fd9ff94612e3
Anastasia Tarasova <stasytar@gmail.com>
2017-08-18 14:39:23 +0300

    iOS: Added agent listen medium cases tests

---
+++ wrappers/ios/libindy-pod/libindy-demoTests/Case Tests/Agent/Medium Cases/AgentMediumCloseConnection.m
@@ -0,0 +1,9 @@
+//
+//  AgentMediumCloseConnection.m
+//  libindy-demo
+//
+//  Created by Anastasia Tarasova on 18/08/2017.
+//  Copyright © 2017 Kirill Neznamov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
