f9b5dfa1bd567c11b9c789000990b482f8d9fd9f
Sergey Minaev <sergey.minaev@dsr-company.com>
2017-08-30 18:19:22 +0300

    Fix naming of win artifact.

--- libindy/ci/libindy-win-zip-and-upload.sh
+++ libindy/ci/libindy-win-zip-and-upload.sh
@@ -19,12 +19,12 @@ mkdir libindy-zip
 mkdir libindy-zip/lib
 cp -r ./include ./libindy-zip
 cp ./target/release/*.dll ./libindy-zip/lib/
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('libindy-zip', 'libindy-$version.zip'); }"
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('libindy-zip', 'libindy_$version.zip'); }"
 rm -rf ./libindy-zip
 
 cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
 mkdir /var/repository/repos/libindy/windows/$type/$version-$number
 cd /var/repository/repos/libindy/windows/$type/$version-$number
-put -r libindy-"$version".zip
+put -r libindy_"$version".zip
 ls -l /var/repository/repos/libindy/windows/$type/$version-$number
 EOF
