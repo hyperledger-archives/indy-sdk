257d84e735106232e24cee0aeebaee49950ff70e
artem.ivanov <artem.ivanov@dsr-company.com>
2017-09-19 11:48:17 +0300

    Fixed ledger tests

--- libindy/src/services/ledger/mod.rs
+++ libindy/src/services/ledger/mod.rs
@@ -244,7 +244,7 @@ mod tests {
         let identifier = "identifier";
         let dest = "dest";
 
-        let expected_result = r#""identifier":"identifier","operation":{"type":"1","dest":"dest",role:null}"#;
+        let expected_result = r#""identifier":"identifier","operation":{"type":"1","dest":"dest","role":null}"#;
 
         let nym_request = ledger_service.build_nym_request(identifier, dest, None, None, None);
         assert!(nym_request.is_ok());
@@ -272,7 +272,7 @@ mod tests {
         let verkey = "verkey";
         let alias = "some_alias";
 
-        let expected_result = r#""identifier":"identifier","operation":{"type":"1","dest":"dest","verkey":"verkey","alias":"some_alias"}"#;
+        let expected_result = r#""identifier":"identifier","operation":{"type":"1","dest":"dest","verkey":"verkey","alias":"some_alias","role":null}"#;
 
         let nym_request = ledger_service.build_nym_request(identifier, dest, Some(verkey), Some(alias), None);
         assert!(nym_request.is_ok());
