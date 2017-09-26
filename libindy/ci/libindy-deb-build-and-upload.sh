884acd4de4c108cc967d7a01042d69af485c5df2
artem.ivanov <artem.ivanov@dsr-company.com>
2017-09-26 08:59:06 +0300

    Added script for Apt publishing

--- libindy/ci/libindy-deb-build-and-upload.sh
+++ libindy/ci/libindy-deb-build-and-upload.sh
@@ -15,14 +15,16 @@ number="$4"
 [ -z $type ] && exit 3
 [ -z $number ] && exit 4
 
+sed -i -E -e 'H;1h;$!d;x' -e "s/libindy ([(,),0-9,.]+)/libindy ($version-$number)/" debian/changelog
+
 dpkg-buildpackage -tc
 
-cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
-mkdir /var/repository/repos/libindy/ubuntu/$type/$version-$number
-cd /var/repository/repos/libindy/ubuntu/$type/$version-$number
-put -r ../libindy-dev_"$version"_amd64.deb
-put -r ../libindy_"$version"_amd64.deb
-ls -l /var/repository/repos/libindy/ubuntu/$type/$version-$number
-EOF
+#cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
+#mkdir /var/repository/repos/libindy/ubuntu/$type/$version-$number
+#cd /var/repository/repos/libindy/ubuntu/$type/$version-$number
+#put -r ../libindy-dev_"$version"-"$number"_amd64.deb
+#put -r ../libindy_"$version"-"$number"_amd64.deb
+#ls -l /var/repository/repos/libindy/ubuntu/$type/$version-$number
+#EOF
 
 ./sovrin-packaging/upload_debs.py ../ $type
