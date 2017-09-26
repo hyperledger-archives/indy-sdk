370ecbb8ca4cc7f81226df7cdb63e81e33cfefa5
artem.ivanov <artem.ivanov@dsr-company.com>
2017-09-26 09:18:23 +0300

    Uncomment block

--- libindy/ci/libindy-deb-build-and-upload.sh
+++ libindy/ci/libindy-deb-build-and-upload.sh
@@ -19,12 +19,12 @@ sed -i -E -e 'H;1h;$!d;x' -e "s/libindy ([(,),0-9,.]+)/libindy ($version-$number
 
 dpkg-buildpackage -tc
 
-#cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
-#mkdir /var/repository/repos/libindy/ubuntu/$type/$version-$number
-#cd /var/repository/repos/libindy/ubuntu/$type/$version-$number
-#put -r ../libindy-dev_"$version"-"$number"_amd64.deb
-#put -r ../libindy_"$version"-"$number"_amd64.deb
-#ls -l /var/repository/repos/libindy/ubuntu/$type/$version-$number
-#EOF
+cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
+mkdir /var/repository/repos/libindy/ubuntu/$type/$version-$number
+cd /var/repository/repos/libindy/ubuntu/$type/$version-$number
+put -r ../libindy-dev_"$version"-"$number"_amd64.deb
+put -r ../libindy_"$version"-"$number"_amd64.deb
+ls -l /var/repository/repos/libindy/ubuntu/$type/$version-$number
+EOF
 
 ./sovrin-packaging/upload_debs.py ../ $type
