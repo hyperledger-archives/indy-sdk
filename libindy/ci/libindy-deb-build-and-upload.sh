01d1f7648351330c8c4c09d2bc9bb768ce630af9
artem.ivanov <artem.ivanov@dsr-company.com>
2017-09-26 16:58:14 +0300

    Updated duild script

--- libindy/ci/libindy-deb-build-and-upload.sh
+++ libindy/ci/libindy-deb-build-and-upload.sh
@@ -6,25 +6,19 @@ if [ "$1" = "--help" ] ; then
 fi
 
 version="$1"
-key="$2"
-type="$3"
-number="$4"
+type="$2"
+number="$3"
 
 [ -z $version ] && exit 1
-[ -z $key ] && exit 2
-[ -z $type ] && exit 3
-[ -z $number ] && exit 4
+[ -z $type ] && exit 2
+[ -z $number ] && exit 3
 
-sed -i -E -e 'H;1h;$!d;x' -e "s/libindy ([(,),0-9,.]+)/libindy ($version-$number)/" debian/changelog
+sed -i -E -e 'H;1h;$!d;x' -e "s/libindy ([(,),0-9,.]+)/libindy ($version)/" debian/changelog
 
 dpkg-buildpackage -tc
 
-cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
-mkdir /var/repository/repos/libindy/ubuntu/$type/$version-$number
-cd /var/repository/repos/libindy/ubuntu/$type/$version-$number
-put -r ../libindy-dev_"$version"-"$number"_amd64.deb
-put -r ../libindy_"$version"-"$number"_amd64.deb
-ls -l /var/repository/repos/libindy/ubuntu/$type/$version-$number
-EOF
+rename -v "s/$version/$version-$number/" ../*.deb
 
-./sovrin-packaging/upload_debs.py ../ $type
+mkdir debs &&  mv ../*.deb ./debs/
+
+./sovrin-packaging/upload_debs.py ./debs $type
