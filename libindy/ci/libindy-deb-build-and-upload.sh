261242b55f6891083d1c608c3198a5987dd36e70
artem.ivanov <artem.ivanov@dsr-company.com>
2017-09-29 09:42:22 +0300

    Rebuild deb files on Rc publishing step

--- libindy/ci/libindy-deb-build-and-upload.sh
+++ libindy/ci/libindy-deb-build-and-upload.sh
@@ -7,17 +7,15 @@ fi
 
 version="$1"
 type="$2"
-number="$3"
+suffix="$3"
 
 [ -z $version ] && exit 1
 [ -z $type ] && exit 2
-[ -z $number ] && exit 3
+[ -z $suffix ] && exit 3
 
-sed -i -E -e 'H;1h;$!d;x' -e "s/libindy ([(,),0-9,.]+)/libindy ($version)/" debian/changelog
+sed -i -E -e 'H;1h;$!d;x' -e "s/libindy ([(,),0-9,.]+)/libindy ($version$suffix)/" debian/changelog
 
 dpkg-buildpackage -tc
-
-rename -v "s/$version/$version-$number/" ../*.deb
 
 mkdir debs &&  mv ../*.deb ./debs/
 
