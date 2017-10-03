5e1217cae843775d96b9927c63dac58c9d8192a3
ryMarsh44 <ryan.marsh44@gmail.com>
2017-10-03 14:59:46 -0600

    Node wrapper bindings (#8)
    * created basic structure for node wrapper
    
    * Added unit test that called cxs_init
    
    * Fixed Jenkins file to look in correct place for .so file
    
    * Updated the default path to libcxs.so
    
    * Changed the default location for the .so file. The application
    will search in its own node_modules folder for the installed libcxs
    module.
    
    Signed-off-by: ryMarsh44 <ryan.marsh44@gmail.com>

---
+++ cxs/wrappers/node/src/index.ts
@@ -0,0 +1,45 @@
+import { FFIEntryPoint,FFIConfiguration, FFIInterfaceConfig } from './rustlib'
+import * as ffi from 'ffi'
+import * as path from 'path'
+
+export interface CXSAcessType {
+    readonly ffi: FFIEntryPoint
+}
+
+export class CXSRuntimeConfig {
+    basepath?:string
+    constructor(_basepath?: string){
+        this.basepath = _basepath
+    }
+
+}
+
+/** CXSRuntime is the object that interfaces with the cxs sdk functions **/
+/** FFIConfiguration will contain all the sdk api functions **/
+/** CXSRuntimeConfg is a class that currently only contains a chosen basepath for the .so file
+ * I made it a class just in case we think of more needed configs **/
+
+export class CXSRuntime implements CXSAcessType {
+    readonly basepath: string
+    readonly ffi: FFIEntryPoint
+
+    constructor(config?: CXSRuntimeConfig) {
+        config = config || {}
+
+        function _initialize_basepath(): string {
+            // this needs additional logic
+            let basepath = config.basepath
+
+            if (basepath === undefined) {
+                //This basepath is in the local/appSpecific node_modules
+                basepath = path.resolve("../node_modules/cxs/lib/libcxs.so");
+            }
+
+            return basepath
+        }
+
+        // initialize FFI
+        const libraryPath = _initialize_basepath()
+        this.ffi = ffi.Library(libraryPath, FFIConfiguration)
+    }
+}
