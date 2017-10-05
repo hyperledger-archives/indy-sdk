ea0e201a3e9911f78f46a4cc89ef99a1a4d6d60c
Mark Hadley <hadleym@email.arizona.edu>
2017-10-05 13:06:00 -0600

    Modified path
    Signed-off-by: Mark Hadley <hadleym@email.arizona.edu>

--- cxs/wrappers/node/src/api/init.js
+++ cxs/wrappers/node/src/api/init.js
@@ -5,7 +5,7 @@ var parentDir = require('path');
 
 function init_cxs (poolName, configName, walletName, walletType) {
     var currentDir = parentDir.dirname(module.filename)
-    var path = '/home/mark/dev/sdk/cxs/wrappers/node/lib/libcxs.so'
+    var path = '../../lib/libcxs.so'
     var config = new index.CXSRuntimeConfig(path)
     var rust = new index.CXSRuntime(config)
     return rust.ffi.cxs_init(poolName, configName,walletName,walletType)
