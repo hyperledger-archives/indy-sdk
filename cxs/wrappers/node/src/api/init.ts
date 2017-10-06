166d9b202d91a1beb6ca2e9400dc74f2666fde90
Mark Hadley <hadleym@email.arizona.edu>
2017-10-06 14:16:39 -0600

    Updated where libcxs.so is located
    Signed-off-by: Mark Hadley <hadleym@email.arizona.edu>

--- cxs/wrappers/node/src/api/init.ts
+++ cxs/wrappers/node/src/api/init.ts
@@ -3,9 +3,7 @@ import { CXSRuntime } from '../index'
 import { CXSRuntimeConfig } from '../rustlib'
 
 export function init_cxs (filename) {
-  const currentDir = posix.dirname('.')
-  const path = currentDir + '/../node/lib/libcxs.so'
-  const config = new CXSRuntimeConfig(path)
+  const config = new CXSRuntimeConfig(null)
   const rust = new CXSRuntime(config)
   return rust.ffi.cxs_init(filename)
 }
--- cxs/wrappers/node/src/index.ts
+++ cxs/wrappers/node/src/index.ts
@@ -20,9 +20,9 @@ export class CXSRuntime implements ICXSAcessType {
     function _initialize_basepath (): string {
       let basepath = config.basepath
 
-      if (basepath === undefined) {
+      if (basepath === undefined || basepath == null) {
       // This basepath is in the local/appSpecific node_modules
-        basepath = path.resolve('../node_modules/cxs/lib/libcxs.so')
+        basepath = path.resolve('./lib/libcxs.so')
       }
 
       return basepath
@@ -33,3 +33,4 @@ export class CXSRuntime implements ICXSAcessType {
     this.ffi = ffi.Library(libraryPath, FFIConfiguration)
   }
 }
+export { init_cxs } from './api/init'
