56d16376327bf14697e05c8123f8d7c7cbc6d221
Ryan Marsh <ryan.marsh44@gmail.com>
2017-10-09 15:00:32 -0600

    added dist directory. This will be temporary
    Signed-off-by: Ryan Marsh <ryan.marsh44@gmail.com>

---
+++ cxs/wrappers/node/dist/api/mod.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ErrorCode;
+(function (ErrorCode) {
+    ErrorCode[ErrorCode["Success"] = 0] = "Success";
+    ErrorCode[ErrorCode["Failure"] = 1] = "Failure";
+    ErrorCode[ErrorCode["Waiting"] = 2] = "Waiting";
+})(ErrorCode = exports.ErrorCode || (exports.ErrorCode = {}));
+//# sourceMappingURL=mod.js.map
---
+++ cxs/wrappers/node/dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ffi = require("ffi");
+var path = require("path");
+var rustlib_1 = require("./rustlib");
+// CXSRuntime is the object that interfaces with the cxs sdk functions
+// FFIConfiguration will contain all the sdk api functions
+// CXSRuntimeConfg is a class that currently only contains a chosen basepath for the .so file
+// I made it a class just in case we think of more needed configs
+var CXSRuntime = /** @class */ (function () {
+    function CXSRuntime(config) {
+        config = config || {};
+        function _initialize_basepath() {
+            var basepath = config.basepath;
+            if (basepath === undefined) {
+                // This basepath is in the local/appSpecific node_modules
+                basepath = path.resolve('../node_modules/cxs/lib/libcxs.so');
+            }
+            return basepath;
+        }
+        // initialize FFI
+        var libraryPath = _initialize_basepath();
+        this.ffi = ffi.Library(libraryPath, rustlib_1.FFIConfiguration);
+    }
+    return CXSRuntime;
+}());
+exports.CXSRuntime = CXSRuntime;
+//# sourceMappingURL=index.js.map
