096729c17942a0eea27c9a9ca85f69f7b9e25299
artem.ivanov <artem.ivanov@dsr-company.com>
2017-10-10 08:42:49 +0300

    Corrected testCreatePoolWorksForNullConfig test. Transection file creation.
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- wrappers/java/src/test/java/org/hyperledger/indy/sdk/pool/CreatePoolTest.java
+++ wrappers/java/src/test/java/org/hyperledger/indy/sdk/pool/CreatePoolTest.java
@@ -7,7 +7,6 @@ import org.hyperledger.indy.sdk.utils.PoolUtils;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertTrue;
 
@@ -15,7 +14,11 @@ public class CreatePoolTest extends IndyIntegrationTest {
 
 	@Test
 	public void testCreatePoolWorksForNullConfig() throws Exception {
-		PoolUtils.createGenesisTxnFile("testCreatePoolWorks.txn");
+		File file = new File("testCreatePoolWorks.txn");
+		file.deleteOnExit();
+		assertTrue(file.createNewFile());
+		PoolUtils.writeTransactions(file, 1);
+
 		Pool.createPoolLedgerConfig("testCreatePoolWorks", null).get();
 	}
 
