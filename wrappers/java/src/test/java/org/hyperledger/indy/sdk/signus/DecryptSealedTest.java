823a057c055ae55781f5a78383b51d1aa628fe44
artem.ivanov <artem.ivanov@dsr-company.com>
2017-10-10 16:23:47 +0300

    Refactored Java tests for Anonymous encrypt/decrypt Api calls
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- wrappers/java/src/test/java/org/hyperledger/indy/sdk/signus/DecryptSealedTest.java
+++ wrappers/java/src/test/java/org/hyperledger/indy/sdk/signus/DecryptSealedTest.java
@@ -2,75 +2,45 @@ package org.hyperledger.indy.sdk.signus;
 
 import org.hyperledger.indy.sdk.ErrorCode;
 import org.hyperledger.indy.sdk.ErrorCodeMatcher;
-import org.hyperledger.indy.sdk.IndyIntegrationTest;
-import org.hyperledger.indy.sdk.pool.Pool;
-import org.hyperledger.indy.sdk.pool.PoolJSONParameters;
+import org.hyperledger.indy.sdk.IndyIntegrationTestWithPoolAndSingleWallet;
 import org.hyperledger.indy.sdk.signus.SignusResults.CreateAndStoreMyDidResult;
-import org.hyperledger.indy.sdk.utils.PoolUtils;
-import org.hyperledger.indy.sdk.wallet.Wallet;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
-import static org.hyperledger.indy.sdk.utils.PoolUtils.DEFAULT_POOL_NAME;
 import static org.junit.Assert.assertTrue;
 
-public class DecryptSealedTest extends IndyIntegrationTest {
+public class DecryptSealedTest extends IndyIntegrationTestWithPoolAndSingleWallet {
 
-	private Pool pool;
-	private Wallet wallet;
 	private String trusteeDid;
 	private String trusteeVerkey;
 	private String myDid;
 	private String myVerkey;
-	private String walletName = "signusWallet";
-	private byte[] msg = "{\"reqId\":1496822211362017764}".getBytes();
-	private String identityJsonTemplate = "{\"did\":\"%s\",\"verkey\":\"%s\"}";
 
 	@Before
-	public void createWalletWithDid() throws Exception {
-		String poolName = PoolUtils.createPoolLedgerConfig();
-
-		PoolJSONParameters.OpenPoolLedgerJSONParameter config2 =
-				new PoolJSONParameters.OpenPoolLedgerJSONParameter(null, null, null);
-		pool = Pool.openPoolLedger(poolName, config2.toJson()).get();
-
-		Wallet.createWallet(DEFAULT_POOL_NAME, walletName, "default", null, null).get();
-		wallet = Wallet.openWallet(walletName, null, null).get();
-
-		SignusJSONParameters.CreateAndStoreMyDidJSONParameter didJson =
-				new SignusJSONParameters.CreateAndStoreMyDidJSONParameter(null, TRUSTEE_SEED, null, false);
-
-		CreateAndStoreMyDidResult trusteeNym = Signus.createAndStoreMyDid(wallet, didJson.toJson()).get();
+	public void before() throws Exception {
+		CreateAndStoreMyDidResult trusteeNym = Signus.createAndStoreMyDid(wallet, TRUSTEE_IDENTITY_JSON).get();
 		trusteeDid = trusteeNym.getDid();
 		trusteeVerkey = trusteeNym.getVerkey();
 
-		SignusJSONParameters.CreateAndStoreMyDidJSONParameter didJson2 =
-				new SignusJSONParameters.CreateAndStoreMyDidJSONParameter(null, MY1_SEED, null, null);
-
-		CreateAndStoreMyDidResult myNym = Signus.createAndStoreMyDid(wallet, didJson2.toJson()).get();
+		CreateAndStoreMyDidResult myNym = Signus.createAndStoreMyDid(wallet, MY1_IDENTITY_JSON).get();
 		myDid = myNym.getDid();
 		myVerkey = myNym.getVerkey();
-	}
 
-	@After
-	public void deleteWallet() throws Exception {
-		wallet.closeWallet().get();
-		Wallet.deleteWallet(walletName, null).get();
-		pool.closePoolLedger().get();
+		String identityJson = String.format(IDENTITY_JSON_TEMPLATE, trusteeDid, trusteeVerkey);
+		Signus.storeTheirDid(wallet, identityJson).get();
 	}
 
 	@Test
 	public void testDecryptSealedWorks() throws Exception {
-		String identityJson = String.format(identityJsonTemplate, trusteeDid, trusteeVerkey);
+		String identityJson = String.format(IDENTITY_JSON_TEMPLATE, trusteeDid, trusteeVerkey);
 		Signus.storeTheirDid(wallet, identityJson).get();
 
-		byte[] encryptedMessage = Signus.encryptSealed(wallet, pool, trusteeDid, msg).get();
+		byte[] encryptedMessage = Signus.encryptSealed(wallet, pool, trusteeDid, MESSAGE).get();
 		byte[] decryptedMessage = Signus.decryptSealed(wallet, trusteeDid, encryptedMessage).get();
-		assertTrue(Arrays.equals(msg, decryptedMessage));
+		assertTrue(Arrays.equals(MESSAGE, decryptedMessage));
 	}
 
 	@Test
@@ -78,13 +48,10 @@ public class DecryptSealedTest extends IndyIntegrationTest {
 		thrown.expect(ExecutionException.class);
 		thrown.expectCause(new ErrorCodeMatcher(ErrorCode.CommonInvalidStructure));
 
-		String identityJson = String.format(identityJsonTemplate, trusteeDid, trusteeVerkey);
+		String identityJson = String.format(IDENTITY_JSON_TEMPLATE, myDid, myVerkey);
 		Signus.storeTheirDid(wallet, identityJson).get();
 
-		identityJson = String.format(identityJsonTemplate, myDid, myVerkey);
-		Signus.storeTheirDid(wallet, identityJson).get();
-
-		byte[] encryptResult = Signus.encryptSealed(wallet, pool, myDid, msg).get();
+		byte[] encryptResult = Signus.encryptSealed(wallet, pool, myDid, MESSAGE).get();
 
 		Signus.decryptSealed(wallet, trusteeDid, encryptResult).get();
 	}
--- wrappers/java/src/test/java/org/hyperledger/indy/sdk/signus/EncryptSealedTest.java
+++ wrappers/java/src/test/java/org/hyperledger/indy/sdk/signus/EncryptSealedTest.java
@@ -2,14 +2,9 @@ package org.hyperledger.indy.sdk.signus;
 
 import org.hyperledger.indy.sdk.ErrorCode;
 import org.hyperledger.indy.sdk.ErrorCodeMatcher;
-import org.hyperledger.indy.sdk.IndyIntegrationTest;
+import org.hyperledger.indy.sdk.IndyIntegrationTestWithPoolAndSingleWallet;
 import org.hyperledger.indy.sdk.ledger.Ledger;
-import org.hyperledger.indy.sdk.pool.Pool;
-import org.hyperledger.indy.sdk.pool.PoolJSONParameters;
 import org.hyperledger.indy.sdk.signus.SignusResults.CreateAndStoreMyDidResult;
-import org.hyperledger.indy.sdk.utils.PoolUtils;
-import org.hyperledger.indy.sdk.wallet.Wallet;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,35 +12,17 @@ import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertNotNull;
 
-public class EncryptSealedTest extends IndyIntegrationTest {
+public class EncryptSealedTest extends IndyIntegrationTestWithPoolAndSingleWallet {
 
-	private Pool pool;
-	private Wallet wallet;
 	private String did;
 	private String verkey;
-	private String walletName = "signusWallet";
-	private byte[] msg = "{\"reqId\":1496822211362017764}".getBytes();
 
 	@Before
 	public void before() throws Exception {
-		String poolName = PoolUtils.createPoolLedgerConfig();
-
-		PoolJSONParameters.OpenPoolLedgerJSONParameter config = new PoolJSONParameters.OpenPoolLedgerJSONParameter(null, null, null);
-		pool = Pool.openPoolLedger(poolName, config.toJson()).get();
-
-		Wallet.createWallet(poolName, walletName, "default", null, null).get();
-		wallet = Wallet.openWallet(walletName, null, null).get();
-
-		SignusJSONParameters.CreateAndStoreMyDidJSONParameter didJson =
-				new SignusJSONParameters.CreateAndStoreMyDidJSONParameter(null, TRUSTEE_SEED, null, false);
-
-		CreateAndStoreMyDidResult result = Signus.createAndStoreMyDid(wallet, didJson.toJson()).get();
+		CreateAndStoreMyDidResult result = Signus.createAndStoreMyDid(wallet, TRUSTEE_IDENTITY_JSON).get();
 		String trusteeDid = result.getDid();
 
-		SignusJSONParameters.CreateAndStoreMyDidJSONParameter didJson2 =
-				new SignusJSONParameters.CreateAndStoreMyDidJSONParameter(null, MY1_SEED, null, null);
-
-		CreateAndStoreMyDidResult nym = Signus.createAndStoreMyDid(wallet, didJson2.toJson()).get();
+		CreateAndStoreMyDidResult nym = Signus.createAndStoreMyDid(wallet, MY1_IDENTITY_JSON).get();
 		did = nym.getDid();
 		verkey = nym.getVerkey();
 
@@ -53,19 +30,12 @@ public class EncryptSealedTest extends IndyIntegrationTest {
 		Ledger.signAndSubmitRequest(pool, wallet, trusteeDid, nymRequest).get();
 	}
 
-	@After
-	public void after() throws Exception {
-		wallet.closeWallet().get();
-		Wallet.deleteWallet(walletName, null).get();
-		pool.closePoolLedger().get();
-	}
-
 	@Test
 	public void testEncryptSealedWorksForPkCachedInWallet() throws Exception {
-		String identityJson = String.format("{\"did\":\"%s\",\"verkey\":\"%s\"}", did, verkey);
+		String identityJson = String.format(IDENTITY_JSON_TEMPLATE, did, verkey);
 		Signus.storeTheirDid(wallet, identityJson).get();
 
-		byte[] encryptResult = Signus.encryptSealed(wallet, pool, did, msg).get();
+		byte[] encryptResult = Signus.encryptSealed(wallet, pool, did, MESSAGE).get();
 		assertNotNull(encryptResult);
 	}
 
@@ -74,13 +44,13 @@ public class EncryptSealedTest extends IndyIntegrationTest {
 		String identityJson = String.format("{\"did\":\"%s\"}", did);
 		Signus.storeTheirDid(wallet, identityJson).get();
 
-		byte[] encryptResult = Signus.encryptSealed(wallet, pool, did, msg).get();
+		byte[] encryptResult = Signus.encryptSealed(wallet, pool, did, MESSAGE).get();
 		assertNotNull(encryptResult);
 	}
 
 	@Test
 	public void testEncryptSealedWorksForGetNymFromLedger() throws Exception {
-		byte[] encryptResult = Signus.encryptSealed(wallet, pool, did, msg).get();
+		byte[] encryptResult = Signus.encryptSealed(wallet, pool, did, MESSAGE).get();
 		assertNotNull(encryptResult);
 	}
 
@@ -89,6 +59,6 @@ public class EncryptSealedTest extends IndyIntegrationTest {
 		thrown.expect(ExecutionException.class);
 		thrown.expectCause(new ErrorCodeMatcher(ErrorCode.CommonInvalidState));
 		
-		Signus.encryptSealed(wallet, pool, "8wZcEriaNLNKtteJvx7f8i", msg).get();
+		Signus.encryptSealed(wallet, pool, DID1, MESSAGE).get();
 	}
 }
