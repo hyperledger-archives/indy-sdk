dda2dd803be1a10aca7d345c505541fca7390715
Symon Rottem <s.rottem@gmail.com>
2017-10-12 19:01:20 +0200

    Add temporary program entry point in unit tests for troubleshooting as MSTest framework hides console output.

---
+++ wrappers/dotnet/indy-sdk-dotnet-test/Program.cs
@@ -0,0 +1,17 @@
+﻿using Hyperledger.Indy.Test.PoolTests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hyperledger.Indy.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new CreatePoolTest();
+            test.TestCreatePoolWorksForNullConfig().Wait();
+        }
+    }
+
+}
