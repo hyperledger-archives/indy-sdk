7010678609c82bb1081da052803486f08a7da330
Symon Rottem <s.rottem@gmail.com>
2017-10-15 11:28:22 +0200

    Added code docs for exceptions and refactored constructors.
    Signed-off-by: Symon Rottem <s.rottem@gmail.com>

--- wrappers/java/src/main/java/org/hyperledger/indy/sdk/pool/PoolClosedException.java
+++ wrappers/java/src/main/java/org/hyperledger/indy/sdk/pool/PoolClosedException.java
@@ -1,17 +1,21 @@
 package org.hyperledger.indy.sdk.pool;
 
+import org.hyperledger.indy.sdk.ErrorCode;
 import org.hyperledger.indy.sdk.IndyException;
 
+/**
+ * Exception thrown when attempting to use a poll that has already been closed.
+ */
 public class PoolClosedException extends IndyException
 {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 7124250084655044699L;
 	private final static String message = "The pool is closed and cannot be used.";
 
-	public PoolClosedException(int sdkErrorCode) 
+	/**
+	 * Initializes a new PoolClosedExecption.
+	 */
+	public PoolClosedException() 
     {
-    	super(message, sdkErrorCode);
+    	super(message, ErrorCode.PoolLedgerInvalidPoolHandle.value());
     }
 }
--- wrappers/java/src/main/java/org/hyperledger/indy/sdk/pool/PoolLedgerConfigExistsException.java
+++ wrappers/java/src/main/java/org/hyperledger/indy/sdk/pool/PoolLedgerConfigExistsException.java
@@ -1,17 +1,21 @@
 package org.hyperledger.indy.sdk.pool;
 
+import org.hyperledger.indy.sdk.ErrorCode;
 import org.hyperledger.indy.sdk.IndyException;
 
+/**
+ * Exception thrown when attempting to create a pool configuration with the same name as one that already exists.
+ */
 public class PoolLedgerConfigExistsException extends IndyException
 {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 2032790158242533689L;
 	private final static String message = "A pool ledger configuration already exists with the specified name.";
 
-	public PoolLedgerConfigExistsException(int sdkErrorCode) 
+	/**
+	 * Initializes a new PoolLedgerConfigExistsException.
+	 */
+	public PoolLedgerConfigExistsException() 
     {
-    	super(message, sdkErrorCode);
+    	super(message, ErrorCode.PoolLedgerConfigAlreadyExistsError.value());
     }
 }
