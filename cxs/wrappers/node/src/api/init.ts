a53c8bb2c0098e474e7449f652c03cdc0549a1e1
keenondrums <frantic1993@gmail.com>
2017-10-20 10:45:16 -0600

    Updated connection API
    Signed-off-by: keenondrums <frantic1993@gmail.com>

--- cxs/wrappers/node/src/api/init.ts
+++ cxs/wrappers/node/src/api/init.ts
@@ -2,7 +2,7 @@ import * as posix from 'path'
 import { CXSRuntime } from '../index'
 import { CXSRuntimeConfig } from '../rustlib'
 
-export function init_cxs (filename) {
+export function init_cxs (filename: string) {
   const config = new CXSRuntimeConfig(null)
   const rust = new CXSRuntime(config)
   return rust.ffi.cxs_init(filename)
