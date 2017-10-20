89601ef910aa779a916c3fc96c36eb7b3d622913
Sergey Minaev <sergey.minaev@dsr-company.com>
2017-10-20 13:36:30 +0300

    Disable warnings for unimplemented functions.
    Signed-off-by: Sergey Minaev <sergey.minaev@dsr-company.com>

--- libindy/src/api/agent.rs
+++ libindy/src/api/agent.rs
@@ -42,6 +42,7 @@ pub extern fn indy_prep_msg(command_handle: i32,
 }
 
 #[no_mangle]
+#[allow(unused_variables)]
 pub extern fn indy_prep_anonymous_msg(command_handle: i32,
                                       recipient_vk: *const c_char,
                                       msg_data: *const u8,
@@ -54,6 +55,7 @@ pub extern fn indy_prep_anonymous_msg(command_handle: i32,
 }
 
 #[no_mangle]
+#[allow(unused_variables)]
 pub extern fn indy_parse_msg(command_handle: i32,
                              recipient_vk: *const c_char,
                              encrypted_msg: *const u8,
