6f72d3895fadeaa182167b39f81060c6fa98d55c
Symon Rottem <s.rottem@gmail.com>
2017-10-28 21:50:42 +0200

    Add crypto tests - some unimplemented.
    Signed-off-by: Symon Rottem <s.rottem@gmail.com>

---
+++ wrappers/dotnet/indy-sdk-dotnet-test/CryptoTests/BoxSealTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Hyperledger.Indy.Test.CryptoTests
+{
+    [TestClass]
+    public class BoxSealTest : CryptoIntegrationTestBase
+    {
+        [TestMethod]
+        [Ignore]
+        public async Task TestBoxSealWorks()
+        {
+            //Not sure of the best way to implement this test...
+        }
+    }
+}
---
+++ wrappers/dotnet/indy-sdk-dotnet-test/CryptoTests/BoxTest.cs
@@ -0,0 +1,26 @@
+﻿using Hyperledger.Indy.CryptoApi;
+using Hyperledger.Indy.WalletApi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Hyperledger.Indy.Test.CryptoTests
+{
+    [TestClass]
+    public class BoxTest : CryptoIntegrationTestBase
+    {
+        [TestMethod]
+        [Ignore]
+        public async Task TestBoxWorks()
+        {
+            //Not sure how to implement this test.
+        }
+
+        [TestMethod]
+        public async Task TestBoxFailsIfSenderKeyNotInWallet()
+        {
+            var ex = await Assert.ThrowsExceptionAsync<WalletValueNotFoundException>(() =>
+               Crypto.BoxAsync(wallet, KEY_NOT_IN_WALLET, recipientVerKey, MESSAGE)
+           );
+        }
+    }
+}
