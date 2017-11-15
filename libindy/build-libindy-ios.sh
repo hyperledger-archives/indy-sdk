f3b96766f394b378baf59f6f40ac0b6c25b6a756
Sergey Minaev <sergey.minaev@dsr-corporation.com>
2017-11-15 16:16:02 +0300

    [ios] Add building libindy pod step to CI; fix appropriate build script.
    Signed-off-by: Sergey Minaev <sergey.minaev@dsr-corporation.com>

--- libindy/build-libindy-ios.sh
+++ libindy/build-libindy-ios.sh
@@ -1,18 +1,16 @@
-#!/bin/sh 
+#!/bin/sh -e
 
 export PKG_CONFIG_ALLOW_CROSS=1
 export OPENSSL_DIR=/usr/local/Cellar/openssl/1.0.2l
-export EVERNYM_REPO_KEY=~/Documents/EvernymRepo
-export LIBINDY_POD_VERSION=0.0.3
 export POD_FILE_NAME=libindy.tar.gz
 
-echo "\nBuild IOS POD started..."
+echo "Build IOS POD started..."
 cargo lipo
 echo 'Build completed successfully.'
 
-WORK_DIR=`mktemp -d`
-
-echo "Try to create temporary directory: $WORK_DIR"
+WORK_DIR="out_libindy_pod"
+echo "Try to create out directory: $WORK_DIR"
+mkdir $WORK_DIR
 
 if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
   echo "Could not create temp dir $WORK_DIR"
@@ -28,21 +26,7 @@ cd $WORK_DIR
 tar -cvzf $POD_FILE_NAME *
 ls -l $WORK_DIR/$POD_FILE_NAME
 
-echo "\nPacking completed."
+echo "Packing completed."
 cd $CUR_DIR
 
-#echo "Uploading...."
-
-#cat <<EOF | sftp -i $EVERNYM_REPO_KEY repo@54.187.56.182
-#ls -l /var/repositories/libindy/ios/stable/libindy-core/$LIBINDY_POD_VERSION/$POD_FILE_NAME
-#rm /var/repositories/libindy/ios/stable/libindy-core/$LIBINDY_POD_VERSION/$POD_FILE_NAME
-#rmdir /var/repositories/libindy/ios/stable/libindy-core/$LIBINDY_POD_VERSION
-#mkdir /var/repositories/libindy/ios/stable/libindy-core/$LIBINDY_POD_VERSION
-#cd /var/repositories/libindy/ios/stable/libindy-core/$LIBINDY_POD_VERSION
-#put $WORK_DIR/$POD_FILE_NAME
-#ls -l /var/repositories/libindy/ios/stable/libindy-core/$LIBINDY_POD_VERSION
-#EOF
-
-echo "Temporary directory: $WORK_DIR"
-#rm -rf "$WORK_DIR"
-
+echo "Out directory: $WORK_DIR"
