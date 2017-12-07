5d57bb2c59e0efa23172536b43e1ddc853977916
artem.ivanov <artem.ivanov@dsr-company.com>
2017-12-07 18:59:30 +0300

    Renamed variables
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- cli/src/utils/table.rs
+++ cli/src/utils/table.rs
@@ -6,19 +6,19 @@ use prettytable::Table;
 use prettytable::row::Row;
 use prettytable::cell::Cell;
 
-pub fn print_table(keys: &Vec<(String, String)>, rows: &Vec<serde_json::Value>) {
+pub fn print_table(headers: &Vec<(String, String)>, rows: &Vec<serde_json::Value>) {
     let mut table = Table::new();
 
-    let headers = keys.iter().clone()
+    let tittles = headers.iter().clone()
         .map(|&(_, ref header)| Cell::new(header)
             .with_style(Attr::Bold)
             .with_style(Attr::ForegroundColor(color::GREEN))
         ).collect::<Vec<Cell>>();
 
-    table.add_row(Row::new(headers));
+    table.add_row(Row::new(tittles));
 
     for row in rows {
-        let columns = keys.iter().clone()
+        let columns = headers.iter().clone()
             .map(|&(ref key, _)| Cell::new(row[key].as_str().unwrap_or("-")))
             .collect::<Vec<Cell>>();
 
