4d5b881264363b1dcc5d7475a69292ef37a7d6e0
artem.ivanov <artem.ivanov@dsr-company.com>
2017-12-11 10:33:20 +0300

    Try update build.rs file
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- cli/build.rs
+++ cli/build.rs
@@ -4,7 +4,6 @@ use std::path::Path;
 
 fn main() {
     println!("cargo:rustc-link-lib=indy");
-    println!("cargo:rustc-link-lib=sodium");
 
     let target = env::var("TARGET").unwrap();
     println!("target={}", target);
