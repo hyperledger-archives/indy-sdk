a1c99920d3987b1b2e6e2ae271f8b553cea5b62b
artem.ivanov <artem.ivanov@dsr-company.com>
2018-01-15 10:57:26 +0300

    Uncommented samples
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- samples/python/src/main.py
+++ samples/python/src/main.py
@@ -5,9 +5,9 @@ from src import anoncreds, crypto, ledger, getting_started
 
 
 async def main():
-    # await anoncreds.demo()
-    # await crypto.demo()
-    # await ledger.demo()
+    await anoncreds.demo()
+    await crypto.demo()
+    await ledger.demo()
     await getting_started.run()
 
 if __name__ == '__main__':
