23ef62b5463bb57e8d7279a02107007c452ed643
Matthew Wright <matthew@smallhelm.com>
2018-02-16 20:27:48 -0600

    nodejs - fixing mem leak, cleanup, and reading arguments
    Signed-off-by: Matthew Wright <matthew@smallhelm.com>

--- wrappers/nodejs/src/indy.cc
+++ wrappers/nodejs/src/indy.cc
@@ -7,38 +7,39 @@ indy_handle_t command_handle = 1;
 Nan::Callback *callback;
 struct Payload {
     indy_error_t err;
-    const char* verkey;
+    std::string verkey;
 };
 uv_async_t* uvHandle = (uv_async_t*)malloc(sizeof(uv_async_t));
 
 
 void freeUvHandle (uv_handle_t* handle) {
-    // free(handle);
+    free(handle);
 };
 
 NAUV_WORK_CB(asyncfunctionwat) {
     Nan::HandleScope scope;
 
-    Payload* p = (Payload*)uvHandle->data; 
-    const char* response = p->verkey;
-    printf("[SECOND]%s[WAT]", p->verkey);
+    Payload* p = (struct Payload*)uvHandle->data;
 
     v8::Local<v8::Value> argv[] = {
         Nan::New<v8::Number>(p->err),
-        Nan::New<v8::String>(response).ToLocalChecked()
+        Nan::New<v8::String>(p->verkey).ToLocalChecked()
     };
     callback->Call(2, argv);
     delete callback;
+    delete p;
 
     uv_close((uv_handle_t*)uvHandle, freeUvHandle);
 }
 
 void abbreviate_verkey_cb(indy_handle_t resp_command_handle, indy_error_t resp_err, const char *const resp_verkey) {
 
-    Payload* p = new Payload();
+    struct Payload* p = new Payload();
     p->err = resp_err;
-    p->verkey = resp_verkey;
-    printf("[FIRST]%s[WAT]", p->verkey);
+
+    std::string verkey(resp_verkey);
+    p->verkey = verkey;
+
     uvHandle->data = (void *)p;
 
     uv_async_send(uvHandle);
@@ -51,14 +52,23 @@ NAN_METHOD(hello) {
 
 NAN_METHOD(abbreviate_verkey) {
 
-    std::string did = "VsKV7grR1BUE29mG2Fm2kX";
-    std::string verkey = "GjZWsBLgZCR18aL468JAT7w9CZRiBnpxUPPgyQxh4voa";
+    if(info.Length() != 3) {
+        return Nan::ThrowError(Nan::New("abbreviate_verkey expected 3 args").ToLocalChecked());
+    }
 
-    callback = new Nan::Callback(Nan::To<v8::Function>(info[0]).ToLocalChecked());
+
+
+    Nan::Utf8String didUTF(info[0]);
+    const char* did = (const char*)(*didUTF);
+
+    Nan::Utf8String verkeyUTF(info[1]);
+    const char* verkey = (const char*)(*verkeyUTF);
+
+    callback = new Nan::Callback(Nan::To<v8::Function>(info[2]).ToLocalChecked());
 
     uv_async_init(uv_default_loop(), uvHandle, asyncfunctionwat);
 
-    indy_error_t res = indy_abbreviate_verkey(command_handle, did.c_str(), verkey.c_str(), abbreviate_verkey_cb);
+    indy_error_t res = indy_abbreviate_verkey(command_handle, did, verkey, abbreviate_verkey_cb);
 
     if(res != 0){
         v8::Local<v8::Value> argv[] = {
@@ -70,16 +80,11 @@ NAN_METHOD(abbreviate_verkey) {
 
 }
 
-NAN_MODULE_INIT(InitAll) {
-    Nan::Set(target,
-        Nan::New("hello").ToLocalChecked(),
-        Nan::GetFunction(Nan::New<v8::FunctionTemplate>(hello)).ToLocalChecked()
-    );
+#define EXPORT_FN(NAME) (Nan::Set(target, Nan::New(""#NAME"").ToLocalChecked(), Nan::GetFunction(Nan::New<v8::FunctionTemplate>(NAME)).ToLocalChecked()))
 
-    Nan::Set(target,
-        Nan::New("abbreviate_verkey").ToLocalChecked(),
-        Nan::GetFunction(Nan::New<v8::FunctionTemplate>(abbreviate_verkey)).ToLocalChecked()
-    );
+NAN_MODULE_INIT(InitAll) {
+    EXPORT_FN(hello);
+    EXPORT_FN(abbreviate_verkey);
 }
 
 NODE_MODULE(indy, InitAll)
--- wrappers/nodejs/tests/index.js
+++ wrappers/nodejs/tests/index.js
@@ -6,8 +6,10 @@ test('hello world', function (t) {
 })
 
 test.cb('abbreviate_verkey', function (t) {
+  var did = 'VsKV7grR1BUE29mG2Fm2kX'
+  var verkey = 'GjZWsBLgZCR18aL468JAT7w9CZRiBnpxUPPgyQxh4voa'
   t.plan(2)
-  t.log(indy.abbreviate_verkey(function (err, verkey) {
+  t.log(indy.abbreviate_verkey(did, verkey, function (err, verkey) {
     t.is(err, 0)
     t.is(verkey, '~HYwqs2vrTc8Tn4uBV7NBTe')
     t.end()
