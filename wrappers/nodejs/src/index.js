3d1167218bf18f222c42ab4f587bf0abd9ec8525
Matthew Wright <matthew@smallhelm.com>
2018-03-02 10:30:01 -0500

    nodejs - opt-in promise mode, and testing argument type assertions
    Signed-off-by: Matthew Wright <matthew@smallhelm.com>

--- wrappers/nodejs/src/index.js
+++ wrappers/nodejs/src/index.js
@@ -1,1 +1,19 @@
 module.exports = require('bindings')('indy')
+
+module.exports.promise = {}
+Object.keys(module.exports).forEach(function (name) {
+  var fn = module.exports[name]
+  if (typeof fn !== 'function') {
+    return
+  }
+  module.exports.promise[name] = function () {
+    var args = Array.prototype.slice.call(arguments)
+    return new Promise(function (resolve, reject) {
+      args.push(function (err, data) {
+        if (err) reject(err)
+        else resolve(data)
+      })
+      fn.apply(null, args)
+    })
+  }
+})
--- wrappers/nodejs/tests/index.js
+++ wrappers/nodejs/tests/index.js
@@ -1,13 +1,21 @@
 var test = require('ava')
-var indy = require('../')
+var indy = require('../').promise
 
-test.cb('abbreviate_verkey', function (t) {
+test('abbreviate_verkey', async function (t) {
   var did = 'VsKV7grR1BUE29mG2Fm2kX'
   var verkey = 'GjZWsBLgZCR18aL468JAT7w9CZRiBnpxUPPgyQxh4voa'
-  t.plan(2)
-  t.log(indy.abbreviate_verkey(did, verkey, function (err, verkey) {
-    t.is(err, 0)
-    t.is(verkey, '~HYwqs2vrTc8Tn4uBV7NBTe')
-    t.end()
-  }))
+
+  t.is(await indy.abbreviate_verkey(did, verkey), '~HYwqs2vrTc8Tn4uBV7NBTe')
+
+  var err = await t.throws(indy.abbreviate_verkey())
+  t.is(err.message, 'abbreviate_verkey expected 3 args')
+
+  err = await t.throws(indy.abbreviate_verkey(did, verkey, did))
+  t.is(err.message, 'abbreviate_verkey expected 3 args')
+
+  err = await t.throws(indy.abbreviate_verkey(1, verkey))
+  t.is(err.message, 'abbreviate_verkey arg 0 expected String')
+
+  err = await t.throws(indy.abbreviate_verkey(did, 2))
+  t.is(err.message, 'abbreviate_verkey arg 1 expected String')
 })
