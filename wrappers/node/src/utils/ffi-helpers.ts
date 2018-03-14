68c1ad815fa25420621ec26cd8f52adb623b8abe
Andrei Goncharov <frantic1993@gmail.com>
2018-03-14 18:51:02 +0300

    Bug Fixed: Node process exits before a callback is called (#215)

--- wrappers/node/src/utils/ffi-helpers.ts
+++ wrappers/node/src/utils/ffi-helpers.ts
@@ -1,12 +1,18 @@
+const maxTimeout = 2147483647
+
 export const createFFICallbackPromise = <T>(fn, cb) => {
   let cbRef = null
+  // TODO: Research why registering a callback doesn't keep parent thread alive https://github.com/node-ffi/node-ffi
+  const processKeepAliveTimer = setTimeout(() => undefined, maxTimeout)
   return (new Promise<T>( (resolve, reject) => fn(resolve, reject, cbRef = cb(resolve, reject))))
         .then((res) => {
           cbRef = null
+          clearTimeout(processKeepAliveTimer)
           return res
         })
         .catch((err) => {
           cbRef = null
+          clearTimeout(processKeepAliveTimer)
           throw err
         })
 }
