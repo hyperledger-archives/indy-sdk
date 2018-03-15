691627b1a7c32ce06ff52b33167227d455f36010
Sergey Minaev <sergey.minaev@dsr-company.com>
2018-03-15 11:12:42 +0300

    Restore ledger tests.
    Signed-off-by: Sergey Minaev <sergey.minaev@dsr-company.com>

--- libindy/tests/ledger.rs
+++ libindy/tests/ledger.rs
@@ -788,7 +788,6 @@ mod high_cases {
         }
 
         #[test]
-        #[ignore] //FIXME
         #[cfg(feature = "local_nodes_pool")]
         fn indy_claim_def_request_works_without_signature() {
             TestUtils::cleanup_storage();
@@ -821,7 +820,6 @@ mod high_cases {
         }
 
         #[test]
-        #[ignore] //FIXME
         #[cfg(feature = "local_nodes_pool")]
         fn indy_claim_def_requests_works() {
             TestUtils::cleanup_storage();
