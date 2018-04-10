cfd063578383aea05fc2aa577595cbd85a04ab00
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-04-10 14:55:33 +0530

    Updated android build script
    Signed-off-by: Mohammad Abdul Sami <faisal00813@gmail.com>

--- vcx/libvcx/android_build.sh
+++ vcx/libvcx/android_build.sh
@@ -1,4 +1,15 @@
 
+if [ -d "${HOME}/.NDK_TOOLCHAINS" ]; then
+    export NDK_TOOLCHAIN_DIR=${HOME}/.NDK_TOOLCHAINS
+fi
+
+
+if [[ -z "${NDK_TOOLCHAIN_DIR}"  ]]; then
+    echo "NDK_TOOLCHAIN_DIR is not set. Exiting.... "
+    echo "If you have not setup Toolchains then try running install_toolchains.sh."
+    exit 1
+fi
+
 if [ "$1" == "aarm64" ]; then
     echo "Building for aarch64-linux-android"
     # Link to static libindy library
@@ -9,6 +20,6 @@ if [ "$1" == "aarm64" ]; then
     export CC=${NDK_TOOLCHAIN_DIR}/arm64/bin/aarch64-linux-android-clang
 
     # build commands
-    # cargo clean --target aarch64-linux-android
+    cargo clean --target aarch64-linux-android
     cargo build --target aarch64-linux-android --verbose
 fi
