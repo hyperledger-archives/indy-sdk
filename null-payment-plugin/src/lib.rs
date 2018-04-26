2b8ef8ed91dcc2f71bad44598f686689aef0ae63
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2018-04-26 18:05:34 +0300

    Implement null payment handler in a separate crate
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

---
+++ null-payment-plugin/src/lib.rs
@@ -0,0 +1,14 @@
+extern crate indy;
+
+pub mod payments;
+
+#[macro_use]
+extern crate lazy_static;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
