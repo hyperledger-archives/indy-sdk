3727a55477e29403a37d6969521eed0709f0dfa7
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2018-05-03 11:32:27 +0300

    Refactor to use PaymentsUtils
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- libindy/tests/payments.rs
+++ libindy/tests/payments.rs
@@ -16,26 +16,31 @@ extern crate log;
 #[macro_use]
 mod utils;
 
-use nullpaymentplugin::payments::payment_callbacks::nullpayment_init;
-use utils::timeout::TimeoutUtils;
 use utils::wallet::WalletUtils;
 use utils::test::TestUtils;
+use utils::payments::PaymentsUtils;
 use indy::api::ErrorCode;
-use indy::api::payments::*;
 use std::ffi::CString;
 
-#[test]
-fn create_payment_method() {
-    extern fn cb (cmd_handle: i32, err: nullpaymentplugin::payments::ErrorCode) {
+mod high {
+    use nullpaymentplugin::payments::payment_callbacks::nullpayment_init;
 
+    #[test]
+    fn create_payment_address() {
+        extern fn cb(cmd_handle: i32, _err: nullpaymentplugin::payments::ErrorCode) {}
+        nullpayment_init(1, Some(cb));
+        TestUtils::cleanup_storage();
+        let wallet = WalletUtils::create_and_open_wallet("WALLET", None);
+
+        let res = match wallet {
+            Ok(handle) => PaymentsUtils::create_payment_address(handle),
+            Err(e) => Err(e)
+        };
+        assert_match!(Ok(_), res);
+        let res = res.unwrap();
+        assert!(res.starts_with("pay:null"));
     }
-    nullpayment_init(1, Some(cb));
-    TestUtils::cleanup_storage();
-    let wallet = WalletUtils::create_and_open_wallet("WALLET", None);
 
-
-
-
-    assert_eq!(res.0, ErrorCode::Success);
-    assert!(res.1.starts_with("pay:null"))
+    #[test]
+    fn create
 }
