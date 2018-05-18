385b322f8ca6c88734e0de495b5f398b6a14685c
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-company.com>
2018-05-18 17:26:20 +0300

    * Fixed inter-pluggins linking on windows platform
    Signed-off-by: Vyacheslav Gudkov <vyacheslav.gudkov@dsr-company.com>

--- libnullpay/build.rs
+++ libnullpay/build.rs
@@ -1,17 +1,19 @@
 use std::env;
 
 fn main() {
-    println!("cargo:rustc-link-lib=dylib=indy");
+    println!("cargo:rustc-link-lib=dylib=indy.dll");
 
     let target = env::var("TARGET").unwrap();
     println!("target={}", target);
 
     if target.find("-windows-").is_some() {
-        println!("cargo:rustc-link-lib=dylib=ssleay32");
-        println!("cargo:rustc-link-lib=dylib=zmq");
-        println!("cargo:rustc-link-lib=dylib=sodium");
-        let prebuilt_dir = env::var("INDY_PREBUILT_DEPS_DIR").unwrap();
-        println!("cargo:rustc-flags=-L {}\\lib", prebuilt_dir);
+        let profile = env::var("PROFILE").unwrap();
+        println!("profile={}", profile);
+
+        let indy_dir = env::var("INDY_DIR").unwrap_or(format!("..\\libindy\\target\\{}", profile));
+        println!("indy_dir={}", indy_dir);
+
+        println!("cargo:rustc-flags=-L {}", indy_dir);
         return;
     }
 }
