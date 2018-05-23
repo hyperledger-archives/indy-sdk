554ccbc85d95c0a0658bf4a243d6310f37e0e677
Michael Lodder <redmike7@gmail.com>
2018-05-23 15:56:26 -0600

    More cleanup
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -9,3 +9,15 @@ macro_rules! opt_c_str {
         $x.map(|s| CString::new(s).unwrap()).unwrap_or(CString::new("").unwrap())
     }
 }
+
+macro_rules! rust_str {
+    ($x:ident) => {
+        unsafe { CStr::from_ptr($x).to_str().unwrap().to_string() }
+    }
+}
+
+macro_rules! rust_slice {
+    ($x:ident, $y:ident) => {
+        unsafe { slice::from_raw_parts($x, $y as usize) }
+    }
+}
