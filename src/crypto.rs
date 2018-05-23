a872b13b52cbecaa24a9b1ca1019592b228e79a7
Michael Lodder <redmike7@gmail.com>
2018-05-23 16:33:26 -0600

    Added more docs
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- src/crypto.rs
+++ src/crypto.rs
@@ -12,8 +12,8 @@ pub struct Key {}
 impl Key {
     /// Creates key pair in wallet
     /// # Arguments
-    /// * `wallet_handle` - Wallet handle (created by open_wallet)
-    /// * `my_key_json` - Key information as json
+    /// * `wallet_handle` - wallet handle (created by Wallet::open)
+    /// * `my_key_json` - key information as json
     ///
     /// # Example
     /// my_key_json
@@ -22,7 +22,7 @@ impl Key {
     ///     "crypto_type": string, // Optional (if not set then ed25519 curve is used); Currently only 'ed25519' value is supported for this field.
     /// }
     /// # Returns
-    /// Ver key of generated key pair, also used as key identifier
+    /// verkey of generated key pair, also used as key identifier
     pub fn create(wallet_handle: IndyHandle, my_key_json: &str) -> Result<String, ErrorCode> {
         let (receiver, command_handle, cb) = ClosureHandler::cb_ec_string();
 
@@ -36,7 +36,7 @@ impl Key {
 
     /// Saves/replaces the metadata for the `verkey` in the wallet
     /// # Arguments
-    /// * `wallet_handle` - Wallet handle (created by open_wallet)
+    /// * `wallet_handle` - wallet handle (created by Wallet::open)
     /// * `verkey` - the public key or key id where to store the metadata
     /// * `metadata` - the metadata that will be stored with the key
     pub fn set_metadata(wallet_handle: IndyHandle, verkey: &str, metadata: &str) -> Result<(), ErrorCode> {
@@ -53,10 +53,10 @@ impl Key {
 
     /// Retrieves the metadata for the `verkey` in the wallet
     /// # Argument
-    /// * `wallet_handle` - Wallet handle (created by open_wallet)
+    /// * `wallet_handle` - wallet handle (created by Wallet::open)
     /// * `verkey` - the public key or key id to retrieve metadata
     /// # Returns
-    /// Metadata currently stored with the key; Can be empty if no metadata was saved for this key
+    /// metadata currently stored with the key; Can be empty if no metadata was saved for this key
     pub fn get_metadata(wallet_handle: IndyHandle, verkey: &str) -> Result<String, ErrorCode> {
         let (receiver, command_handle, cb) = ClosureHandler::cb_ec_string();
 
@@ -73,6 +73,13 @@ impl Key {
 pub struct Crypto {}
 
 impl Crypto {
+    /// Signs a message with a key
+    /// # Arguments
+    /// * `wallet_handle` - wallet handle (created by Wallet::open)
+    /// * `signer_vk` - key id or verkey of my key. The key must be created by calling Key::create or Did::new
+    /// * `message` - the data to be signed
+    /// # Returns
+    /// the signature
     pub fn sign(wallet_handle: IndyHandle, signer_vk: &str, message: &[u8]) -> Result<Vec<u8>, ErrorCode> {
         let (receiver, command_handle, cb) = ClosureHandler::cb_ec_slice();
 
@@ -87,11 +94,21 @@ impl Crypto {
         ResultHandler::one(err, receiver)
     }
 
-    pub fn verify(wallet_handle: IndyHandle, message: &[u8], signature: &[u8]) -> Result<bool, ErrorCode> {
+    /// Verify a signature with a verkey
+    /// # Arguments
+    /// * `wallet_handle` - wallet handle (created by Wallet::open)
+    /// * `signer_vk` - key id or verkey of my key. The key must be created by calling Key::create or Did::new
+    /// * `message` - the data that was signed
+    /// * `signature` - the signature to verify
+    /// # Returns
+    /// true if signature is valid, false otherwise
+    pub fn verify(wallet_handle: IndyHandle, signer_vk: &str, message: &[u8], signature: &[u8]) -> Result<bool, ErrorCode> {
         let (receiver, command_handle, cb) = ClosureHandler::cb_ec_bool();
 
+        let signer_vk = c_str!(signer_vk);
+
         let err = unsafe {
-            crypto::indy_crypto_verify(command_handle, wallet_handle,
+            crypto::indy_crypto_verify(command_handle, wallet_handle, signer_vk.as_ptr(),
                                message.as_ptr() as *const u8, message.len() as u32,
                                signature.as_ptr() as *const u8, signature.len() as u32, cb)
         };
@@ -99,6 +116,23 @@ impl Crypto {
         ResultHandler::one(err, receiver)
     }
 
+    /// Encrypt a message by authenticated-encryption scheme.
+    ///
+    /// Sender can encrypt a confidential message specifically for Recipient, using Sender's public key.
+    /// Using Recipient's public key, Sender can compute a shared secret key.
+    /// Using Sender's public key and his secret key, Recipient can compute the exact same shared secret key.
+    /// That shared secret key can be used to verify that the encrypted message was not tampered with,
+    /// before eventually decrypting it.
+    ///
+    /// Note to use DID keys with this function you can call Did::get_ver_key to get key id (verkey)
+    /// for specific DID.
+    /// # Arguments
+    /// * `wallet_handle` - wallet handle (created by Wallet::open)
+    /// * `signer_vk` - key id or verkey of my key. The key must be created by calling Key::create or Did::new
+    /// * `recipient_vk` - key id or verkey of the other party's key
+    /// * `message` - the data to be encrypted
+    /// # Returns
+    /// the encrypted message
     pub fn auth_crypt(wallet_handle: IndyHandle, sender_vk: &str, recipient_vk: &str, message: &[u8]) -> Result<Vec<u8>, ErrorCode> {
         let (receiver, command_handle, cb) = ClosureHandler::cb_ec_slice();
 
