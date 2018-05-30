492ead251369eb1a73d612ce1313f03c654f6499
Mark Hadley <hadleym@email.arizona.edu>
2018-05-30 15:40:20 -0600

    Cherry pick inputs/outupts 22fa3d3 (#292)
    Signed-off-by: Mark Hadley <hadleym@email.arizona.edu>

--- vcx/libvcx/src/utils/libindy/payments.rs
+++ vcx/libvcx/src/utils/libindy/payments.rs
@@ -11,20 +11,26 @@ use settings;
 
 static NULL_PAYMENT: &str = "null";
 static EMPTY_CONFIG: &str = "{}";
-static FEES: &str = r#"{"1":1, "101":2}"#;
+static FEES: &str = r#"{"1":1, "101":2, "102":44}"#;
 
 static PAYMENT_INIT: Once = ONCE_INIT;
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WalletInfo {
+    balance: u32,
+    addresses: Vec<AddressInfo>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct AddressInfo {
     address: String,
     utxo: Vec<UTXO>,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct UTXO {
     input: String,
-    amount: i32,
+    amount: u32,
     extra: String,
 }
 
@@ -50,7 +56,7 @@ pub fn init_payments() -> Result<(), u32> {
 }
 
 pub fn create_address() -> Result<String, u32> {
-    if settings::test_indy_mode_enabled() { return Ok(r#"["pay:null:J81AxU9hVHYFtJc"]"#.to_string()); }
+    if settings::test_indy_mode_enabled() { return Ok(r#"pay:null:J81AxU9hVHYFtJc"#.to_string()); }
 
     match Payment::create_payment_address(get_wallet_handle() as i32, NULL_PAYMENT, EMPTY_CONFIG) {
         Ok(x) => Ok(x),
@@ -61,7 +67,7 @@ pub fn create_address() -> Result<String, u32> {
 fn get_address_info(address: &str) -> Result<AddressInfo, u32> {
     if settings::test_indy_mode_enabled() {
         let utxo: Vec<UTXO> = serde_json::from_str(r#"[{"input":"pov:null:1","amount":1,"extra":"yqeiv5SisTeUGkw"},{"input":"pov:null:2","amount":2,"extra":"Lu1pdm7BuAN2WNi"}]"#).unwrap();
-        return Ok(AddressInfo { address: address.to_string(), utxo, })
+        return Ok(AddressInfo { address: address.to_string(), utxo})
     }
 
     let did = settings::get_config_value(settings::CONFIG_INSTITUTION_DID).unwrap();
@@ -83,7 +89,8 @@ fn get_address_info(address: &str) -> Result<AddressInfo, u32> {
         Ok(x) => x,
         Err(_) => return Err(error::INVALID_JSON.code_num),
     };
-    Ok(AddressInfo { address: address.to_string(), utxo, })
+
+    Ok(AddressInfo { address: address.to_string(), utxo })
 }
 
 pub fn list_addresses() -> Result<Vec<String>, u32> {
@@ -117,14 +124,14 @@ pub fn get_wallet_token_info() -> Result<String, u32> {
         let mut info = get_address_info(&address)?;
 
         for utxo in info.utxo.iter() {
-            balance += utxo.amount as i64;
+            balance += utxo.amount as u32;
         }
 
         wallet_info.push(info);
     }
 
-    let addresses = serde_json::to_string(&wallet_info).unwrap();
-    Ok(format!("{{\"balance\":{}, {}}}", balance, addresses))
+    let wallet_info = WalletInfo {balance, addresses: wallet_info};
+    Ok(serde_json::to_string(&wallet_info).unwrap())
 }
 
 pub fn get_ledger_fees() -> Result<String, u32> {
@@ -143,9 +150,69 @@ pub fn get_ledger_fees() -> Result<String, u32> {
     }
 }
 
+pub fn pay_for_txn(req: &str, txn_type: &str) -> Result<String, u32> {
+    // Find cost for txn_type
+    // gen inputs and outputs
+    // indy_add_request_fees
+    // sign_and_submit
+    // parse response to make sure its valid
+    // return response
+    Ok("{}".to_string())
+}
+
+fn get_txn_cost(txn_type: &str) -> Result<u32, u32> {
+    let ledger_fees = get_ledger_fees()?;
+
+    let fees: ::std::collections::HashMap<String, u32> = serde_json::from_str(&ledger_fees).unwrap();
+
+    Ok(fees.get(txn_type).ok_or(error::UNKNOWN_TXN_TYPE.code_num)?.clone())
+}
+
+fn _address_balance(address: &Vec<UTXO>) -> u32 {
+    address.iter().fold(0, |balance, utxo| balance + utxo.amount) as u32
+}
+
+pub fn inputs(cost: u32) -> Result<(u32, String), u32> {
+    let mut inputs: Vec<String> = Vec::new();
+    let mut balance = 0;
+
+    let wallet_info: WalletInfo = serde_json::from_str(&get_wallet_token_info()?)
+        .or(Err(error::INVALID_JSON.code_num))?;
+
+    if wallet_info.balance < cost {
+        warn!("not enough tokens in wallet to pay");
+        return Err(error::INSUFFICIENT_TOKEN_AMOUNT.code_num);
+    }
+
+    // Todo: explore 'smarter' ways of selecting utxos ie bitcoin algorithms etc
+    'outer: for address in wallet_info.addresses.iter() {
+        'inner: for utxo in address.utxo.iter() {
+            if balance < cost {
+                inputs.push(utxo.input.to_string());
+                balance += utxo.amount;
+            } else { break 'outer }
+        }
+    }
+
+    let remainder = balance - cost;
+
+    Ok((remainder, serde_json::to_string(&inputs).or(Err(error::INVALID_JSON.code_num))?))
+}
+
+pub fn outputs(amount: u32) -> Result<String, u32> {
+    // In the future we might provide a way for users to specify multiple output address
+    // As of now, we only handle one output address which we create
+    Ok(json!([
+        {
+            "paymentAddress": create_address()?,
+            "amount": amount,
+            "extra": null
+        }
+    ]).to_string())
+}
+
 #[cfg(test)]
 pub mod tests {
-
     use super::*;
     use settings;
 
@@ -164,14 +231,14 @@ pub mod tests {
     #[test]
     fn test_init_payments() {
         settings::set_defaults();
-        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE,"true");
+        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE, "true");
         init_payments().unwrap();
     }
 
     #[test]
     fn test_create_address() {
         settings::set_defaults();
-        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE,"true");
+        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE, "true");
         init_payments().unwrap();
         create_address().unwrap();
     }
@@ -179,7 +246,7 @@ pub mod tests {
     #[test]
     fn test_get_addresses() {
         settings::set_defaults();
-        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE,"true");
+        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE, "true");
         init_payments().unwrap();
         create_address().unwrap();
         let addresses = list_addresses().unwrap();
@@ -188,11 +255,11 @@ pub mod tests {
     #[test]
     fn test_get_wallet_token_info() {
         settings::set_defaults();
-        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE,"true");
+        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE, "true");
         init_payments().unwrap();
         create_address().unwrap();
         let balance = get_wallet_token_info().unwrap();
-        assert_eq!(balance, r#"{"balance":6, [{"address":"pay:null:9UFgyjuJxi1i1HD","utxo":[{"input":"pov:null:1","amount":1,"extra":"yqeiv5SisTeUGkw"},{"input":"pov:null:2","amount":2,"extra":"Lu1pdm7BuAN2WNi"}]},{"address":"pay:null:zR3GN9lfbCVtHjp","utxo":[{"input":"pov:null:1","amount":1,"extra":"yqeiv5SisTeUGkw"},{"input":"pov:null:2","amount":2,"extra":"Lu1pdm7BuAN2WNi"}]}]}"#);
+        assert_eq!(balance, r#"{"balance":6,"addresses":[{"address":"pay:null:9UFgyjuJxi1i1HD","utxo":[{"input":"pov:null:1","amount":1,"extra":"yqeiv5SisTeUGkw"},{"input":"pov:null:2","amount":2,"extra":"Lu1pdm7BuAN2WNi"}]},{"address":"pay:null:zR3GN9lfbCVtHjp","utxo":[{"input":"pov:null:1","amount":1,"extra":"yqeiv5SisTeUGkw"},{"input":"pov:null:2","amount":2,"extra":"Lu1pdm7BuAN2WNi"}]}]}"#);
     }
 
     #[cfg(feature = "nullpay")]
@@ -212,7 +279,7 @@ pub mod tests {
     #[test]
     fn test_get_ledger_fees() {
         settings::set_defaults();
-        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE,"true");
+        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE, "true");
         let fees = get_ledger_fees().unwrap();
         assert!(fees.contains(r#""101":2"#));
         assert!(fees.contains(r#""1":1"#));
@@ -229,5 +296,61 @@ pub mod tests {
         assert!(fees.contains(r#""101":2"#));
         assert!(fees.contains(r#""1":1"#));
         ::utils::devsetup::cleanup_dev_env(name);
+    }
+
+    #[test]
+    fn test_address_balance() {
+        let addresses = vec![
+            UTXO { input: "pov::null:2".to_string(), amount: 2, extra: "abcde".to_string() },
+            UTXO { input: "pov::null:3".to_string(), amount: 3, extra: "bcdef".to_string() }
+        ];
+
+        assert_eq!(_address_balance(&addresses), 5);
+    }
+
+    #[test]
+    fn test_inputs() {
+        settings::set_defaults();
+        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE, "true");
+
+        // Success - Exact amount
+        assert_eq!(inputs(6).unwrap(), (0, r#"["pov:null:1","pov:null:2","pov:null:1","pov:null:2"]"#.to_string()));
+
+        // Success - utxo with remainder tokens
+        assert_eq!(inputs(5).unwrap(), (1, r#"["pov:null:1","pov:null:2","pov:null:1","pov:null:2"]"#.to_string()));
+
+        // Success - requesting amount that partial address (1 of 2 utxos) can satisfy
+        assert_eq!(inputs(1).unwrap(), (0, r#"["pov:null:1"]"#.to_string()));
+
+        // Err - request more than wallet contains
+        assert_eq!(inputs(7), Err(error::INSUFFICIENT_TOKEN_AMOUNT.code_num));
+    }
+
+    #[test]
+    fn test_select_outputs() {
+        settings::set_defaults();
+        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE, "true");
+
+        let cost = 6;
+        let expected_output = r#"[{"amount":0,"extra":null,"paymentAddress":"pay:null:J81AxU9hVHYFtJc"}]"#;
+        let (remainder, _) = inputs(cost).unwrap();
+        assert_eq!(&outputs(remainder).unwrap(), expected_output);
+    }
+
+    #[test]
+    fn test_get_txn_cost() {
+        settings::set_defaults();
+        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE, "true");
+
+        assert_eq!(get_txn_cost("101").unwrap(), 2);
+        assert_eq!(get_txn_cost("102").unwrap(), 44);
+        assert_eq!(get_txn_cost("Unknow txn type"), Err(error::UNKNOWN_TXN_TYPE.code_num));
+    }
+
+    #[test]
+    fn test_pay_for_txn() {
+        // Schema
+        let create_schema_req = ::utils::constants::SCHEMA_CREATE_JSON.to_string();
+        let price_req = pay_for_txn(&create_schema_req, "101").unwrap();
     }
 }
