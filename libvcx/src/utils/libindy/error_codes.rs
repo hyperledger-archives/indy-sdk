117fad9c97cb5f159f6771a28a456910f9296df9
ryMarsh44 <ryan.marsh44@gmail.com>
2018-06-01 10:35:16 -0600

    Test Insufficient tokens on ledger (#298)
    * functionality for paying for a ledger txn
    
    * test to verify ledger error for not enough tokens. Empty outputs with no remainder
    
    * fixed test and updated cargo to use no_payments

--- libvcx/src/utils/libindy/error_codes.rs
+++ libvcx/src/utils/libindy/error_codes.rs
@@ -1,5 +1,6 @@
 extern crate num_traits;
 
+use indy::ErrorCode;
 use utils::error;
 use std::ffi::NulError;
 use self::num_traits::int::PrimInt;
@@ -10,6 +11,23 @@ pub fn map_indy_error<T, C: PrimInt>(rtn: T, error_code: C) -> Result<T, u32> {
     }
 
     Err(map_indy_error_code(error_code))
+}
+
+// Todo - this will replace map_indy_error_code once we stop using our own indy cbs and move everything to rust-indy-sdk
+pub fn map_rust_indy_sdk_error_code(error_code: ErrorCode) -> u32 {
+    let error_code= error_code as u32;
+    if error_code >= error::UNKNOWN_ERROR.code_num {
+        return error_code;
+    }
+
+    match error_code {
+        306 =>  error::CREATE_POOL_CONFIG.code_num,
+        203 =>  error::WALLET_ALREADY_EXISTS.code_num,
+        206 =>  error::WALLET_ALREADY_OPEN.code_num,
+        407 =>  error::CREDENTIAL_DEF_ALREADY_CREATED.code_num,
+        702 =>  error::INSUFFICIENT_TOKEN_AMOUNT.code_num,
+        _ =>    error::UNKNOWN_LIBINDY_ERROR.code_num
+    }
 }
 
 pub fn map_indy_error_code<C: PrimInt>(error_code: C) -> u32 {
