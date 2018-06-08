a30614d1f1a836c79355e89e710e33cfd9902ba9
Janko Krstic <keichiri@protonmail.com>
2018-06-08 12:53:20 +0200

    function declarations in wallet.h for export/import
    Signed-off-by: Janko Krstic <keichiri@protonmail.com>

--- libindy/include/indy_wallet.h
+++ libindy/include/indy_wallet.h
@@ -124,6 +124,70 @@ extern "C" {
                                           void          (*fn)(indy_handle_t xcommand_handle, indy_error_t err, const char *const wallets)
                                           );
 
+    /// Exports opened wallet's content using key and path provided in export_config_json
+    ///
+    /// #Params
+    /// handle: wallet handle returned by indy_open_wallet.
+    /// export_config_json: JSON containing settings for input operation.
+    ///   {
+    ///     "path": path of the file that contains exported wallet content
+    ///     "key": passphrase used to export key
+    ///   }
+    ///
+    /// #Returns
+    /// Error code
+    ///
+    /// #Errors
+    /// Common*
+    /// Wallet*
+
+    extern indy_error_t indy_export_wallet(indy_handle_t  command_handle,
+                                           indy_handle_t  handle,
+                                           const char*    export_config_json,
+                                           void           (*fn)(indy_handle_t xcommand_handle, indy_error_t err)
+                                           );
+
+
+    /// Creates a new secure wallet with the given unique name and imports its content from
+    /// the file created using indy_export_wallet
+    ///
+    /// #Params
+    /// pool_name: Name of the pool that corresponds to this wallet.
+    /// name: Name of the wallet.
+    /// xtype(optional): Type of the wallet. Defaults to 'default'.
+    ///                  Custom types can be registered with indy_register_wallet_type call.
+    /// config(optional): Wallet configuration json. List of supported keys are defined by wallet type.
+    ///                    if NULL, then default config will be used.
+    /// credentials: Wallet credentials json
+    ///   {
+    ///       "key": string,
+    ///       "rekey": Optional<string>,
+    ///       "storage": Optional<object>  List of supported keys are defined by wallet type.
+    ///
+    ///   }
+    /// import_config_json: JSON containing settings for input operation.
+    ///   {
+    ///     "path": path of the file that contains exported wallet content
+    ///     "key": passphrase used to export key
+    ///   }
+    ///
+    /// #Returns
+    /// Error code
+    ///
+    /// #Errors
+    /// Common*
+    /// Wallet*
+
+    extern indy_error_t indy_import_wallet(indy_handle_t  command_handle,
+                                           const char*    pool_name,
+                                           const char*    name,
+                                           const char*    xtype,
+                                           const char*    config,
+                                           const char*    credentials,
+                                           const char*    import_config_json,
+                                           void           (*fn)(indy_handle_t xcommand_handle, indy_error_t err)
+                                           );
+
     /// Closes opened wallet and frees allocated resources.
     ///
     /// #Params
