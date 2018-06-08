79c722970762bc19238d97298979c8375caddaf1
artem.ivanov <artem.ivanov@dsr-company.com>
2018-06-08 13:03:11 +0300

    Reverted indy_pool dockerfile. Simplified getting reqId
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- libnullpay/src/services/response_storage.rs
+++ libnullpay/src/services/response_storage.rs
@@ -25,20 +25,8 @@ pub fn get_response(response: &str) -> Result<String, ErrorCode> {
     let val = str_to_val(response)?;
 
     let req_id = match val["result"]["ver"].as_str() {
-        Some("1") =>
-            val.get("result")
-                .and_then(|result| result.as_object())
-                .and_then(|result| result.get("txn"))
-                .and_then(|txn| txn.as_object())
-                .and_then(|txn| txn.get("metadata"))
-                .and_then(|metadata| metadata.as_object())
-                .and_then(|metadata| metadata.get("reqId"))
-                .and_then(|req_id| req_id.as_i64()),
-        None =>
-            val.get("result")
-                .and_then(|result| result.as_object())
-                .and_then(|result| result.get("reqId"))
-                .and_then(|req_id| req_id.as_i64()),
+        Some("1") => val["result"]["txn"]["metadata"]["reqId"].as_i64(),
+        None => val["result"]["reqId"].as_i64(),
         _ => return Err(ErrorCode::CommonInvalidState)
     };
 
