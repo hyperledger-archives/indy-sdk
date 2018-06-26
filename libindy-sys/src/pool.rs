7d12c52916456f4b07ca24bcecf3d85bf9a1bc8f
Michael Lodder <redmike7@gmail.com>
2018-06-26 10:26:15 -0600

    Added new pool and ledger functions
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libindy-sys/src/pool.rs
+++ libindy-sys/src/pool.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-use {Error, Handle, CString};
+use {CString, Error, Handle};
 
 extern {
     #[no_mangle]
@@ -8,29 +8,29 @@ extern {
                                           config_name: CString,
                                           config: CString,
                                           cb: Option<ResponseEmptyCB>) -> Error;
-
     #[no_mangle]
     pub fn indy_open_pool_ledger(command_handle: Handle,
                                  config_name: CString,
                                  config: CString,
                                  cb: Option<ResponseI32CB>) -> Error;
-
     #[no_mangle]
     pub fn indy_refresh_pool_ledger(command_handle: Handle,
                                     handle: Handle,
                                     cb: Option<ResponseEmptyCB>) -> Error;
-
     #[no_mangle]
     pub fn indy_list_pools(command_handle: Handle,
                            cb: Option<ResponseStringCB>) -> Error;
-
     #[no_mangle]
     pub fn indy_close_pool_ledger(command_handle: Handle,
                                   handle: Handle,
                                   cb: Option<ResponseEmptyCB>) -> Error;
-
     #[no_mangle]
     pub fn indy_delete_pool_ledger_config(command_handle: Handle,
                                           config_name: CString,
                                           cb: Option<ResponseEmptyCB>) -> Error;
+    #[no_mangle]
+    pub fn indy_set_protocol_version(command_handle: Handle,
+                                     protocol_version: usize,
+                                     cb: Option<ResponseEmptyCB>) -> Error;
 }
+
