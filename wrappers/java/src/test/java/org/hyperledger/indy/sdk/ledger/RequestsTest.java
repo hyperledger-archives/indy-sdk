25179b0951b18f79b03121e1e8ac31f8c033e26a
Sergey Minaev <sergey.minaev@dsr-company.com>
2018-06-26 18:42:28 +0300

    Fix tests in wrappers.
    Signed-off-by: Sergey Minaev <sergey.minaev@dsr-company.com>

--- wrappers/java/src/test/java/org/hyperledger/indy/sdk/ledger/RequestsTest.java
+++ wrappers/java/src/test/java/org/hyperledger/indy/sdk/ledger/RequestsTest.java
@@ -24,6 +24,7 @@ public class RequestsTest extends IndyIntegrationTestWithPoolAndSingleWallet {
 				"             \"type\":\"105\",\n" +
 				"             \"dest\":\"Th7MpTaRZVRYnPiabds81Y\"\n" +
 				"          },\n" +
+				"          \"protocolVersion\":2,\n" +
 				"          \"signature\":\"4o86XfkiJ4e2r3J6Ufoi17UU3W5Zi9sshV6FjBjkVw4sgEQFQov9dxqDEtLbAJAWffCWd5KfAk164QVo7mYwKkiV\"}";
 
 		String response = Ledger.submitRequest(pool, request).get();
