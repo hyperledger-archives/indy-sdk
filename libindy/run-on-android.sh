53c195d30ba3cef36b3de6a1a7ccaa5ac96dc7b7
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-06-29 12:34:52 +0530

    [INITIAL COMMIT] Android running tests using the runner in cargo
    Signed-off-by: Mohammad Abdul Sami <faisal00813@gmail.com>

---
+++ libindy/run-on-android.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+EXE="$1"
+EXE_NAME=`basename $EXE`
+#Before pushing to the adb link all the dependencies of the indy statically
+adb push "$EXE" "/data/local/tmp/$EXE_NAME"
+adb shell "chmod 755 /data/local/tmp/$EXE_NAME"
+OUT="$(mktemp)"
+MARK="ADB_SUCCESS!"
+adb shell "RUST_LOG=debug /data/local/tmp/$EXE_NAME && echo $MARK" 2>&1 | tee $OUT
+grep $MARK $OUT
