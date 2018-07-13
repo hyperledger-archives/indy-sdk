a6f8cca2dd48481083f7f76622e977ff2f665b2b
Norman Jarvis <norman.jarvis@gmail.com>
2018-07-12 20:42:20 -0600

    CO-1654 - Adding scripts to quickly build the cocoapod file for iOS (#367)
    * Adding the logic to reduce the size of the libvcx.a library file
    
    * Turning off the debug symbols in the libvcx Cargo.toml file so that the libraries are smaller
    
    * Added the echo statement for the vcx/wrappers/ios/vcx/lib folder which is where the cocoapod zip file is created from
    
    * Changing version 0.0.15 to point at different cocoapod zip file
    
    * Now the DATETIME does not need to be specified on the command line
    
    * Added a cocoapod containing the changes Naga made to add,delete,get wallet items
    
    * Reverting back to cocoapod that does not have the add,delete,get wallet items
    
    * Changed the launchd build to pass in a parameter for which splatforms to build for
    
    * Now the scripts will build just one architecture based on the parameter being passed in
    
    * Now I am pointing at an x86_64 only build of the cocoapod
    
    * Fixed the final step of the vcx.framework build to support a varied set of architectures
    
    * Fixed the script to not remove the needed vcx.framework directory
    
    * Another cocoapod build of the vcx library
    
    * Need to switch back over to libindy master in order to use libvcx master
    
    * Settings to trun on debuginfo in the build of libindy and libnullpay
    
    * The staticlib is now already in the list of crate-types
    
    * Adding a cocoapod for just x86_64 with all the debug symbols contained in the libvcx.a

--- libvcx/build_scripts/ios/mac/launchd.daemon.build.libvxc.sh
+++ libvcx/build_scripts/ios/mac/launchd.daemon.build.libvxc.sh
@@ -8,10 +8,14 @@ git pull > ./vcx/libvcx/build_scripts/ios/mac/git.pull.out 2>&1
 # git pull
 # git checkout mobile_vcx
 # git merge libindy_refactor
+#IOS_TARGETS="aarch64-apple-ios,armv7-apple-ios,armv7s-apple-ios,i386-apple-ios,x86_64-apple-ios"
+IOS_TARGETS="x86_64-apple-ios"
+#IOS_ARCHS="armv7,armv7s,arm64,i386,x86_64"
+IOS_ARCHS="x86_64"
 cd vcx/libvcx/build_scripts/ios/mac
-./mac.03.libindy.build.sh > ./mac.03.libindy.build.sh.out 2>&1
+./mac.03.libindy.build.sh debuginfo "${IOS_TARGETS}" > ./mac.03.libindy.build.sh.out 2>&1
 ./mac.04.libvcx.setup.sh > ./mac.04.libvcx.setup.sh.out 2>&1
-./mac.06.libvcx.build.sh > ./mac.06.libvcx.build.sh.out 2>&1
+./mac.06.libvcx.build.sh debuginfo "${IOS_TARGETS}" > ./mac.06.libvcx.build.sh.out 2>&1
 ./mac.11.copy.static.libs.to.app.sh > ./mac.11.copy.static.libs.to.app.sh.out 2>&1
-./mac.12.combine.static.libs.sh libvcxall delete > ./mac.12.combine.static.libs.sh.out 2>&1
-./mac.upload.ios.build.files.sh > ./mac.upload.ios.build.files.sh.out 2>&1
+./mac.12.combine.static.libs.sh libvcxall delete debuginfo "${IOS_ARCHS}" > ./mac.12.combine.static.libs.sh.out 2>&1
+./mac.upload.ios.build.files.sh "${IOS_ARCHS}" > ./mac.upload.ios.build.files.sh.out 2>&1
--- libvcx/build_scripts/ios/mac/mac.upload.ios.build.files.sh
+++ libvcx/build_scripts/ios/mac/mac.upload.ios.build.files.sh
@@ -10,11 +10,12 @@ WORK_DIR=$(abspath "$WORK_DIR")
 VCX_SDK=$START_DIR/../../../../..
 VCX_SDK=$(abspath "$VCX_SDK")
 
-DATETIME=$1
-if [ "$DATETIME" = "" ]; then
-    echo "You must pass the datetime as the first parameter to the script. (i.e. 20180522.1527 - YYYYmmdd.hhMM)"
-    exit 1
-fi
+# DATETIME=$1
+# if [ "$DATETIME" = "" ]; then
+#     echo "You must pass the datetime as the first parameter to the script. (i.e. 20180522.1527 - YYYYmmdd.hhMM)"
+#     exit 1
+# fi
+DATETIME=$(date +"%Y%m%d.%H%M")
 
 # cd $VCX_SDK/vcx/wrappers/ios/vcx/lib
 # mv libvcx.a libvcx.a.original
@@ -34,17 +35,43 @@ fi
 # 6) Now upload iOS .tar.gz and .zip files from the build as assets to servers...
 #    Just run the script /Users/iosbuild1/forge/work/code/evernym/sdk/vcx/libvcx/build_scripts/ios/mac/mac.upload.ios.build.files.sh
 
+IOS_ARCHS="armv7,armv7s,arm64,i386,x86_64"
+if [ ! -z "$1" ]; then
+    IOS_ARCHS=$1
+fi
+bkpIFS="$IFS"
+IFS=',()][' read -r -a archs <<<"${IOS_ARCHS}"
+echo "Building vcx.framework wrapper for architectures: ${archs[@]}"    ##Or printf "%s\n" ${array[@]}
+IFS="$bkpIFS"
+
 cd $VCX_SDK/vcx/wrappers/ios/vcx
 mv lib/libvcx.a lib/libvcx.a.original
 cp -v lib/libvcxall.a lib/libvcx.a
 xcodebuild -project vcx.xcodeproj -scheme vcx -configuration Debug CONFIGURATION_BUILD_DIR=. clean > $START_DIR/xcodebuild.vcx.framework.build.out 2>&1
-xcodebuild -project vcx.xcodeproj -scheme vcx -configuration Debug -sdk iphonesimulator CONFIGURATION_BUILD_DIR=. build >> $START_DIR/xcodebuild.vcx.framework.build.out 2>&1
-mv vcx.framework vcx.framework.iphonesimulator
-xcodebuild -project vcx.xcodeproj -scheme vcx -configuration Debug -sdk iphoneos CONFIGURATION_BUILD_DIR=. build >> $START_DIR/xcodebuild.vcx.framework.build.out 2>&1
-lipo -create -output combined.ios.vcx vcx.framework/vcx vcx.framework.iphonesimulator/vcx
-mv combined.ios.vcx vcx.framework/vcx
+
+IPHONE_SDK=iphoneos
+for arch in ${archs[*]}
+do
+    rm -rf vcx.framework
+    if [ "${arch}" = "i386" ] || [ "${arch}" = "x86_64" ]; then
+        # This sdk supports i386 and x86_64
+        IPHONE_SDK=iphonesimulator
+    elif [ "${arch}" = "armv7" ] || [ "${arch}" = "armv7s" ] || [ "${arch}" = "arm64" ]; then
+        # This sdk supports armv7, armv7s, and arm64
+        IPHONE_SDK=iphoneos
+    fi
+    xcodebuild -project vcx.xcodeproj -scheme vcx -configuration Debug -arch ${arch} -sdk ${IPHONE_SDK} CONFIGURATION_BUILD_DIR=. build >> $START_DIR/xcodebuild.vcx.framework.build.out 2>&1
+
+    if [ -d "./vcx.framework.previousbuild" ]; then
+        lipo -create -output combined.ios.vcx vcx.framework/vcx vcx.framework.previousbuild/vcx
+        mv combined.ios.vcx vcx.framework/vcx
+        rm -rf vcx.framework.previousbuild
+    fi
+    cp -rp vcx.framework vcx.framework.previousbuild
+done
+
 mv lib/libvcx.a.original lib/libvcx.a
-rm -rf vcx.framework.iphonesimulator
+rm -rf vcx.framework.previousbuild
 
 #cd $VCX_SDK/vcx/wrappers/ios/vcx
 #cp -v lib/libnullpay.a vcx.framework/lib/libnullpay.a
