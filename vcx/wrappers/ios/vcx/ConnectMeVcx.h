eb3d0c943ed04f545a33a47128c9afe201c6fbc6
tcasey <hello@tcasey.me>
2018-07-18 11:40:11 -0600

    fix: importWallet method params in ios & java wrappers
    Signed-off-by: tcasey <hello@tcasey.me>

--- vcx/wrappers/ios/vcx/ConnectMeVcx.h
+++ vcx/wrappers/ios/vcx/ConnectMeVcx.h
@@ -140,8 +140,7 @@ extern void VcxWrapperCommonNumberStringCallback(vcx_command_handle_t xcommand_h
             encryptWith:(NSString *)encryptionKey
            completion:(void (^)(NSError *error, NSInteger exportHandle))completion;
 
-- (void)importWallet:(NSString *)importPath
-            decryptWith:(NSString *)decryptionKey
+- (void)importWallet:(NSString *)config
            completion:(void (^)(NSError *error, NSInteger importHandle))completion;
 
 - (void)addRecordWallet:(NSString *)recordType
