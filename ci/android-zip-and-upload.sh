c29cc73652f17de3467a85a372965b4ab06dd59e
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-07-19 00:55:33 +0530

    Prep for CICD commit
    Signed-off-by: Mohammad Abdul Sami <faisal00813@gmail.com>

--- ci/android-zip-and-upload.sh
+++ ci/android-zip-and-upload.sh
@@ -4,7 +4,6 @@ if [ "$1" = "--help" ] ; then
   echo "Usage: <architecture> <version> <key> <branchName> <number>"
   return
 fi
-export ANDROID_BUILD_FOLDER="/tmp/android_build"
 
 arch="$1"
 version="$2"
@@ -22,6 +21,6 @@ ssh -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.115 mkdir -p /var/repo
 
 cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.115
 cd /var/repository/repos/android/libindy/$branchName/$version-$buildNumber
-put -r ../libindy/libindy_android_${arch}_${version}.zip
+put -r libindy/libindy_android_${arch}_${version}.zip
 ls -l /var/repository/repos/android/libindy/$branchName/$version-$buildNumber
 EOF
