e5e6c38b5a5ab012a7bb23957978290885fe135e
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-07-24 21:36:31 +0530

    Added branch name to publishing path
    Signed-off-by: Mohammad Abdul Sami <faisal00813@gmail.com>

--- ci/android-zip-and-upload.sh
+++ ci/android-zip-and-upload.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 if [ "$1" = "--help" ] ; then
-  echo "Usage: <architecture> <version> <key> <number> <artifact_name> <is_stable>"
+  echo "Usage: <architecture> <version> <key> <branchName> <number> <artifact_name> <is_stable>"
   return
 fi
 
 arch="$1"
 version="$2"
 key="$3"
-buildNumber="$4"
-artifact="$5"
-is_stable="$6"
+branchName="$4"
+buildNumber="$5"
+artifact="$6"
+is_stable="$7"
 
 ${arch:?'arch variable is not set'}
 ${version:?'version variable is not set'}
 ${key:?'key variable is not set'}
+${branchName:?'branchName variable is not set'}
 ${buildNumber:?'buildNumber variable is not set'}
 ${artifact:?'artifact variable is not set'}
 ${is_stable:?'is_stable variable is not set'}
@@ -28,7 +30,7 @@ cp -vr /var/repository/repos/android/${artifact}/rc/$version/${artifact}/${artif
 ls -l /var/repository/repos/android/${artifact}/stable/$version
 EOF
     else
-    ssh -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.115 mkdir -p /var/repository/repos/android/${artifact}/rc/${version}-${buildNumber}
+    ssh -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.115 mkdir -p /var/repository/repos/android/${artifact}/${branchName}/${version}-${buildNumber}
 
 cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.115
 cd /var/repository/repos/android/${artifact}/rc/$version-$buildNumber
