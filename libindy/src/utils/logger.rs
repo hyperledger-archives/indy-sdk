b624bc41c8b392ee8a2625bd74ab353773ad982b
Sergey Minaev <sergey.minaev@dsr-company.com>
2018-07-25 14:52:45 +0300

    Fix warnings.
    Signed-off-by: Sergey Minaev <sergey.minaev@dsr-company.com>

--- libindy/src/utils/logger.rs
+++ libindy/src/utils/logger.rs
@@ -5,7 +5,7 @@ extern crate log;
 extern crate android_logger;
 
 use self::env_logger::Builder;
-use self::log::{Record, LevelFilter, Level};
+use self::log::LevelFilter;
 use std::env;
 use std::io::Write;
 #[cfg(target_os = "android")]
