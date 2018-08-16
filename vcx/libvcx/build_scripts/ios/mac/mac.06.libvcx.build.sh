f7245db7b91eb8542366e30bfc8f4881219f2521
Norman Jarvis <norman.jarvis@evernym.com>
2018-08-16 12:31:16 -0600

    The most recent build of libsovtoken only has the universal libsovtoken.a file and so we need to extract it now
    Signed-off-by: Norman Jarvis <norman.jarvis@evernym.com>

--- vcx/libvcx/build_scripts/ios/mac/mac.06.libvcx.build.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.06.libvcx.build.sh
@@ -65,23 +65,34 @@ do
         target_arch="x86_64"
     fi
 
-    if [ -d $WORK_DIR/libindy/${target_arch} ]; then
+    libtool="/usr/bin/libtool"
+    libsovtoken_dir="${WORK_DIR}/libsovtoken-ios/libsovtoken"
+    libindy_dir="${WORK_DIR}/libindy"
+
+    if [ -d ${libsovtoken_dir}/${target_arch} ]; then
+        echo "${target_arch} libsovtoken architecture already extracted"
+    else
+        mkdir -p ${libsovtoken_dir}/${target_arch}
+        lipo -extract $target_arch ${libsovtoken_dir}/universal/libsovtoken.a -o ${libsovtoken_dir}/${target_arch}/libsovtoken.a
+        ${libtool} -static ${libsovtoken_dir}/${target_arch}/libsovtoken.a -o ${libsovtoken_dir}/${target_arch}/libsovtoken_libtool.a
+        mv ${libsovtoken_dir}/${target_arch}/libsovtoken_libtool.a ${libsovtoken_dir}/${target_arch}/libsovtoken.a
+    fi
+
+    if [ -d ${libindy_dir}/${target_arch} ]; then
         echo "${target_arch} libindy architecture already extracted"
     else
-        libtool="/usr/bin/libtool"
-        libindy_dir="${WORK_DIR}/libindy"
-        mkdir ${WORK_DIR}/libindy/${target_arch}
+        mkdir -p ${libindy_dir}/${target_arch}
         lipo -extract $target_arch ${libindy_dir}/libindy.a -o ${libindy_dir}/${target_arch}/libindy.a
         ${libtool} -static ${libindy_dir}/${target_arch}/libindy.a -o ${libindy_dir}/${target_arch}/libindy_libtool.a
         mv ${libindy_dir}/${target_arch}/libindy_libtool.a ${libindy_dir}/${target_arch}/libindy.a
-    fi 
+    fi
 
     export OPENSSL_LIB_DIR=$WORK_DIR/OpenSSL-for-iPhone/lib/${target_arch}
     export IOS_SODIUM_LIB=$WORK_DIR/libzmq-ios/libsodium-ios/dist/ios/lib/${target_arch}
     export IOS_ZMQ_LIB=$WORK_DIR/libzmq-ios/dist/ios/lib/${target_arch}
-    export LIBINDY_DIR=$WORK_DIR/libindy/${target_arch}
+    export LIBINDY_DIR=${libindy_dir}/${target_arch}
     #export LIBNULLPAY_DIR=$WORK_DIR/vcx-indy-sdk/libnullpay/target/${target}/release
-    export LIBSOVTOKEN_DIR=$WORK_DIR/libsovtoken-ios/libsovtoken/${target}
+    export LIBSOVTOKEN_DIR=${libsovtoken_dir}/${target_arch}
 
     # To build for macos
     #cargo build
@@ -90,7 +101,7 @@ do
     # To build for iOS
     #LIBINDY_DIR=/usr/local/lib RUST_BACKTRACE=1 cargo lipo --release
     #cargo lipo --release --verbose --targets="${IOS_TARGETS}"
-    
+
     # if [ -f "./target/universal/release/libvcx.a" ]; then
     #     mv ./target/universal/release/libvcx.a ./libvcx.previous.a
     # fi
@@ -117,4 +128,4 @@ export OPENSSL_LIB_DIR=$OPENSSL_LIB_DIR_DARWIN
 
 #cargo test
 
-#lipo -info 
+#lipo -info
