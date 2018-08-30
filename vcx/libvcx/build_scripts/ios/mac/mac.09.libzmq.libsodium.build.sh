94be3e25955198008a2663a2dab9ab7ad62ed178
Norman Jarvis <norman.jarvis@gmail.com>
2018-08-30 10:50:45 -0600

    Changed the libsodium version to 1.0.16 and the libzmq version to 4.2.5 for iOS (#480)
    * Changed the libsodium version to 1.0.16 and the libzmq version to 4.2.5
    
    * Added back in the commented out parts of the script that checkout the code
    
    Signed-off-by: Mark Hadley <mark.hadley@evernym.com>
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- vcx/libvcx/build_scripts/ios/mac/mac.09.libzmq.libsodium.build.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.09.libzmq.libsodium.build.sh
@@ -10,9 +10,9 @@ WORK_DIR=$(abspath "$WORK_DIR")
 if [ -d $WORK_DIR/libzmq-ios ]; then
     rm -rf $WORK_DIR/libzmq-ios
 fi
-git clone https://github.com/azawawi/libzmq-ios.git $WORK_DIR/libzmq-ios
+git clone https://github.com/evernym/libzmq-ios.git $WORK_DIR/libzmq-ios
 cd $WORK_DIR/libzmq-ios
-git clone https://github.com/azawawi/libsodium-ios.git $WORK_DIR/libzmq-ios/libsodium-ios
+git clone https://github.com/evernym/libsodium-ios.git $WORK_DIR/libzmq-ios/libsodium-ios
 cd $WORK_DIR/libzmq-ios/libsodium-ios
 ./libsodium.rb
 cd ..
