957ab1fa2bed7aaf9e13697083ba1453ded2ccf6
dastardlychimp <darien.hess@evernym.com>
2018-09-06 11:34:11 -0600

    TOK-396 Fix a few issues from merge.
    Signed-off-by: dastardlychimp <darien.hess@evernym.com>

--- tests/utils/constants.rs
+++ tests/utils/constants.rs
@@ -3,3 +3,4 @@ pub const TRUSTEE_SEED: &str = "000000000000000000000000Trustee1";
 pub const MY1_SEED: &str = "00000000000000000000000000000My1";
 pub const PROTOCOL_VERSION: i32 = 2;
 pub const EXPORT_KEY: &str = "export_key";
+pub const DID: &str = "8wZcEriaNLNKtteJvx7f8i";
