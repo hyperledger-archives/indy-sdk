2b6e4205a7b6aad38906e8763eee17c7a4706e9d
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2018-09-06 19:19:17 +0300

    Tests for open pool
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,10 +1,17 @@
-extern crate serde_json;
+extern crate rust_indy_sdk_wrapper as indy;
 
 use std::env;
 use std::path::PathBuf;
 use std::fs;
+use serde_json;
 
 pub mod constants;
+pub mod did;
+pub mod environment;
+pub mod pool;
+mod rand;
+pub mod setup;
+pub mod wallet;
 
 macro_rules! safe_wallet_create {
     ($x:ident) => {
