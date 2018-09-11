37d91fc6dd953fbcfc8201411121417255ece605
Norman Jarvis <norman.jarvis@evernym.com>
2018-09-11 16:21:16 -0600

    Turned on the iOS build for libindy 1.6.6-25 and to publish the cocoapod
    Signed-off-by: Mark Hadley <mark.hadley@evernym.com>
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- vcx/libvcx/build_scripts/ios/mac/shared.functions.sh
+++ vcx/libvcx/build_scripts/ios/mac/shared.functions.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 export LIBSOVTOKEN_IOS_BUILD_URL="https://repo.corp.evernym.com/filely/ios/libsovtoken_0.9.2-201809042322-b51bfba_all.zip"
-export LIBINDY_IOS_BUILD_URL="https://repo.sovrin.org/ios/libindy/stable/libindy-core/1.6.5/libindy.tar.gz"
+#export LIBINDY_IOS_BUILD_URL="https://repo.sovrin.org/ios/libindy/stable/libindy-core/1.6.5/libindy.tar.gz"
+export LIBINDY_IOS_BUILD_URL="https://repo.sovrin.org/ios/libindy/rc/libindy-core/1.6.6-25/libindy.tar.gz"
+
 export LIBSOVTOKEN_FILE=$(basename ${LIBSOVTOKEN_IOS_BUILD_URL})
 export LIBSOVTOKEN_VERSION=$(echo ${LIBSOVTOKEN_FILE} | cut -d'_' -f 2)
 export LIBINDY_FILE=$(basename ${LIBINDY_IOS_BUILD_URL})
