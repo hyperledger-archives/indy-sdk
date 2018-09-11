4700a8e06447430f9dba587e9609ea4612045162
matt raffel <matt.raffel@evernym.com>
2018-09-11 10:56:41 -0600

    Fixes wallet, updates pool genesis file
    Signed-off-by: matt raffel <matt.raffel@evernym.com>
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- wrappers/dotnet/indy-sdk-dotnet-test/Util/WalletUtils.cs
+++ wrappers/dotnet/indy-sdk-dotnet-test/Util/WalletUtils.cs
@@ -8,33 +8,22 @@ namespace Hyperledger.Indy.Test.Util
 {
     public static class WalletUtils
     {
-
-        private class CreateWalletConfig
-        {
-            public string id;
-        }
-
-        private class OpenWalletCredentials
-        {
-            public string key;
-        }
-
+    
         public static string GetCreateWalletConfig(string walletName)
         {
-            CreateWalletConfig config = new CreateWalletConfig() { id = walletName };
+            WalletConfig config = new WalletConfig() { id = walletName };
 
             return JsonConvert.SerializeObject(config, Formatting.Indented);
         }
 
         public static string GetOpenWalletCredentials(string key)
         {
-            OpenWalletCredentials cred = new OpenWalletCredentials() { key = key };
+            Credentials cred = new Credentials() { key = key };
             return JsonConvert.SerializeObject(cred, Formatting.Indented);
         }
 
         public static async Task CreateDefaultWallet(string pool, string wallet)
         {
-
             string config = WalletUtils.GetCreateWalletConfig(wallet);
             await Wallet.CreateWalletAsync(pool, config);
         }
