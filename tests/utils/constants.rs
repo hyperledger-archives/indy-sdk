42e6c4f28908a4c853b07126cb87161053cff075
dastardlychimp <darien.hess@evernym.com>
2018-09-11 14:01:10 -0600

    TOK-393 Add `Did::abbreviate_verkey` tests.
    Signed-off-by: dastardlychimp <darien.hess@evernym.com>

--- tests/utils/constants.rs
+++ tests/utils/constants.rs
@@ -3,6 +3,7 @@ pub const DID_1: &str = "VsKV7grR1BUE29mG2Fm2kX";
 pub const EXPORT_KEY: &str = "export_key";
 pub const MY1_SEED: &str = "00000000000000000000000000000My1";
 pub const PROTOCOL_VERSION: i32 = 2;
+pub const VERKEY_ABV_1: &str = "~HYwqs2vrTc8Tn4uBV7NBTe";
 pub const DID: &str = "8wZcEriaNLNKtteJvx7f8i";
 pub const DID_TRUSTEE: &str = "V4SGRU86Z58d6TV7PBUe6f";
 pub const VERKEY_TRUSTEE: &str = "GJ1SzoWzavQYfNL9XkaJdrQejfztN4XqdsiV4ct3LXKL";
