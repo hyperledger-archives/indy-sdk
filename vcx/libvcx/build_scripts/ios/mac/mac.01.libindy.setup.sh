43be2698fda4e1fbd601b7f7e4dad0aa9cecc9ac
Norman Jarvis <norman.jarvis@evernym.com>
2018-09-12 21:50:44 -0600

    Commented out the Android build and added an echo logging statement and set to build only one iOS target for now
    Signed-off-by: Mark Hadley <mark.hadley@evernym.com>
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- vcx/libvcx/build_scripts/ios/mac/mac.01.libindy.setup.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.01.libindy.setup.sh
@@ -46,7 +46,7 @@ if [[ $RUSTUP_VERSION =~ ^'rustup ' ]]; then
     rustup default 1.27.0
     rustup update
     rustup component add rls-preview rust-analysis rust-src
-
+    echo "Using rustc version $(rustc --version)"
     rustup target remove aarch64-linux-android armv7-linux-androideabi arm-linux-androideabi i686-linux-android x86_64-linux-android
     rustup target add aarch64-apple-ios armv7-apple-ios armv7s-apple-ios x86_64-apple-ios i386-apple-ios
 
