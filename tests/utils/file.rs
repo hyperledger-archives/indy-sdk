6af7cd0738d250a28fb9e9d31cf491f9034bea0f
dastardlychimp <darien.hess@evernym.com>
2018-09-13 09:18:58 -0600

    TOK-424 Add `Wallet::create` tests.
    Added `TempDir` to `utils::file`.
    Still need test for creating a wallet with a custom registered wallet.
    
    Signed-off-by: dastardlychimp <darien.hess@evernym.com>
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- tests/utils/file.rs
+++ tests/utils/file.rs
@@ -1,10 +1,9 @@
 use std::env;
 use std::fs;
-use std::fs::File;
+use std::fs::{File, DirBuilder};
 use std::io;
 use std::path::{Path, PathBuf};
 use utils::rand;
-
 
 pub struct TempFile {
     path: PathBuf,
@@ -12,22 +11,13 @@ pub struct TempFile {
 
 impl TempFile {
     pub fn new(file_name: Option<&str>) -> io::Result<TempFile> {
-        let mut path = env::temp_dir();
-
-        if let Some(name) = file_name {
-            path.push(name);
-        } else {
-            let name = format!("TempFile{}", rand::random_string(12));
-            path.push(name);
-        }
+        let path = generate_temp_path(file_name, "TempFile_");
 
         let _file = File::create(&path)?;
 
-        let tempfile = TempFile {
+        Ok(TempFile {
             path: path
-        };
-
-        Ok(tempfile)
+        })
     }
 }
 
@@ -41,6 +31,47 @@ impl Drop for TempFile {
     fn drop(&mut self) {
         let _ = fs::remove_file(&self.path);
     }
+}
+
+pub struct TempDir {
+    path: PathBuf
+}
+
+impl TempDir {
+    pub fn new(dir_name: Option<&str>) -> io::Result<TempDir> {
+        let path = generate_temp_path(dir_name, "TempDir_");
+
+        fs::create_dir(&path)?;
+
+        Ok(TempDir {
+            path: path
+        })
+    }
+}
+
+impl AsRef<Path> for TempDir {
+    fn as_ref(&self) -> &Path {
+        self.path.as_ref()
+    }
+}
+
+impl Drop for TempDir {
+    fn drop(&mut self) {
+        fs::remove_dir_all(&self.path).unwrap();
+    }
+}
+
+fn generate_temp_path(name: Option<&str>, prefix: &str) -> PathBuf {
+    let mut path = env::temp_dir();
+
+    if let Some(name) = name {
+        path.push(name);
+    } else {
+        let name = format!("{}{}", prefix, rand::random_string(12));
+        path.push(name);
+    }
+
+    path
 }
 
 
@@ -74,4 +105,19 @@ mod test_temp_file {
 
         assert_eq!(CONTENT.as_bytes(), contents.as_slice());
     }
+
+    #[test]
+    fn temp_dir() {
+        let name = "test_dir";
+        let mut path = env::temp_dir();
+        path.push(name);
+
+        {
+            let dir = TempDir::new(Some(name)).unwrap();
+            assert!(path.exists());
+        }
+
+        assert!(! path.exists());
+    }
+
 }
