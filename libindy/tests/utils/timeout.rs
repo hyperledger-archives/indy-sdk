76e353ac8993273d7e4bcc0b8059471d9d790002
Sergey Minaev <sergey.minaev@dsr-corporation.com>
2018-09-17 16:26:36 +0300

    Increase timeouts for android.
    Signed-off-by: Sergey Minaev <sergey.minaev@dsr-corporation.com>

--- libindy/tests/utils/timeout.rs
+++ libindy/tests/utils/timeout.rs
@@ -1,13 +1,13 @@
 use std::time::Duration;
 
 pub fn short_timeout() -> Duration {
-    Duration::from_secs(5)
+    Duration::from_secs(300)
 }
 
 pub fn medium_timeout() -> Duration {
-    Duration::from_secs(20)
+    Duration::from_secs(300)
 }
 
 pub fn long_timeout() -> Duration {
-    Duration::from_secs(200)
+    Duration::from_secs(500)
 }
