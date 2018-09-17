cbcae9c1bd45414960d38cc7beba52b941bc6051
Sergey Minaev <sergey.minaev@dsr-corporation.com>
2018-09-17 12:17:31 +0300

    Enable testing on emulator for Android CI.
    Signed-off-by: Sergey Minaev <sergey.minaev@dsr-corporation.com>

--- libindy/android.test.sh
+++ libindy/android.test.sh
@@ -15,6 +15,8 @@ if [ -z "${TARGET_ARCH}" ]; then
     exit 1
 fi
 
+set -e
+
 source ${CI_DIR}/setup.android.env.sh
 generate_arch_flags ${TARGET_ARCH}
 
@@ -26,6 +28,8 @@ build_test_artifacts(){
     pushd ${WORKDIR}
 
         cargo clean
+        RUSTFLAGS="-L${TOOLCHAIN_DIR}/sysroot/usr/lib -lz -L${TOOLCHAIN_DIR}/${TRIPLET}/lib -L${LIBZMQ_LIB_DIR} -L${SODIUM_LIB_DIR} -lsodium -lzmq -lgnustl_shared" \
+                    cargo test --target=${TRIPLET} --no-run
         EXE_ARRAY=($( RUSTFLAGS="-L${TOOLCHAIN_DIR}/sysroot/usr/lib -lz -L${TOOLCHAIN_DIR}/${TRIPLET}/lib -L${LIBZMQ_LIB_DIR} -L${SODIUM_LIB_DIR} -lsodium -lzmq -lgnustl_shared" \
                     cargo test --target=${TRIPLET} --no-run --message-format=json | jq -r "select(.profile.test == true) | .filenames[]"))
     popd
@@ -62,7 +66,7 @@ execute_on_device(){
         adb shell "chmod 755 /data/local/tmp/$EXE_NAME"
         OUT="$(mktemp)"
         MARK="ADB_SUCCESS!"
-        adb shell "LD_LIBRARY_PATH=/data/local/tmp RUST_TEST_THREADS=1 RUST_LOG=debug /data/local/tmp/$EXE_NAME && echo $MARK" 2>&1 | tee $OUT
+        adb shell "TEST_POOL_IP=10.0.0.2 LD_LIBRARY_PATH=/data/local/tmp RUST_TEST_THREADS=1 RUST_LOG=debug /data/local/tmp/$EXE_NAME && echo $MARK" 2>&1 | tee $OUT
         grep $MARK $OUT
     done
 
