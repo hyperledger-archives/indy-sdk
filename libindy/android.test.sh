ff7a084044352ee3345e800681014c33ceb2c267
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-09-20 16:41:50 +0530

    Reverted exe array
    Signed-off-by: Mohammad Abdul Sami <faisal00813@gmail.com>

--- libindy/android.test.sh
+++ libindy/android.test.sh
@@ -30,10 +30,9 @@ build_test_artifacts(){
         set -e
         cp "${TOOLCHAIN_PREFIX}/android-ndk-r16b/platforms/android-21/arch-${TARGET_ARCH}/usr/lib/libc.so" "${TOOLCHAIN_DIR}/sysroot/usr/lib"
         cargo clean
-        RUSTFLAGS="-L${TOOLCHAIN_DIR}/sysroot/usr/lib -lc -lz -L${TOOLCHAIN_DIR}/${TRIPLET}/lib -L${LIBZMQ_LIB_DIR} -L${SODIUM_LIB_DIR} -lsodium -lzmq -lgnustl_shared" \
-                    cargo test --release --target=${TRIPLET} --no-run
-#        EXE_ARRAY=($( RUSTFLAGS="-L${TOOLCHAIN_DIR}/sysroot/usr/lib -lz -L${TOOLCHAIN_DIR}/${TRIPLET}/lib -L${LIBZMQ_LIB_DIR} -L${SODIUM_LIB_DIR} -lsodium -lzmq -lgnustl_shared" \
-#                    cargo test --release --target=${TRIPLET} --no-run --message-format=json | jq -r "select(.profile.test == true) | .filenames[]"))
+
+        EXE_ARRAY=($( RUSTFLAGS="-L${TOOLCHAIN_DIR}/sysroot/usr/lib -lc -lz -L${TOOLCHAIN_DIR}/${TRIPLET}/lib -L${LIBZMQ_LIB_DIR} -L${SODIUM_LIB_DIR} -lsodium -lzmq -lgnustl_shared" \
+                    cargo test --release --target=${TRIPLET} --no-run --message-format=json | jq -r "select(.profile.test == true) | .filenames[]"))
     popd
 }
 
