5f0543bc83734bbe5a62badfa68fb0f0719cec5a
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-09-26 20:47:07 +0530

    Bugfix[EN-833] - Android publishing was silently failing (#528)
    * Added connection tests to java wrapper
    
    * Added more tests to Connection suite
    
    * Fixes:
    Removed enums
    Deprecated Api with misleading name.
    Some api level fixes
    New:
    Test suite for ConnectionApi is complete
    
    * Added unit tests for Credential Apis
    Upgraded from junit 4.12 to 5.0
    Moved exception classes to the relevant packages
    
    * General refactoring
    
    * General refactoring
    Added credentialdef api tests
    
    * Modified Jenkinsfile so that java unit tests execute.
    Refactored android.dockerfile so that it is more aligned to the other docker files.
    
    * CI gradle bugfix in docker
    
    * CI Bugfix
    
    * CI Bugfix
    
    * Removed android.dockerfile
    
    * Jenkins file fix
    
    * docker file fix for android
    
    * Seperated android and java dockerfiles
    
    * Added rust to android dockerfile
    
    * Fixed rust version in android dockerfile
    
    * Fixed cargo path android dockerfile
    
    * Fix in android dockerfile
    
    * CI sll platforms restore
    
    * Added tests for vcxApi
    
    * Added --no-deamon flag to gradlew
    
    * Fixed ndk.dir and sdk.dir path.
    
    * Added set -e flag so that scripts should not fail silently anyomre.
    
    * Removed reduntant and empty docker file
    
    Signed-off-by: Mark Hadley <mark.hadley@evernym.com>

--- vcx/wrappers/java/src/test/java/com/evernym/sdk/vcx/ConnectionApiTest.java
+++ vcx/wrappers/java/src/test/java/com/evernym/sdk/vcx/ConnectionApiTest.java
@@ -1,117 +1,195 @@
-//package com.evernym.sdk.vcx;
-//
-//import android.util.Log;
-//
-//import com.evernym.sdk.vcx.connection.ConnectionApi;
-//import com.evernym.sdk.vcx.connection.InvalidConnectionHandleException;
-//import com.evernym.sdk.vcx.vcx.VcxApi;
-//
-//import org.awaitility.Awaitility;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.util.concurrent.ExecutionException;
-//
-//import java9.util.concurrent.CompletableFuture;
-//import java.util.concurrent.Callable;
-//
-//import org.awaitility.Awaitility.*;
-//import org.awaitility.Duration.*;
-//
-//import java.util.concurrent.TimeUnit.*;
-//
-//import org.hamcrest.Matchers.*;
-//import org.junit.Assert.*;
-//
-//import static junit.framework.Assert.*;
-//
-//public class ConnectionApiTest {
-//
-//    @Before
-//    public void setup() throws VcxException, ExecutionException, InterruptedException {
-//
-//        if (!TestHelper.vcxInitialized) {
-//            CompletableFuture<Integer> result = VcxApi.vcxInit(TestHelper.VCX_CONFIG_TEST_MODE);
-//            result.get();
-//            TestHelper.vcxInitialized = true;
-//        }
-//
-//    }
-//
-//    private int _createConnection() throws VcxException {
-//        CompletableFuture<Integer> futureResult = ConnectionApi.vcxConnectionCreate(TestHelper.getConnectionId());
-//        Awaitility.await().until(futureResult::isDone);
-//
-//        Integer result = futureResult.getNow(-1);
-//        if(result == -1){
-//            throw new VcxException("Unable to create connection handle",0);
-//        }else{
-////            System.out.println("Connection created with connection handle => "  + result);
-//            return result;
-//        }
-//
-//    }
-//
-//    @Test
-//    public void createConnection() throws VcxException {
-//
-//        long ConnectionHandle = _createConnection();
-//        assertNotSame(null,ConnectionHandle);
-//        assertNotSame( 0,ConnectionHandle);
-//    }
-//
-//    @Test
-//    public void createConnectionWithoutPhone() throws VcxException {
-//        String payload= "{ 'connection_type': 'SMS' }";
-//        Integer ConnectionHandle = _createConnection();
-//        CompletableFuture<String> future = ConnectionApi.vcxAcceptInvitation(ConnectionHandle,TestHelper.convertToValidJson(payload));
-//        Awaitility.await().until(future::isDone);
-//        assertNotSame("",future.getNow(""));
-//    }
-//
-//    @Test
-//    public void createConnectionWithPhone() throws VcxException {
-//        String payload= "{ 'connection_type': 'SMS', 'phone':'7202200000' }";
-//        Integer ConnectionHandle = _createConnection();
-//        CompletableFuture<String> future = ConnectionApi.vcxAcceptInvitation(ConnectionHandle,TestHelper.convertToValidJson(payload));
-//        Awaitility.await().until(future::isDone);
-//        assertNotSame("",future.getNow(""));
-//
-//
-//    }
-//
-//    @Test(expected = InvalidConnectionHandleException.class)
-//    public void throwInvalidConnectionHandleException() throws VcxException {
-//        String payload= "{ 'connection_type': 'SMS', 'phone':'7202200000' }";
-//        CompletableFuture<String> future = ConnectionApi.vcxAcceptInvitation(8765,TestHelper.convertToValidJson(payload));
-//        Awaitility.await().until(future::isDone);
-//        assertNotSame("",future.getNow(""));
-//    }
-//
-//    @Test
-//    public void serializeConnection() throws VcxException {
-//        Integer ConnectionHandle = _createConnection();
-//        CompletableFuture<String> future = ConnectionApi.connectionSerialize(ConnectionHandle);
-//        Awaitility.await().until(future::isDone);
-//        String serializedJson = future.getNow("");
-//        System.out.println(serializedJson);
-//        assertNotSame("",serializedJson);
-//        assert(serializedJson.contains("version"));
-//        assert(serializedJson.contains("data"));
-//    }
-//
-//    @Test
-//    public void deserializeConnection() throws VcxException {
-//        Integer ConnectionHandle = _createConnection();
-//        CompletableFuture<String> future = ConnectionApi.connectionSerialize(ConnectionHandle);
-//        Awaitility.await().until(future::isDone);
-//        String serializedJson = future.getNow("");
-//        System.out.println(serializedJson);
-//        CompletableFuture<Integer> deserializeFuture = ConnectionApi.connectionDeserialize(serializedJson);
-//        Awaitility.await().until(deserializeFuture::isDone);
-//        assertEquals(ConnectionHandle,deserializeFuture.getNow(-1));
-//
-//    }
-//
-//
-//}
+package com.evernym.sdk.vcx;
+
+
+import com.evernym.sdk.vcx.connection.ConnectionApi;
+import com.evernym.sdk.vcx.connection.InvalidConnectionHandleException;
+import com.evernym.sdk.vcx.vcx.VcxApi;
+import java9.util.concurrent.CompletableFuture;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+
+import static com.evernym.sdk.vcx.TestHelper._createConnection;
+import static junit.framework.Assert.assertNotSame;
+
+class ConnectionApiTest {
+
+    @BeforeEach
+    void setup() throws Exception {
+        System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "DEBUG");
+        if (!TestHelper.vcxInitialized) {
+            TestHelper.getResultFromFuture(VcxApi.vcxInit(TestHelper.VCX_CONFIG_TEST_MODE));
+            TestHelper.vcxInitialized = true;
+        }
+    }
+
+
+    @Test
+    @DisplayName("create a connection")
+    void createConnection() throws VcxException {
+
+        long connectionHandle = _createConnection();
+        assertNotSame(null,connectionHandle);
+        assertNotSame( 0,connectionHandle);
+    }
+
+    @Test
+    @DisplayName("connect connection without phone number")
+    void connectConnectionWithoutPhone() throws VcxException {
+        String payload= "{ 'connection_type': 'SMS' }";
+        Integer connectionHandle = _createConnection();
+        CompletableFuture<String> future = ConnectionApi.vcxConnectionConnect(connectionHandle,TestHelper.convertToValidJson(payload));
+        Awaitility.await().until(future::isDone);
+        assertNotSame("",future.getNow(""));
+    }
+
+    @Test
+    @DisplayName("connect connection with phone number")
+    void connectConnectionWithPhone() throws VcxException {
+        String payload= "{ 'connection_type': 'SMS', 'phone':'7202200000' }";
+        Integer connectionHandle = _createConnection();
+        CompletableFuture<String> future = ConnectionApi.vcxConnectionConnect(connectionHandle,TestHelper.convertToValidJson(payload));
+        Awaitility.await().until(future::isDone);
+        assertNotSame("",future.getNow(""));
+
+
+    }
+
+    @Test
+    @DisplayName("throw invalid connection handle exception for wrong handle")
+    void throwInvalidConnectionHandleException() {
+
+        Assertions.assertThrows(InvalidConnectionHandleException.class, ()-> {
+            String payload= "{ 'connection_type': 'SMS', 'phone':'7202200000' }";
+            CompletableFuture<String> future = ConnectionApi.vcxConnectionConnect(8765,TestHelper.convertToValidJson(payload));
+            Awaitility.await().until(future::isDone);
+            assertNotSame("",future.getNow(""));
+        });
+
+
+    }
+
+    @Test
+    @DisplayName("serialize a connection")
+    void serializeConnection() throws VcxException {
+        Integer connectionHandle = _createConnection();
+        CompletableFuture<String> future = ConnectionApi.connectionSerialize(connectionHandle);
+        Awaitility.await().until(future::isDone);
+        String serializedJson = future.getNow("");
+        System.out.println(serializedJson);
+        assertNotSame("",serializedJson);
+        assert(serializedJson.contains("version"));
+        assert(serializedJson.contains("data"));
+    }
+
+    @Test
+    @DisplayName("throw invalid connection handle exception for serializing invalid connection ")
+    void serializeConnectionWithBadHandle() {
+        Assertions.assertThrows(InvalidConnectionHandleException.class, ()-> {
+            CompletableFuture<String> future = ConnectionApi.connectionSerialize(0);
+            Awaitility.await().until(future::isDone);
+        });
+
+    }
+
+    @Test
+    @DisplayName("delete a connection")
+    void deleteConnection() throws VcxException, ExecutionException, InterruptedException {
+        Integer connectionHandle = _createConnection();
+        CompletableFuture<Integer> futureDelete= ConnectionApi.deleteConnection(connectionHandle);
+        Awaitility.await().until(futureDelete::isDone);
+        assert(futureDelete.get() == 0);
+    }
+
+    @Test
+    @DisplayName("throw invalid connection handle exception if trying to serialize deleted connection ")
+    void serlializeDeletedConnection() {
+
+        Assertions.assertThrows(InvalidConnectionHandleException.class, ()-> {
+            Integer connectionHandle = _createConnection();
+            CompletableFuture<Integer> futureDelete= ConnectionApi.deleteConnection(connectionHandle);
+            Awaitility.await().until(futureDelete::isDone);
+            CompletableFuture<String> future = ConnectionApi.connectionSerialize(connectionHandle);
+            Awaitility.await().until(future::isDone);
+        });
+
+    }
+
+    @Test
+    @DisplayName("throw invalid connection handle exception if trying to serialize released connection")
+    void serlializeReleasedConnection() {
+        Assertions.assertThrows(InvalidConnectionHandleException.class, ()-> {
+            Integer connectionHandle = _createConnection();
+            int releaseResult= ConnectionApi.connectionRelease(connectionHandle);
+            assert(releaseResult == 0 );
+            CompletableFuture<String> future = ConnectionApi.connectionSerialize(connectionHandle);
+            Awaitility.await().until(future::isDone);
+        });
+    }
+
+    @Test
+    @DisplayName("release a connection")
+    void releaseConnection() throws VcxException {
+        Integer connectionHandle = _createConnection();
+        int result= ConnectionApi.connectionRelease(connectionHandle);
+        assert(result == 0 );
+    }
+
+    @Test
+    @DisplayName("initialise a connction")
+    void initialiseConnection() throws VcxException, ExecutionException, InterruptedException {
+        Integer connectionHandle = _createConnection();
+        CompletableFuture<Integer> futureUpdateState= ConnectionApi.vcxConnectionUpdateState(connectionHandle);
+        Awaitility.await().until(futureUpdateState::isDone);
+        int updateStateResult = futureUpdateState.get();
+        assert(updateStateResult== 1 );
+        CompletableFuture<Integer> futureGetState= ConnectionApi.connectionGetState(connectionHandle);
+        Awaitility.await().until(futureGetState::isDone);
+        assert(futureGetState.get()== updateStateResult);
+
+    }
+    @Test
+    @DisplayName("send offer connection")
+    void sendOfferConnection() throws VcxException, ExecutionException, InterruptedException {
+        String payload= "{ 'connection_type': 'SMS', 'phone':'7202200000' }";
+        Integer connectionHandle = _createConnection();
+        CompletableFuture<String> future = ConnectionApi.vcxConnectionConnect(connectionHandle,TestHelper.convertToValidJson(payload));
+        Awaitility.await().until(future::isDone);
+        CompletableFuture<Integer> futureGetState= ConnectionApi.connectionGetState(connectionHandle);
+        Awaitility.await().until(futureGetState::isDone);
+        int connectionState = futureGetState.get();
+        assert(connectionState == 2);
+    }
+
+    @Test
+    @DisplayName("get abbreviated invite detials")
+    void inviteDetailsAbbreviatedConnection() throws VcxException, ExecutionException, InterruptedException {
+        String payload= "{ 'connection_type': 'SMS', 'phone':'7202200000' }";
+        int connectionHandle = _createConnection();
+        CompletableFuture<String> acceptInvitation = ConnectionApi.vcxConnectionConnect(connectionHandle,TestHelper.convertToValidJson(payload));
+        Awaitility.await().until(acceptInvitation::isDone);
+        CompletableFuture<String> detials = ConnectionApi.connectionInviteDetails(connectionHandle,1);
+        Awaitility.await().until(detials::isDone);
+        assert(detials.get().contains("dp"));
+
+    }
+
+    @Test
+    @DisplayName("get un-abbreviated invite detials")
+    void inviteDetailsUnAbbreviatedConnection() throws VcxException, ExecutionException, InterruptedException {
+        String payload= "{ 'connection_type': 'SMS', 'phone':'7202200000' }";
+        int connectionHandle = _createConnection();
+        CompletableFuture<String> acceptInvitation = ConnectionApi.vcxConnectionConnect(connectionHandle,TestHelper.convertToValidJson(payload));
+        Awaitility.await().until(acceptInvitation::isDone);
+        CompletableFuture<String> detials = ConnectionApi.connectionInviteDetails(connectionHandle,0);
+        Awaitility.await().until(detials::isDone);
+        assert(detials.get().contains("senderAgencyDetail"));
+
+    }
+
+
+}
