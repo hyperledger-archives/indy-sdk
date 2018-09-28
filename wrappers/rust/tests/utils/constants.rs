ab7ab777258434c543d2ddeb85a5ae4b998743c6
dastardlychimp <darien.hess@evernym.com>
2018-09-28 08:13:33 -0600

    Add rust wrapper wallet tests.
    ## Changes in the rust wrapper
    
    Added tests for the rust wrapper's wallet module.
    
    There are not any tests for registering a wallet right now.
    
    Deleted a couple methods that were unused.
    
    Ordered the constants alphabetically.
    
    Signed-off-by: dastardlychimp <darien.hess@evernym.com>
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- wrappers/rust/tests/utils/constants.rs
+++ wrappers/rust/tests/utils/constants.rs
@@ -1,13 +1,13 @@
 pub const DEFAULT_CREDENTIALS: &str = r#"{"key":""}"#;
 pub const DID_1: &str = "VsKV7grR1BUE29mG2Fm2kX";
-pub const EXPORT_KEY: &str = "export_key";
+pub const DID: &str = "8wZcEriaNLNKtteJvx7f8i";
+pub const DID_TRUSTEE: &str = "V4SGRU86Z58d6TV7PBUe6f";
+pub const INVALID_HANDLE: i32 = 583741;
+pub const METADATA: &str = "some_metadata";
 pub const MY1_SEED: &str = "00000000000000000000000000000My1";
 pub const PROTOCOL_VERSION: i32 = 2;
 pub const SEED_1: &str = "00000000000000000000000000000My1";
 pub const TRUSTEE_SEED: &str = "000000000000000000000000Trustee1";
 pub const VERKEY_1: &str = "GjZWsBLgZCR18aL468JAT7w9CZRiBnpxUPPgyQxh4voa";
 pub const VERKEY_ABV_1: &str = "~HYwqs2vrTc8Tn4uBV7NBTe";
-pub const DID: &str = "8wZcEriaNLNKtteJvx7f8i";
-pub const DID_TRUSTEE: &str = "V4SGRU86Z58d6TV7PBUe6f";
 pub const VERKEY_TRUSTEE: &str = "GJ1SzoWzavQYfNL9XkaJdrQejfztN4XqdsiV4ct3LXKL";
-pub const METADATA: &str = "some_metadata";
