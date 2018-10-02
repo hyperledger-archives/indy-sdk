b19f071d8794c2015bbca135b4487dc111db04de
Ian Costanzo <ian@anon-solutions.ca>
2018-10-02 10:13:47 -0700

    Re-factor in-mem storage sample to stand-alone library

---
+++ samples/storage/storage-inmem/src/lib.rs
@@ -0,0 +1,47 @@
+#![cfg_attr(feature = "fatal_warnings", deny(warnings))]
+
+extern crate base64;
+
+//#[macro_use]
+//extern crate log;
+
+//extern crate serde;
+
+//#[macro_use]
+//extern crate serde_derive;
+
+//#[macro_use]
+extern crate serde_json;
+
+//extern crate rmp_serde;
+
+#[macro_use]
+extern crate lazy_static;
+
+//extern crate named_type;
+
+//#[macro_use]
+//extern crate named_type_derive;
+
+extern crate indy;
+extern crate indy_crypto;
+extern crate libc;
+extern crate rand;
+
+// Note that to use macroses from util inside of other modules it must me loaded first!
+#[macro_use]
+pub mod utils;
+
+//pub mod api;
+//mod errors;
+
+#[cfg(test)]
+mod tests {
+    //use super::*;
+
+    #[test]
+    fn dummy() {
+        assert!(true, "Dummy check!");
+    }
+}
+
