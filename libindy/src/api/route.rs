e5c97ec8ca503c3c488d281a4ed1a9333533769b
Kyle Den Hartog <kdenhar@gmail.com>
2018-10-08 17:51:35 -0600

    rustfmt code; fixed first test; needs further updates to unpack return data
    Signed-off-by: Kyle Den Hartog <kdenhar@gmail.com>

--- libindy/src/api/route.rs
+++ libindy/src/api/route.rs
@@ -1,23 +1,22 @@
 extern crate libc;
 
 use api::ErrorCode;
-use errors::ToErrorCode;
-use commands::{Command, CommandExecutor};
 use commands::route::RouteCommand;
+use commands::{Command, CommandExecutor};
+use errors::ToErrorCode;
 use utils::ctypes;
 
 use self::libc::c_char;
 
 #[no_mangle]
-pub fn indy_auth_pack_message(command_handle: i32,
-                     wallet_handle: i32,
-                     message: *const c_char,
-                     recv_keys: *const c_char,
-                     my_vk: *const c_char,
-                     cb: Option<extern fn(xcommand_handle: i32,
-                                          err: ErrorCode,
-                                          ames: *const c_char)>) -> ErrorCode {
-
+pub fn indy_auth_pack_message(
+    command_handle: i32,
+    wallet_handle: i32,
+    message: *const c_char,
+    recv_keys: *const c_char,
+    my_vk: *const c_char,
+    cb: Option<extern "C" fn(xcommand_handle: i32, err: ErrorCode, ames: *const c_char)>,
+) -> ErrorCode {
     trace!("indy_auth_pack_message: >>> wallet_handle: {:?}, message: {:?}, recv_keys: {:?}, my_vk: {:?}",
            wallet_handle, message, recv_keys, my_vk);
 
@@ -29,19 +28,22 @@ pub fn indy_auth_pack_message(command_handle: i32,
     trace!("indy_auth_pack_message: entities >>> wallet_handle: {:?}, message: {:?}, recv_keys: {:?}, my_vk: {:?}",
            wallet_handle, message, recv_keys, my_vk);
 
-    let result = CommandExecutor::instance()
-    .send(Command::Route(RouteCommand::AuthPackMessage(
+    let result = CommandExecutor::instance().send(Command::Route(RouteCommand::AuthPackMessage(
         message,
         recv_keys,
         my_vk,
         wallet_handle,
         Box::new(move |result| {
             let (err, ames) = result_to_err_code_1!(result, String::new());
-            trace!("indy_auth_pack_message: cb command_handle: {:?}, err: {:?}, ames: {:?}",
-                   command_handle, err, ames);
+            trace!(
+                "indy_auth_pack_message: cb command_handle: {:?}, err: {:?}, ames: {:?}",
+                command_handle,
+                err,
+                ames
+            );
             let ames = ctypes::string_to_cstring(ames);
             cb(command_handle, err, ames.as_ptr())
-        })
+        }),
     )));
 
     let res = result_to_err_code!(result);
@@ -52,36 +54,42 @@ pub fn indy_auth_pack_message(command_handle: i32,
 }
 
 #[no_mangle]
-pub fn indy_anon_pack_message(command_handle: i32,
-                              message: *const c_char,
-                              recv_keys: *const c_char,
-                              cb: Option<extern fn(xcommand_handle: i32,
-                                                   err: ErrorCode,
-                                                   ames: *const c_char)>) -> ErrorCode {
-
-
-    trace!("indy_anon_pack_message: >>> message: {:?}, recv_keys: {:?}",
-           message, recv_keys);
+pub fn indy_anon_pack_message(
+    command_handle: i32,
+    message: *const c_char,
+    recv_keys: *const c_char,
+    cb: Option<extern "C" fn(xcommand_handle: i32, err: ErrorCode, ames: *const c_char)>,
+) -> ErrorCode {
+    trace!(
+        "indy_anon_pack_message: >>> message: {:?}, recv_keys: {:?}",
+        message,
+        recv_keys
+    );
 
     check_useful_c_str!(message, ErrorCode::CommonInvalidParam3);
     check_useful_c_str!(recv_keys, ErrorCode::CommonInvalidParam4);
     check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam5);
 
-    trace!("indy_anon_pack_message: entities >>> message: {:?}, recv_keys: {:?}",
-           message, recv_keys);
+    trace!(
+        "indy_anon_pack_message: entities >>> message: {:?}, recv_keys: {:?}",
+        message,
+        recv_keys
+    );
 
-
-    let result = CommandExecutor::instance()
-    .send(Command::Route(RouteCommand::AnonPackMessage(
+    let result = CommandExecutor::instance().send(Command::Route(RouteCommand::AnonPackMessage(
         message,
         recv_keys,
         Box::new(move |result| {
             let (err, ames) = result_to_err_code_1!(result, String::new());
-            trace!("indy_anon_pack_message: cb command_handle: {:?}, err: {:?}, ames: {:?}",
-                   command_handle, err, ames);
+            trace!(
+                "indy_anon_pack_message: cb command_handle: {:?}, err: {:?}, ames: {:?}",
+                command_handle,
+                err,
+                ames
+            );
             let verkey = ctypes::string_to_cstring(ames);
             cb(command_handle, err, verkey.as_ptr())
-        })
+        }),
     )));
 
     let res = result_to_err_code!(result);
@@ -93,37 +101,55 @@ pub fn indy_anon_pack_message(command_handle: i32,
 
 //update function to return key used
 #[no_mangle]
-pub fn indy_unpack_message(command_handle: i32,
-                  wallet_handle: i32,
-                  ames_json: *const c_char,
-                  my_vk: *const c_char,
-                  cb: Option<extern fn(xcommand_handle: i32,
-                                       err: ErrorCode,
-                                       plaintext: *const c_char,
-                                       sender_vk: *const c_char)>) -> ErrorCode {
-    trace!("indy_unpack_message: >>> wallet_handle: {:?}, ames: {:?}, my_vk: {:?}",
-           wallet_handle, ames_json, my_vk);
+pub fn indy_unpack_message(
+    command_handle: i32,
+    wallet_handle: i32,
+    ames_json: *const c_char,
+    my_vk: *const c_char,
+    cb: Option<
+        extern "C" fn(
+            xcommand_handle: i32,
+            err: ErrorCode,
+            plaintext: *const c_char,
+            sender_vk: *const c_char,
+        ),
+    >,
+) -> ErrorCode {
+    trace!(
+        "indy_unpack_message: >>> wallet_handle: {:?}, ames: {:?}, my_vk: {:?}",
+        wallet_handle,
+        ames_json,
+        my_vk
+    );
 
     check_useful_c_str!(ames_json, ErrorCode::CommonInvalidParam3);
     check_useful_c_str!(my_vk, ErrorCode::CommonInvalidParam4);
     check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam5);
 
-    trace!("indy_unpack_message: entities >>> wallet_handle: {:?}, ames: {:?}, my_vk: {:?}",
-           wallet_handle, ames_json, my_vk);
+    trace!(
+        "indy_unpack_message: entities >>> wallet_handle: {:?}, ames: {:?}, my_vk: {:?}",
+        wallet_handle,
+        ames_json,
+        my_vk
+    );
 
-    let result = CommandExecutor::instance()
-    .send(Command::Route(RouteCommand::UnpackMessage(
+    let result = CommandExecutor::instance().send(Command::Route(RouteCommand::UnpackMessage(
         ames_json,
         my_vk,
         wallet_handle,
         Box::new(move |result| {
-            let (err, plaintext, sender_vk) = result_to_err_code_2!(result, String::new(), String::new());
-            trace!("indy_unpack_message: cb command_handle: {:?}, err: {:?}, plaintext: {:?}",
-                   command_handle, err, plaintext);
+            let (err, plaintext, sender_vk) =
+                result_to_err_code_2!(result, String::new(), String::new());
+            trace!(
+                "indy_unpack_message: cb command_handle: {:?}, err: {:?}, plaintext: {:?}",
+                command_handle,
+                err,
+                plaintext
+            );
             let plaintext = ctypes::string_to_cstring(plaintext);
             let sender_vk = ctypes::string_to_cstring(sender_vk);
             cb(command_handle, err, plaintext.as_ptr(), sender_vk.as_ptr())
-        })
+        }),
     )));
 
     let res = result_to_err_code!(result);
@@ -131,5 +157,4 @@ pub fn indy_unpack_message(command_handle: i32,
     trace!("indy_unpack_message: <<< res: {:?}", res);
 
     res
-
 }
--- libindy/src/commands/route.rs
+++ libindy/src/commands/route.rs
@@ -1,43 +1,52 @@
 use errors::route::RouteError;
-use services::wallet::WalletService;
+use serde_json;
 use services::crypto::CryptoService;
 use services::route::RouteService;
-use std::result;
+use services::wallet::WalletService;
 use std::rc::Rc;
-use serde_json;
-
+use std::result;
 
 type Result<T> = result::Result<T, RouteError>;
-
 
 pub enum RouteCommand {
     AuthPackMessage(
         String, // plaintext message
         String, //list of receiving keys
         String, //my verkey
-        i32, //wallet_handle
-        Box<Fn(Result<String/*JWM serialized as string*/>) + Send>),
+        i32,    //wallet_handle
+        Box<Fn(Result<String /*JWM serialized as string*/>) + Send>,
+    ),
     AnonPackMessage(
         String, // plaintext message
         String, // list of receiving keys
-        Box<Fn(Result<String/*JWM serialized as string*/>) + Send>),
+        Box<Fn(Result<String /*JWM serialized as string*/>) + Send>,
+    ),
     UnpackMessage(
         String, // AMES either JSON or Compact Serialization
         String, // my verkey
-        i32, // wallet handle
-        Box<Fn(Result<(String /*plaintext*/, String /*if authcrypt return sender_vk else null string*/)>) + Send>),
+        i32,    // wallet handle
+        Box<
+            Fn(
+                    Result<(
+                        String, /*plaintext*/
+                        String, /*if authcrypt return sender_vk else null string*/
+                    )>,
+                ) + Send,
+        >,
+    ),
 }
 
 pub struct RouteCommandExecutor {
     wallet_service: Rc<WalletService>,
     crypto_service: Rc<CryptoService>,
-    route_service: Rc<RouteService>
+    route_service: Rc<RouteService>,
 }
 
 impl RouteCommandExecutor {
-    pub fn new(wallet_service: Rc<WalletService>,
-               crypto_service: Rc<CryptoService>,
-               route_service: Rc<RouteService>,
+    pub fn new(
+        wallet_service: Rc<WalletService>,
+        crypto_service: Rc<CryptoService>,
+        route_service: Rc<RouteService>,
     ) -> RouteCommandExecutor {
         RouteCommandExecutor {
             wallet_service,
@@ -63,36 +72,50 @@ impl RouteCommandExecutor {
         };
     }
 
-    pub fn auth_pack_msg(&self, message: &str, recv_keys_json: &str, my_vk: String, wallet_handle: i32) -> Result<String> {
-
+    pub fn auth_pack_msg(
+        &self,
+        message: &str,
+        recv_keys_json: &str,
+        my_vk: String,
+        wallet_handle: i32,
+    ) -> Result<String> {
         //convert type from json array to Vec<String>
-        let recv_keys : Vec<&str> = serde_json::from_str(recv_keys_json)
-            .map_err(|err| RouteError::SerializationError(format!("Failed to serialize recv_keys {:?}", err)))?;
+        let recv_keys: Vec<&str> = serde_json::from_str(recv_keys_json).map_err(|err| {
+            RouteError::SerializationError(format!("Failed to serialize recv_keys {:?}", err))
+        })?;
 
-        self.route_service.auth_pack_msg(message,
-                                    recv_keys,
-                                    &my_vk,
-                                    wallet_handle,
-                                    self.wallet_service.clone(),
-                                    self.crypto_service.clone())
+        self.route_service.auth_pack_msg(
+            message,
+            recv_keys,
+            &my_vk,
+            wallet_handle,
+            self.wallet_service.clone(),
+            self.crypto_service.clone(),
+        )
     }
 
     pub fn anon_pack_msg(&self, message: &str, recv_keys_json: &str) -> Result<String> {
-
         //convert type from json array to Vec<&str>
-        let recv_keys : Vec<&str> = serde_json::from_str(recv_keys_json)
-            .map_err(|err| RouteError::SerializationError(format!("Failed to serialize recv_keys {:?}", err)))?;
+        let recv_keys: Vec<&str> = serde_json::from_str(recv_keys_json).map_err(|err| {
+            RouteError::SerializationError(format!("Failed to serialize recv_keys {:?}", err))
+        })?;
 
-        self.route_service.anon_pack_msg(message,
-                                         recv_keys,
-                                         self.crypto_service.clone())
+        self.route_service
+            .anon_pack_msg(message, recv_keys, self.crypto_service.clone())
     }
 
-    pub fn unpack_msg(&self, ames_json_str: &str, my_vk: &str, wallet_handle: i32) -> Result<(String, String)> {
-        self.route_service.unpack_msg(ames_json_str,
-                                      my_vk,
-                                      wallet_handle,
-                                      self.wallet_service.clone(),
-                                      self.crypto_service.clone())
+    pub fn unpack_msg(
+        &self,
+        ames_json_str: &str,
+        my_vk: &str,
+        wallet_handle: i32,
+    ) -> Result<(String, String)> {
+        self.route_service.unpack_msg(
+            ames_json_str,
+            my_vk,
+            wallet_handle,
+            self.wallet_service.clone(),
+            self.crypto_service.clone(),
+        )
     }
 }
