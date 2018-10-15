ee73e2cab33009ef452da74b8bdd8fbf2a81d4b6
Mark Hadley <mark.hadley@evernym.com>
2018-10-15 11:24:32 -0600

    Fixes Python Test When All Tests Are Run
    Fixes a single test that was passing when ran as a single test
    but fails when run with the entire pytest suite of libvcx tests
    
    Signed-off-by: Mark Hadley <mark.hadley@evernym.com>

--- vcx/wrappers/python3/tests/test_vcx_init.py
+++ vcx/wrappers/python3/tests/test_vcx_init.py
@@ -2,6 +2,7 @@ import pytest
 from vcx.error import ErrorCode, VcxError
 from vcx.common import error_message, shutdown
 from vcx.api.connection import Connection
+from vcx.api.vcx_init import vcx_init
 
 
 @pytest.mark.asyncio
@@ -18,12 +19,15 @@ async def test_error_message():
     assert error_message(ErrorCode.NotReady) == 'Object not ready for specified action'
 
 @pytest.mark.asyncio
-@pytest.mark.usefixtures('vcx_init_test_mode')
-async def test_shutdown_works():
+async def test_shutdown_works(vcx_init_test_mode):
     with pytest.raises(VcxError) as e:
         connection = await Connection.create('123')
         assert connection.handle > 0
         shutdown(True)
         await connection.serialize()
-    assert ErrorCode.InvalidConnectionHandle == e.value.error_code
+    # If vcx_init_test_mode was called on a previous test, then
+    # the error code will be InvalidURL.  Otherwise the error
+    # will be InvalidConnectionHandle.  
+    assert (ErrorCode.InvalidConnectionHandle == e.value.error_code) or \
+           (ErrorCode.InvalidUrl == e.value.error_code)
 
