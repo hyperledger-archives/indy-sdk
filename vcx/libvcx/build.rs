f517a3b1f0a90a09fb49d0e4e711c5c416e26303
artem.ivanov <artem.ivanov@dsr-company.com>
2018-11-27 15:19:43 +0300

    Updated Libvcx build.rs to copy dependencies
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- vcx/libvcx/build.rs
+++ vcx/libvcx/build.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
@@ -101,11 +102,23 @@ fn main() {
         let profile = env::var("PROFILE").unwrap();
         println!("profile={}", profile);
 
+        let output_dir = env::var("OUT_DIR").unwrap();
+        println!("output_dir={}", output_dir);
+        let output_dir = Path::new(output_dir.as_str());
+
         let indy_dir = env::var("INDY_DIR").unwrap_or(format!("..\\..\\libindy\\target\\{}", profile));
         println!("indy_dir={}", indy_dir);
         let indy_dir = Path::new(indy_dir.as_str());
 
+        let dst = output_dir.join("..\\..\\..\\..");
         println!("cargo:rustc-flags=-L {}", indy_dir.as_os_str().to_str().unwrap());
+
+        let files = vec!["indy.dll", "libeay32md.dll", "libsodium.dll", "libzmq.dll", "ssleay32md.dll"];
+        for f in files.iter() {
+            if let Ok(_) = fs::copy(&indy_dir.join(f), &dst.join(f)) {
+                println!("copy {} -> {}", &indy_dir.join(f).display(), &dst.join(f).display());
+            }
+        }
     }
 
     match env::var("CARGO_FEATURE_CI") {
