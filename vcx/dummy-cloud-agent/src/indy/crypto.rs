0ddc4eec172190fb5ddc4c30c43e978ea306b54d
artem.ivanov <artem.ivanov@dsr-company.com>
2019-01-16 17:55:07 +0300

    Updated Dummy Agent to handle new IndyError type
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- vcx/dummy-cloud-agent/src/indy/crypto.rs
+++ vcx/dummy-cloud-agent/src/indy/crypto.rs
@@ -1,31 +1,31 @@
-use indyrs::{crypto, ErrorCode};
+use indyrs::{crypto, IndyError};
 use futures::*;
 use utils::futures::*;
 
 pub fn auth_crypt(wallet_handle: i32,
                   sender_vk: &str,
                   recipient_vk: &str,
-                  message: &[u8]) -> Box<Future<Item=Vec<u8>, Error=ErrorCode>> {
+                  message: &[u8]) -> Box<Future<Item=Vec<u8>, Error=IndyError>> {
     crypto::auth_crypt(wallet_handle, sender_vk, recipient_vk, message)
         .into_box()
 }
 
 pub fn auth_decrypt(wallet_handle: i32,
                     recipient_vk: &str,
-                    encrypted_message: &[u8]) -> Box<Future<Item=(String, Vec<u8>), Error=ErrorCode>> {
+                    encrypted_message: &[u8]) -> Box<Future<Item=(String, Vec<u8>), Error=IndyError>> {
     crypto::auth_decrypt(wallet_handle, recipient_vk, encrypted_message)
         .into_box()
 }
 
 pub fn anon_crypt(recipient_vk: &str,
-                  message: &[u8]) -> Box<Future<Item=Vec<u8>, Error=ErrorCode>> {
+                  message: &[u8]) -> Box<Future<Item=Vec<u8>, Error=IndyError>> {
     crypto::anon_crypt(recipient_vk, message)
         .into_box()
 }
 
 pub fn anon_decrypt(wallet_handle: i32,
                     recipient_vk: &str,
-                    encrypted_message: &[u8]) -> Box<Future<Item=Vec<u8>, Error=ErrorCode>> {
+                    encrypted_message: &[u8]) -> Box<Future<Item=Vec<u8>, Error=IndyError>> {
     crypto::anon_decrypt(wallet_handle, recipient_vk, encrypted_message)
         .into_box()
 }
@@ -33,14 +33,14 @@ pub fn anon_decrypt(wallet_handle: i32,
 #[cfg(test)]
 pub fn sign(wallet_handle: i32,
             signer_vk: &str,
-            message: &[u8]) -> Box<Future<Item=Vec<u8>, Error=ErrorCode>> {
+            message: &[u8]) -> Box<Future<Item=Vec<u8>, Error=IndyError>> {
     crypto::sign(wallet_handle, signer_vk, message)
         .into_box()
 }
 
 pub fn verify(signer_vk: &str,
               message: &[u8],
-              signature: &[u8]) -> Box<Future<Item=bool, Error=ErrorCode>> {
+              signature: &[u8]) -> Box<Future<Item=bool, Error=IndyError>> {
     crypto::verify(signer_vk, message, signature)
         .into_box()
 }
