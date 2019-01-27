ad5cf50417d03ab1ca4c15b30a24c0975b549ea1
Kyle Den Hartog <kdenhar@gmail.com>
2019-01-27 03:39:12 -0700

    added additional pack and unpack tests
    Signed-off-by: Kyle Den Hartog <kdenhar@gmail.com>

--- wrappers/python/tests/crypto/test_pack_message.py
+++ wrappers/python/tests/crypto/test_pack_message.py
@@ -23,11 +23,9 @@ async def test_pack_message_authcrypt_works(wallet_handle, seed_my1, verkey_my2,
 async def test_pack_message_anoncrypt_works(wallet_handle, verkey_my2):
     receiver_verkeys = [verkey_my2]
     packed_message_bytes = await crypto.pack_message(wallet_handle, "pack_message", receiver_verkeys, None)
-    print(type(packed_message_bytes))
-    with open("/tmp/out", 'w') as f:
-        f.write(str(type(packed_message_bytes)))
     packed_message_json = packed_message_bytes.decode("utf-8")
     json_message = json.loads(packed_message_json)
+    print(json_message)
     assert json_message['protected'] != ""
     assert json_message['tag'] != ""
     assert json_message['ciphertext'] != ""
@@ -35,9 +33,24 @@ async def test_pack_message_anoncrypt_works(wallet_handle, verkey_my2):
 
 @pytest.mark.asyncio
 async def test_pack_message_invalid_verkey(wallet_handle, verkey_my2):
-    sender_verkey = "INVALID_VERKEY"
+    sender_verkey = "INVALID"
     receiver_verkeys = [verkey_my2]
     with pytest.raises(IndyError) as e:
-        pack_message = await crypto.pack_message(wallet_handle, "pack_message", receiver_verkeys, sender_verkey)
+        pack_message = await crypto.pack_message(wallet_handle, "pack_message", receiver_verkeys, "Invalid_VERKEY")
     assert ErrorCode.WalletItemNotFound == e.value.error_code
-    
+
+@pytest.mark.asyncio
+async def test_pack_message_invalid_receiver_verkey(wallet_handle, verkey_my2, seed_my1):
+    sender_verkey = await did.create_key(wallet_handle, json.dumps({'seed': seed_my1}))
+    receiver_verkeys = verkey_my2
+    with pytest.raises(IndyError) as e:
+        pack_message = await crypto.pack_message(wallet_handle, "pack_message", receiver_verkeys, "Invalid_VERKEY")
+    assert ErrorCode.CommonInvalidStructure == e.value.error_code
+
+@pytest.mark.asyncio
+async def test_pack_message_invalid_wallet_handle(wallet_handle, verkey_my2, seed_my1):
+    sender_verkey = await did.create_key(wallet_handle, json.dumps({'seed': seed_my1}))
+    receiver_verkeys = [verkey_my2]
+    with pytest.raises(IndyError) as e:
+        pack_message = await crypto.pack_message(wallet_handle + 1, "pack_message", receiver_verkeys, "Invalid_VERKEY")
+    assert ErrorCode.WalletInvalidHandle == e.value.error_code
--- wrappers/python/tests/crypto/test_unpack_message.py
+++ wrappers/python/tests/crypto/test_unpack_message.py
@@ -27,7 +27,6 @@ async def test_pack_message_and_unpack_message_authcrypt_works(wallet_handle, se
 @pytest.mark.asyncio
 async def test_pack_message_and_unpack_message_anoncrypt_works(wallet_handle, seed_steward1, seed_my1, pack_message):
     #setup keys
-    sender_vk = await did.create_key(wallet_handle, json.dumps({'seed': seed_my1}))
     steward_vk = await did.create_key(wallet_handle, json.dumps({'seed': seed_steward1}))
     recipient_verkeys = [steward_vk]
     
@@ -39,3 +38,17 @@ async def test_pack_message_and_unpack_message_anoncrypt_works(wallet_handle, se
     unpacked_message_json = json.loads(unpacked_message)
     assert unpacked_message_json['message'] == pack_message
     assert unpacked_message_json['recipient_verkey'] == steward_vk
+
+
+@pytest.mark.asyncio
+async def test_pack_message_and_unpack_message_missing_verkey(wallet_handle, seed_my1, verkey_my2, pack_message):
+    # setup keys
+    sender_vk = await did.create_key(wallet_handle, json.dumps({'seed': seed_my1}))
+    recipient_verkeys = [verkey_my2]
+
+    # run pack and unpack
+    packed_message = await crypto.pack_message(wallet_handle, pack_message, recipient_verkeys, sender_vk)
+
+    with pytest.raises(IndyError) as e:
+        await crypto.unpack_message(wallet_handle, packed_message)
+    assert ErrorCode.WalletItemNotFound == e.value.error_code
