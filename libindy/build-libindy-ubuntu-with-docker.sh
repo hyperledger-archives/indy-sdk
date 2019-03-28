402ae053d82d63cfba8f109619f16551ef493d96
Xavier NOPRE <xnopre@twinpeek.net>
2019-03-28 09:43:23 +0100

    Scripts to build libindy for Ubuntu using Docker : complete .dockerignore to limit size of Docker context sent to daemon
    Signed-off-by: Xavier NOPRE <xnopre@twinpeek.net>

--- libindy/build-libindy-ubuntu-with-docker.sh
+++ libindy/build-libindy-ubuntu-with-docker.sh
@@ -3,15 +3,18 @@
 cd ..
 
 # Generate temporary .dockerignore to limit context size sent to Docker daemon
-echo .git/ >> .dockerignore
+echo .git/ > .dockerignore
+echo .idea/ >> .dockerignore
 echo Specs >> .dockerignore
 echo ci >> .dockerignore
 echo cli >> .dockerignore
 echo doc >> .dockerignore
+echo docs >> .dockerignore
 echo experimental >> .dockerignore
 echo libnullpay >> .dockerignore
 echo samples >> .dockerignore
 echo vcx >> .dockerignore
+echo libindy/target >> .dockerignore
 
 # Build Docker image
 docker build -t build-libindy-ubuntu -f libindy/build-libindy-ubuntu.dockerfile .
