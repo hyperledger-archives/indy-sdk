234f714dce4eaa75c90e4d363c381adbbb26c7ad
John Callahan <jcallahan@acm.org>
2019-03-28 12:32:45 -0400

    updated ledger, wallet, etc. in Issue Credential howto
    Signed-off-by: John Callahan <jcallahan@acm.org>

--- docs/how-tos/issue-credential/rust/src/step3.rs
+++ docs/how-tos/issue-credential/rust/src/step3.rs
@@ -2,9 +2,9 @@ println!("12. Creating Prover wallet and opening it to get the handle");
 let prover_did = "VsKV7grR1BUE29mG2Fm2kX";
 let prover_wallet_name = "prover_wallet";
 let prover_wallet_config = json!({ "id" : prover_wallet_name.to_string() }).to_string();
-Wallet::create(&prover_wallet_config, USEFUL_CREDENTIALS).unwrap();
-let prover_wallet_handle: i32 = Wallet::open(&prover_wallet_config, USEFUL_CREDENTIALS).unwrap();
+wallet::create_wallet(&prover_wallet_config, USEFUL_CREDENTIALS).wait().unwrap();
+let prover_wallet_handle: i32 = wallet::open_wallet(&prover_wallet_config, USEFUL_CREDENTIALS).wait().unwrap();
 
 println!("13. Prover is creating Master Secret");
 let master_secret_name = "master_secret";
-Prover::create_master_secret(prover_wallet_handle, Some(master_secret_name)).unwrap();
+anoncreds::prover_create_master_secret(prover_wallet_handle, Some(master_secret_name)).wait().unwrap();
