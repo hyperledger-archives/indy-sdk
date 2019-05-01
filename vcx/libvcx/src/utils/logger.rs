8b28bb529fd4faa5502bcd24b7ac2e4743d4d55e
Norman Jarvis <norman.jarvis@evernym.com>
2019-05-01 04:55:42 -0600

    Changes to prevent Android from crashing.

--- vcx/libvcx/src/utils/logger.rs
+++ vcx/libvcx/src/utils/logger.rs
@@ -62,13 +62,24 @@ impl LibvcxLogger {
     }
 
     pub fn init(context: *const CVoid, enabled: Option<EnabledCB>, log: LogCB, flush: Option<FlushCB>) -> VcxResult<()> {
-        trace!("LibvcxLogger::init >>>");
+        //trace!("LibvcxLogger::init >>>");
         let logger = LibvcxLogger::new(context, enabled, log, flush);
-        log::set_boxed_logger(Box::new(logger))
-            .map_err(|err| VcxError::from_msg(VcxErrorKind::LoggingError, format!("Setting logger failed with: {}", err)))?;
-        log::set_max_level(LevelFilter::Trace);
-        libindy::logger::set_logger(log::logger())
-            .map_err(|err| err.map(VcxErrorKind::LoggingError, "Setting logger failed"))?;
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        // These lines cause Android to crash when invoked in a nested Java Callback
+        // This is not a long term fix BUT we need to prevent crashing of the mobile app
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        //log::set_boxed_logger(Box::new(logger))
+        //    .map_err(|err| VcxError::from_msg(VcxErrorKind::LoggingError, format!("Setting logger failed with: {}", err)))?;
+        //log::set_max_level(LevelFilter::Trace);
+        //libindy::logger::set_logger(log::logger())
+        //    .map_err(|err| err.map(VcxErrorKind::LoggingError, "Setting logger failed"))?;
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        // Temporary fix for the Android crashing
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        libindy::logger::set_default_logger(Some("DEBUG"));
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+
 
         unsafe {
             LOGGER_STATE = LoggerState::Custom;
