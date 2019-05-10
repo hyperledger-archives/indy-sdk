6281ebc7464713b22150e9830860cbca9c9905f2
Darko Kulic <darko.kulic@evernym.com>
2019-05-10 18:08:10 +0200

    CM-2206 Fixed node wrapper
    Signed-off-by: Darko Kulic <darko.kulic@evernym.com>

--- wrappers/nodejs/test/cache.js
+++ wrappers/nodejs/test/cache.js
@@ -2,7 +2,6 @@ var test = require('ava')
 var indy = require('../')
 var cuid = require('cuid')
 var initTestPool = require('./helpers/initTestPool')
-var tempy = require('tempy')
 
 test('cache', async function (t) {
   var pool = await initTestPool()
@@ -10,8 +9,7 @@ test('cache', async function (t) {
   var walletCredentials = { 'key': 'key' }
   await indy.createWallet(walletConfig, walletCredentials)
   var wh = await indy.openWallet(walletConfig, walletCredentials)
-  var [trusteeDid] = await indy.createAndStoreMyDid(wh, { seed: '000000000000000000000000Trustee1' })
-  var [myDid, myVerkey] = await indy.createAndStoreMyDid(wh, { })
+  var [myDid] = await indy.createAndStoreMyDid(wh, { })
   var schemaName = 'schema-' + cuid()
   var [schemaId, schema] = await indy.issuerCreateSchema(myDid, schemaName, '1.0', ['name', 'age'])
 
@@ -27,9 +25,9 @@ test('cache', async function (t) {
   }
 
   // Schema
-  req = await indy.buildSchemaRequest(myDid, schema)
+  var req = await indy.buildSchemaRequest(myDid, schema)
   req = await indy.signRequest(wh, myDid, req)
-  res = await indy.submitRequest(pool.handle, req)
+  await indy.submitRequest(pool.handle, req)
 
   var schemaRes = await indy.getSchema(pool.handle, wh, myDid, schemaId, defaultGetCacheOptions)
   t.is(schemaRes.name, schema.name)
@@ -39,7 +37,7 @@ test('cache', async function (t) {
   // Cred Def
   var [credDefId, credDef] = await indy.issuerCreateAndStoreCredentialDef(wh, myDid, schema, 'TAG', 'CL', { support_revocation: false })
   req = await indy.buildCredDefRequest(myDid, credDef)
-  res = await indy.signAndSubmitRequest(pool.handle, wh, myDid, req)
+  await indy.signAndSubmitRequest(pool.handle, wh, myDid, req)
 
   var credDefRes = await indy.getCredDef(pool.handle, wh, myDid, credDefId, defaultGetCacheOptions)
   t.is(credDefRes.id, credDef.id)
