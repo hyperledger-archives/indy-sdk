c06b780cb95d1b7c01a6fe315197612ae207f43c
Axel Nennker <axel.nennker@telekom.de>
2019-05-11 20:40:14 +0200

    use AtomicUsize::new(0)
    Signed-off-by: Axel Nennker <axel.nennker@telekom.de>

--- vcx/libvcx/src/utils/mod.rs
+++ vcx/libvcx/src/utils/mod.rs
@@ -33,12 +33,12 @@ pub mod threadpool;
 pub mod uuid;
 pub mod author_agreement;
 
-use std::sync::atomic::{AtomicUsize, ATOMIC_USIZE_INIT, Ordering};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::path::PathBuf;
 use std::env;
 
 lazy_static! {
-    static ref COMMAND_HANDLE_COUNTER: AtomicUsize = ATOMIC_USIZE_INIT;
+    static ref COMMAND_HANDLE_COUNTER: AtomicUsize = AtomicUsize::new(0);
 }
 // allows all threads to atomically get a unique command handle
 pub fn generate_command_handle() -> i32 {
