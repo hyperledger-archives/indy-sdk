331dae46e71e0c842347272edbf18c30657b8490
Ian Costanzo <ian@anon-solutions.ca>
2019-07-18 09:02:38 -0700

    Remove perf testing scripts
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- experimental/plugins/postgres_storage/postgres-perf-multi.sh
+++
@@ -1,2 +0,0 @@
-cd ../../../samples/python && \
-PYTHONPATH=.:../../wrappers/python/ python3 src/getting_started_perf.py -t postgres_storage -l libindystrgpostgres.dylib -e postgresstorage_init -c '{"url":"localhost:5432","wallet_scheme":"MultiWalletSingleTable"}' -s '{"account":"postgres","password":"mysecretpassword","admin_account":"postgres","admin_password":"mysecretpassword"}'
