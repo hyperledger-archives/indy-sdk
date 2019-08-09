27e9a211bc3e80943621462c5069191da8b5ceb6
Axel Nennker <axel.nennker@telekom.de>
2019-08-09 23:04:50 +0200

    cleamup this messed up commit
    Signed-off-by: Axel Nennker <axel.nennker@telekom.de>

--- wrappers/java/src/test/java/org/hyperledger/indy/sdk/crypto/PackUnpackMessageTest.java
+++ wrappers/java/src/test/java/org/hyperledger/indy/sdk/crypto/PackUnpackMessageTest.java
@@ -9,7 +9,6 @@ import org.json.JSONObject;
 import org.junit.Test;
 import org.json.JSONArray;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import static org.hamcrest.CoreMatchers.isA;
@@ -78,20 +77,14 @@ public class PackUnpackMessageTest extends IndyIntegrationTestWithSingleWallet {
 	}
 
 	@Test
-	public void testUnpackMessageErrorsWithInvalidPackedMessageStructure() {
+	public void testUnpackMessageErrorsWithInvalidPackedMessageStructure() throws Exception {
 		String packedMessage = "jibberish";
 
 		thrown.expect(ExecutionException.class);
 		thrown.expectCause(isA(InvalidStructureException.class));
 
-		try {
-			CompletableFuture<byte[]> future = Crypto.unpackMessage(wallet, packedMessage.getBytes());
-			assertTrue(future.isCompletedExceptionally());
-		} catch (Exception e) {
-			// expected
-			System.err.println("testUnpackMessageErrorsWithInvalidPackedMessageStructure threw " + e);
-			return;
-		}
+		Crypto.unpackMessage(wallet, packedMessage.getBytes()).get();
+
 		// this assert should never trigger since unpackMessage should throw exception
 		assertTrue(false);
 	}
