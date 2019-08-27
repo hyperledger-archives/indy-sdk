81f72d34c09a4919da8b2d5829593ef347659af8
artem.ivanov <artem.ivanov@dsr-company.com>
2019-08-27 17:55:07 +0300

    IS-1318 -- Initial version of CD pipleine
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

---
+++ ci/rpm-build-and-upload.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -xe
+
+if [ "$1" = "--help" ] ; then
+  echo "Usage: <version> <key> <type> <number> <package>"
+  return
+fi
+
+version="$1"
+key="$2"
+type="$3"
+number="$4"
+package="$5"
+dir=$(pwd)
+
+[ -z $version ] && exit 1
+[ -z $key ] && exit 2
+[ -z $type ] && exit 3
+[ -z $number ] && exit 4
+[ -z $package ] && exit 5
+
+mkdir -p /usr/src/rpm/SOURCES/
+
+sed \
+	-e "s|@version@|$version|g" \
+	-e "s|@dir@|$dir|g" \
+    ci/${package}.spec.in > ${package}.spec
+
+mkdir rpms
+
+spectool -R ${package}.spec || exit 6
+rpmbuild -ba ${package}.spec || exit 7
+
+cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@$SOVRIN_REPO_HOST
+mkdir /var/repository/repos/centos/$package/$type/$version-$number
+cd /var/repository/repos/centos/$package/$type/$version-$number
+put -r /rpms/x86_64/
+ls -l /var/repository/repos/centos/$package/$type/$version-$number
+EOF
