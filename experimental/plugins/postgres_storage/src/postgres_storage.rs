51c741e371d1e9b3fa4ff34875f6db430046e65b
Aaron Reed <aaronr@us.ibm.com>
2020-01-08 17:09:37 -0500

    fix compiler warning

--- experimental/plugins/postgres_storage/src/postgres_storage.rs
+++ experimental/plugins/postgres_storage/src/postgres_storage.rs
@@ -483,7 +483,8 @@ struct MultiWalletMultiTableStrategy {}
 impl WalletStrategy for DatabasePerWalletStrategy {
     // initialize storage based on wallet storage strategy
     fn init_storage(&self, _config: &PostgresConfig, _credentials: &PostgresCredentials) -> Result<(), WalletStorageError> {
-        env_logger::try_init();
+        env_logger::try_init()
+            .expect("Can't init env logger");
         // no-op
         debug!("Initializing storage strategy DatabasePerWalletStrategy.");
         Ok(())
@@ -649,7 +650,8 @@ fn get_multi_database_name(config: &PostgresConfig) -> &str {
 impl WalletStrategy for MultiWalletSingleTableStrategy {
     // initialize storage based on wallet storage strategy
     fn init_storage(&self, config: &PostgresConfig, credentials: &PostgresCredentials) -> Result<(), WalletStorageError> {
-        env_logger::try_init();
+        env_logger::try_init()
+            .expect("Can't init env logger");
         debug!("Entering init_storage");
         // create database and tables for storage
         // if admin user and password aren't provided then bail
