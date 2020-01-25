717c65b641f7dbb856e1df378acf99a8ddf1c702
Sergey Minaev <sergey.minaev@dsr-corporation.com>
2020-01-25 17:21:22 +0300

    Temporary fixes for unit tests.
    Signed-off-by: Sergey Minaev <sergey.minaev@dsr-corporation.com>

--- libindy/tests/ledger.rs
+++ libindy/tests/ledger.rs
@@ -12,6 +12,7 @@ use crate::utils::domain::anoncreds::revocation_registry::RevocationRegistryV1;
 use crate::utils::domain::anoncreds::revocation_registry_definition::RevocationRegistryDefinitionV1;
 use crate::utils::domain::anoncreds::revocation_registry_delta::RevocationRegistryDeltaV1;
 use crate::utils::domain::anoncreds::schema::SchemaV1;
+#[cfg(not(feature="only_high_cases"))]
 use crate::utils::domain::crypto::did::DidValue;
 use crate::utils::domain::ledger::constants;
 use crate::utils::domain::ledger::nym::NymData;
