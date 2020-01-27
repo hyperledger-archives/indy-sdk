4b50e503844f892ecb21f8acedb69baead174f01
artem.ivanov <artem.ivanov@dsr-company.com>
2020-01-27 15:29:48 +0300

    Aries-396: base64url without padding
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- libindy/indy-utils/src/crypto/base64/rust_base64.rs
+++ libindy/indy-utils/src/crypto/base64/rust_base64.rs
@@ -13,11 +13,11 @@ pub fn decode(doc: &str) -> Result<Vec<u8>, IndyError> {
 }
 
 pub fn encode_urlsafe(doc: &[u8]) -> String {
-    base64::encode_config(doc, base64::URL_SAFE)
+    base64::encode_config(doc, base64::URL_SAFE_NO_PAD)
 }
 
 pub fn decode_urlsafe(doc: &str) -> Result<Vec<u8>, IndyError> {
-    base64::decode_config(doc, base64::URL_SAFE)
+    base64::decode_config(doc, base64::URL_SAFE_NO_PAD)
         .context("Invalid base64URL_SAFE sequence")
         .context(IndyErrorKind::InvalidStructure)
         .map_err(|err| err.into())
@@ -53,5 +53,20 @@ mod tests {
 
         assert!(result.is_ok(), "Got error");
         assert_eq!(&[1, 2, 3], &result.unwrap()[..]);
+    }
+
+    #[test] // aries-396
+    fn encode_base64_urlsafe_and_urlsafe_no_pad_compatible() {
+        let data = "Hello World";
+        {
+            let encoded = base64::encode_config(data, base64::URL_SAFE);
+            let decoded_data = base64::decode_config(&encoded, base64::URL_SAFE_NO_PAD).unwrap();
+            assert_eq!(data.as_bytes().to_vec(), decoded_data);
+        }
+        {
+            let encoded = base64::encode_config(data, base64::URL_SAFE_NO_PAD);
+            let decoded_data = base64::decode_config(&encoded, base64::URL_SAFE).unwrap();
+            assert_eq!(data.as_bytes().to_vec(), decoded_data);
+        }
     }
 }
