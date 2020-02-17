3a6f0030720ae94a3dce9b16aa198931c1e3f236
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2020-02-17 18:41:22 +0300

    Fix nodejs wrapper
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- vcx/wrappers/node/test/suite1/wallet.test.ts
+++ vcx/wrappers/node/test/suite1/wallet.test.ts
@@ -98,7 +98,7 @@ describe('Wallet:', () => {
 
   describe('signWithAddress:', () => {
     it('success', async () => {
-      const msg = Buffer.from('random string');
+      const msg = Buffer.from('random string')
       const sig = await Wallet.signWithAddress('pay:sov:1234', msg)
       assert.ok(sig)
     })
@@ -106,8 +106,8 @@ describe('Wallet:', () => {
 
   describe('verifyWithAddress:', () => {
     it('success', async () => {
-      const msg = Buffer.from('random string');
-      const sig = Buffer.from('random string');
+      const msg = Buffer.from('random string')
+      const sig = Buffer.from('random string')
       const valid = await Wallet.verifyWithAddress('pay:sov:1234', msg, sig)
       assert.ok(valid)
     })
