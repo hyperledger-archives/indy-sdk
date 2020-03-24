c6db2021249e4d118285edfb28620c728d5ffb12
artem.ivanov <artem.ivanov@dsr-company.com>
2020-03-24 12:55:46 +0300

    Bugfix: Vcx store pending messages into wallet
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- vcx/libvcx/src/connection.rs
+++ vcx/libvcx/src/connection.rs
@@ -1295,19 +1295,19 @@ pub fn send_message_to_self_endpoint(message: A2AMessage, did_doc: &DidDoc) -> V
 }
 
 pub fn add_pending_messages(handle: u32, messages: HashMap<MessageId, String>) -> VcxResult<()> {
-    CONNECTION_MAP.get_mut(handle, |connection| {
+    CONNECTION_MAP.get(handle, |connection| {
         match connection {
             Connections::V1(_) => Err(VcxError::from(VcxErrorKind::InvalidConnectionHandle)),
-            Connections::V3(ref mut connection) => connection.add_pending_messages(messages.clone())
+            Connections::V3(ref connection) => connection.add_pending_messages(messages.clone())
         }
     })
 }
 
 pub fn remove_pending_message(handle: u32, id: &MessageId) -> VcxResult<()> {
-    CONNECTION_MAP.get_mut(handle, |connection| {
+    CONNECTION_MAP.get(handle, |connection| {
         match connection {
             Connections::V1(_) => Err(VcxError::from(VcxErrorKind::InvalidConnectionHandle)),
-            Connections::V3(ref mut connection) => connection.remove_pending_message(id.clone())
+            Connections::V3(ref connection) => connection.remove_pending_message(id.clone())
         }
     })
 }
--- vcx/libvcx/src/utils/libindy/wallet.rs
+++ vcx/libvcx/src/utils/libindy/wallet.rs
@@ -12,7 +12,7 @@ pub struct WalletRecord {
     #[serde(rename = "type")]
     record_type: Option<String>,
     pub value: Option<String>,
-    tags: Option<String>
+    tags: Option<String>,
 }
 
 impl WalletRecord {
@@ -34,7 +34,7 @@ pub struct RestoreWalletConfigs {
     pub exported_wallet_path: String,
     pub backup_key: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub key_derivation: Option<String>
+    pub key_derivation: Option<String>,
 }
 
 impl RestoreWalletConfigs {
