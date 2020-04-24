15cbbe2f215daf5b908a990854ff6f37b6c84a54
Thomas Kim <jeong.kim@sk.com>
2020-04-24 16:44:50 +0900

    Set up the environment and init LibVcx if it is not initialized.
    Signed-off-by: Thomas Kim <jeong.kim@sk.com>

--- vcx/wrappers/java/demo-android/AliceDemo/app/src/main/java/com/sktelecom/ston/demo/MainActivity.java
+++ vcx/wrappers/java/demo-android/AliceDemo/app/src/main/java/com/sktelecom/ston/demo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.sktelecom.ston.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.system.ErrnoException;
+import android.system.Os;
+
+import com.evernym.sdk.vcx.LibVcx;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Os.setenv("EXTERNAL_STORAGE", getExternalFilesDir(null).getAbsolutePath(), true);
+        } catch (ErrnoException e) {
+            e.printStackTrace();
+        }
+
+        if (!LibVcx.isInitialized()) {
+            LibVcx.init();
+        }
     }
 }
