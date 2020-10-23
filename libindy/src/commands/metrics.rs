0deea5fd5e03c348f7fa049373819d1e23a0a7e0
toktar <renata.toktar@dsr-corporation.com>
2020-10-23 18:26:18 +0300

    VE-2045: refactoring MetricsCommand
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- libindy/src/commands/metrics.rs
+++ libindy/src/commands/metrics.rs
@@ -18,19 +18,17 @@ impl MetricsCommandExecutor {
                 debug!(target: "metrics_command_executor", "CollectMetrics command received");
                 cb(self._collect());
             }
-
         };
     }
     fn _collect(&self) -> IndyResult<String> {
-
         trace!("_collect >>>");
 
-        let metrics_map : HashMap<String, f32> = HashMap::new();
-        let res = serde_json::to_string(&metrics_map).unwrap();
+        let metrics_map: HashMap<String, f32> = HashMap::new();
+        let res = serde_json::to_string(&metrics_map)
+            .to_indy(IndyErrorKind::InvalidState, "Can't serialize a metrics map")?;
 
         trace!("_collect <<< res: {:?}", res);
         debug!("collecting metrics from command thread");
         Ok(res)
-
     }
 }
