2917d0ea5ef437de0d4db59100375a0c1ab3613f
toktar <renata.toktar@dsr-corporation.com>
2020-10-23 14:51:26 +0300

    VE-2045: add metrics from the wallet service and THREADPOOL
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- libindy/src/commands/metrics.rs
+++ libindy/src/commands/metrics.rs
@@ -1,15 +1,30 @@
 use std::collections::HashMap;
 use indy_api_types::errors::prelude::*;
+use indy_wallet::WalletService;
+use std::rc::Rc;
+
+const THREADPOOL_ACTIVE_COUNT : &str = "threadpool_active_count";
+const THREADPOOL_QUEUED_COUNT : &str = "threadpool_queued_count";
+const THREADPOOL_MAX_COUNT : &str = "threadpool_max_count";
+const THREADPOOL_PANIC_COUNT : &str = "threadpool_panic_count";
+const OPENED_WALLETS_COUNT : &str = "opened_wallets_count";
+const OPENED_WALLET_IDS_COUNT : &str = "opened_wallet_ids_count";
+const  PENDING_FOR_IMPORT_WALLETS_COUNT : &str = "pending_for_import_wallets_count";
+const  PENDING_FOR_OPEN_WALLETS_COUNT : &str = "pending_for_open_wallets_count";
 
 pub enum MetricsCommand {
     CollectMetrics(Box<dyn Fn(IndyResult<String>) + Send>)
 }
 
-pub struct MetricsCommandExecutor {}
+pub struct MetricsCommandExecutor {
+    wallet_service: Rc<WalletService>
+}
 
 impl MetricsCommandExecutor {
-    pub fn new() -> MetricsCommandExecutor {
-        MetricsCommandExecutor {}
+    pub fn new(wallet_service: Rc<WalletService>) -> MetricsCommandExecutor {
+        MetricsCommandExecutor {
+            wallet_service
+        }
     }
 
     pub fn execute(&self, command: MetricsCommand) {
@@ -21,11 +36,22 @@ impl MetricsCommandExecutor {
 
         };
     }
+
+
     fn _collect(&self) -> IndyResult<String> {
 
         trace!("_collect >>>");
 
-        let metrics_map : HashMap<String, f32> = HashMap::new();
+        let metrics_map : HashMap<&str, usize> = [
+            (THREADPOOL_ACTIVE_COUNT, crate::commands::THREADPOOL.lock().unwrap().active_count()),
+            (THREADPOOL_QUEUED_COUNT, crate::commands::THREADPOOL.lock().unwrap().queued_count()),
+            (THREADPOOL_MAX_COUNT, crate::commands::THREADPOOL.lock().unwrap().max_count()),
+            (THREADPOOL_PANIC_COUNT, crate::commands::THREADPOOL.lock().unwrap().panic_count()),
+            (OPENED_WALLETS_COUNT, self.wallet_service.get_wallets_count()),
+            (OPENED_WALLET_IDS_COUNT, self.wallet_service.get_wallet_ids_count()),
+            (PENDING_FOR_IMPORT_WALLETS_COUNT, self.wallet_service.get_pending_for_import_count()),
+            (PENDING_FOR_OPEN_WALLETS_COUNT, self.wallet_service.get_pending_for_open_count())
+        ].iter().cloned().collect();
         let res = serde_json::to_string(&metrics_map).unwrap();
 
         trace!("_collect <<< res: {:?}", res);
