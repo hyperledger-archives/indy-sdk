ad68683d4f1923a5aac7ae215d55f297cd595564
Sergey Minaev <sergey.minaev@evernym.com>
2021-01-28 16:53:50 +0300

    Fix WritableBlob implementation in case of async_std::fs::File.
    Signed-off-by: Sergey Minaev <sergey.minaev@evernym.com>

--- libindy/src/services/blob_storage/default_writer.rs
+++ libindy/src/services/blob_storage/default_writer.rs
@@ -64,8 +64,9 @@ impl WritableBlob for DefaultWriter {
     async fn append(&mut self, bytes: &[u8]) -> IndyResult<usize> {
         trace!("append >>>");
 
-        let res = self.file.write(bytes).await.map_err(map_err_trace!())?;
+        let res = self.file.write_all(bytes).await.map_err(map_err_trace!())?;
 
+        let res = bytes.len();
         trace!("append <<< {}", res);
         Ok(res)
     }
