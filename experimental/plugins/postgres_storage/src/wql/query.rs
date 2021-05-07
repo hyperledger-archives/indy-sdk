954b5f3371d6f71d33b14e8e5d06f23216383081
Andraž Cuderman <andraz.cuderman@global.id>
2021-05-07 13:59:44 +0200

    Add wql to sql tests

--- experimental/plugins/postgres_storage/src/wql/query.rs
+++ experimental/plugins/postgres_storage/src/wql/query.rs
@@ -146,7 +146,7 @@ fn lt_to_sql<'a>(name: &'a TagName, value: &'a TargetValue, arguments: &mut Vec<
             arguments.push(queried_value);
             Ok(format!("tags_plaintext as alias{0} ON alias{0}.item_id = i.id AND alias{0}.wallet_id = $$ AND alias{0}.name = $$ AND alias{0}.value < $$", alias))
         },
-        _ => Err(WalletQueryError::StructureErr("Invalid combination of tag name and value for $lte operator".to_string()))
+        _ => Err(WalletQueryError::StructureErr("Invalid combination of tag name and value for $lt operator".to_string()))
     }
 }
 
@@ -235,12 +235,12 @@ fn and_to_sql<'a>(suboperators: &'a [Operator], arguments: &mut Vec<&'a dyn ToSq
 
 
 fn or_to_sql<'a>(suboperators: &'a [Operator], arguments: &mut Vec<&'a dyn ToSql>, wallet_id: &'a String) -> Result<String, WalletQueryError> {
-    Err(WalletQueryError::StructureErr("OR operation not supported".to_string()))
+    Err(WalletQueryError::StructureErr("$or operation not supported".to_string()))
 }
 
 
 fn not_to_sql<'a>(suboperator: &'a Operator, arguments: &mut Vec<&'a dyn ToSql>, wallet_id: &'a String) -> Result<String, WalletQueryError> {
-    Err(WalletQueryError::StructureErr("NOT operation not supported".to_string()))
+    Err(WalletQueryError::StructureErr("$not operation not supported".to_string()))
 }
 
 
@@ -262,6 +262,21 @@ fn join_operators<'a>(operators: &'a [Operator], join_str: &str, arguments: &mut
 mod tests {
     use super::*;
 
+    fn get_expected_query(table_name: &str, operator: &str) -> String {
+
+        return format!("SELECT i.id, i.name, i.value, i.key, i.type FROM items as i INNER JOIN {} as alias0 ON alias0.item_id = i.id AND alias0.wallet_id = $1 AND alias0.name = $2 AND alias0.value {} $3 WHERE i.type = $4 AND i.wallet_id = $5", table_name, operator)
+    }
+
+    fn setup_tag_search() -> (String, Vec<u8>) {
+        let wallet_id = "walletID".to_string();
+        let class = vec![100,100,100];
+
+        return (wallet_id, class);
+    }
+
+    const TAGS_ENCRYPTED_TABLE_NAME: &str = "tags_encrypted";
+    const TAGS_PLAIN_TABLE_NAME: &str = "tags_plaintext";
+
     #[test]
     fn simple_and_convert_args_works() {
         assert_eq!("This $1 is $2 a $3 string!", convert_query_to_psql_args("This $$ is $$ a $$ string!"));
@@ -269,25 +284,248 @@ mod tests {
     }
 
     #[test]
-    fn simple_and() {
+    fn should_format_valid_and_statement() {
         let condition = Operator::And(vec![
             Operator::Eq(TagName::EncryptedTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6])),
             Operator::Eq(TagName::PlainTagName(vec![7,8,9]), TargetValue::Unencrypted("spam".to_string())),
         ]);
-        let wallet_id = "walletID".to_string();
+        let (wallet_id, class) = setup_tag_search();
 
-        let class = vec![100,100,100];
         let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
-        assert_eq!(query, "SELECT i.id, i.name, i.value, i.key, i.type FROM items as i INNER JOIN tags_encrypted as alias0 ON alias0.item_id = i.id AND alias0.wallet_id = $1 AND alias0.name = $2 AND alias0.value = $3 INNER JOIN tags_plaintext as alias3 ON alias3.item_id = i.id AND alias3.wallet_id = $4 AND alias3.name = $5 AND alias3.value = $6 WHERE i.type = $7 AND i.wallet_id = $8")
+
+        assert_eq!(query, "SELECT i.id, i.name, i.value, i.key, i.type FROM items as i INNER JOIN tags_encrypted as alias0 ON alias0.item_id = i.id AND alias0.wallet_id = $1 AND alias0.name = $2 AND alias0.value = $3 INNER JOIN tags_plaintext as alias3 ON alias3.item_id = i.id AND alias3.wallet_id = $4 AND alias3.name = $5 AND alias3.value = $6 WHERE i.type = $7 AND i.wallet_id = $8");
+        assert_eq!(_arguments.len(), 8);
     }
 
     #[test]
-    fn simple_eq() {
-        let condition = Operator::Eq(TagName::EncryptedTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6]));
-        let wallet_id = "walletID".to_string();
+    fn should_format_valid_count_statement() {
+        let condition = Operator::And(vec![
+            Operator::Eq(TagName::EncryptedTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6])),
+            Operator::Eq(TagName::PlainTagName(vec![7,8,9]), TargetValue::Unencrypted("spam".to_string())),
+        ]);
+        let (wallet_id, class) = setup_tag_search();
 
-        let class = vec![100,100,100];
+        let (query, _arguments) = wql_to_sql_count(&class, &wallet_id, &condition).unwrap();
+
+        assert_eq!(query, "SELECT count(*) FROM items as i INNER JOIN tags_encrypted as alias0 ON alias0.item_id = i.id AND alias0.wallet_id = $1 AND alias0.name = $2 AND alias0.value = $3 INNER JOIN tags_plaintext as alias3 ON alias3.item_id = i.id AND alias3.wallet_id = $4 AND alias3.name = $5 AND alias3.value = $6 WHERE i.type = $7 AND i.wallet_id = $8");
+        assert_eq!(_arguments.len(), 8);
+    }
+
+    #[test]
+    fn should_format_valid_encrypted_tags_eq_statement() {
+        let condition = Operator::Eq(TagName::EncryptedTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6]));
+        let (wallet_id, class) = setup_tag_search();
+
         let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
-        assert_eq!(query, "SELECT i.id, i.name, i.value, i.key, i.type FROM items as i INNER JOIN tags_encrypted as alias0 ON alias0.item_id = i.id AND alias0.wallet_id = $1 AND alias0.name = $2 AND alias0.value = $3 WHERE i.type = $4 AND i.wallet_id = $5")
+
+        assert_eq!(query, get_expected_query(TAGS_ENCRYPTED_TABLE_NAME, "="));
+        assert_eq!(_arguments.len(), 5)
+    }
+
+    #[test]
+    fn should_format_valid_plain_tags_eq_statement() {
+        let condition = Operator::Eq(TagName::PlainTagName(vec![1,2,3]), TargetValue::Unencrypted("unencrypted".to_string()));
+        let (wallet_id, class) = setup_tag_search();
+
+        let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+
+        assert_eq!(query, get_expected_query(TAGS_PLAIN_TABLE_NAME, "="));
+        assert_eq!(_arguments.len(), 5)
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid combination of tag name and value for equality operator")]
+    fn should_panic_when_invalid_eq_arguments_used() {
+        let condition = Operator::Eq(TagName::PlainTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6]));
+        let (wallet_id, class) = setup_tag_search();
+
+        wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+    }
+
+    #[test]
+    fn should_format_valid_encrypted_tags_neq_statement() {
+        let condition = Operator::Neq(TagName::EncryptedTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6]));
+        let (wallet_id, class) = setup_tag_search();
+
+        let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+
+        assert_eq!(query, get_expected_query(TAGS_ENCRYPTED_TABLE_NAME, "!="));
+        assert_eq!(_arguments.len(), 5)
+    }
+
+    #[test]
+    fn should_format_valid_plain_tags_neq_statement() {
+        let condition = Operator::Neq(TagName::PlainTagName(vec![1,2,3]), TargetValue::Unencrypted("unencrypted".to_string()));
+        let (wallet_id, class) = setup_tag_search();
+
+        let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+
+        assert_eq!(query, get_expected_query(TAGS_PLAIN_TABLE_NAME, "!="));
+        assert_eq!(_arguments.len(), 5)
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid combination of tag name and value for inequality operator")]
+    fn should_panic_when_invalid_neq_arguments_used() {
+        let condition = Operator::Neq(TagName::PlainTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6]));
+        let (wallet_id, class) = setup_tag_search();
+
+        wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+    }
+
+    #[test]
+    fn should_format_valid_plain_tags_gt_statement() {
+        let condition = Operator::Gt(TagName::PlainTagName(vec![1,2,3]), TargetValue::Unencrypted("unencrypted".to_string()));
+        let (wallet_id, class) = setup_tag_search();
+
+        let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+
+        assert_eq!(query, get_expected_query(TAGS_PLAIN_TABLE_NAME, ">"));
+        assert_eq!(_arguments.len(), 5)
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid combination of tag name and value for $gt operator")]
+    fn should_panic_when_invalid_gt_arguments_used() {
+        let condition = Operator::Gt(TagName::EncryptedTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6]));
+        let (wallet_id, class) = setup_tag_search();
+
+        wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+    }
+
+    #[test]
+    fn should_format_valid_plain_tags_gte_statement() {
+        let condition = Operator::Gte(TagName::PlainTagName(vec![1,2,3]), TargetValue::Unencrypted("unencrypted".to_string()));
+        let (wallet_id, class) = setup_tag_search();
+
+        let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+
+        assert_eq!(query, get_expected_query(TAGS_PLAIN_TABLE_NAME, ">="));
+        assert_eq!(_arguments.len(), 5)
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid combination of tag name and value for $gte operator")]
+    fn should_panic_when_invalid_gte_statement() {
+        let condition = Operator::Gte(TagName::EncryptedTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6]));
+        let (wallet_id, class) = setup_tag_search();
+
+        wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+    }
+
+    #[test]
+    fn should_format_valid_plain_tags_lt_statement() {
+        let condition = Operator::Lt(TagName::PlainTagName(vec![1,2,3]), TargetValue::Unencrypted("unencrypted".to_string()));
+        let (wallet_id, class) = setup_tag_search();
+
+        let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+
+        assert_eq!(query, get_expected_query(TAGS_PLAIN_TABLE_NAME, "<"));
+        assert_eq!(_arguments.len(), 5)
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid combination of tag name and value for $lt operator")]
+    fn should_panic_when_invalid_lt_arguments_used() {
+        let condition = Operator::Lt(TagName::EncryptedTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6]));
+        let (wallet_id, class) = setup_tag_search();
+
+        wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+    }
+
+    #[test]
+    fn should_format_valid_plain_tags_lte_statement() {
+        let condition = Operator::Lte(TagName::PlainTagName(vec![1,2,3]), TargetValue::Unencrypted("unencrypted".to_string()));
+        let (wallet_id, class) = setup_tag_search();
+
+        let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+
+        assert_eq!(query, get_expected_query(TAGS_PLAIN_TABLE_NAME, "<="));
+        assert_eq!(_arguments.len(), 5)
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid combination of tag name and value for $lte operator")]
+    fn should_panic_when_invalid_lte_arguments_used() {
+        let condition = Operator::Lte(TagName::EncryptedTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6]));
+        let (wallet_id, class) = setup_tag_search();
+
+        wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+    }
+
+    #[test]
+    fn should_format_valid_plain_tags_like_statement() {
+        let condition = Operator::Like(TagName::PlainTagName(vec![1,2,3]), TargetValue::Unencrypted("unencrypted".to_string()));
+        let (wallet_id, class) = setup_tag_search();
+
+        let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+
+        assert_eq!(query, get_expected_query(TAGS_PLAIN_TABLE_NAME, "LIKE"));
+        assert_eq!(_arguments.len(), 5)
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid combination of tag name and value for $like operator")]
+    fn should_panic_when_invalid_like_arguments_used() {
+        let condition = Operator::Like(TagName::EncryptedTagName(vec![1,2,3]), TargetValue::Encrypted(vec![4,5,6]));
+        let (wallet_id, class) = setup_tag_search();
+
+        wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+    }
+
+    #[test]
+    fn should_format_valid_encrypted_tags_in_statement() {
+        let arguments: Vec<TargetValue> = vec![
+            TargetValue::Encrypted(vec![4,5,6]),
+            TargetValue::Encrypted(vec![1,2,3]),
+        ];
+        let condition = Operator::In(TagName::EncryptedTagName(vec![1,2,3]), arguments);
+        let (wallet_id, class) = setup_tag_search();
+
+        let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+
+        assert_eq!(query, "SELECT i.id, i.name, i.value, i.key, i.type FROM items as i INNER JOIN tags_encrypted as alias0 ON alias0.item_id = i.id AND alias0.wallet_id = $1 AND alias0.name = $2 AND alias0.value IN ($3,$4) WHERE i.type = $5 AND i.wallet_id = $6");
+        assert_eq!(_arguments.len(), 6)
+    }
+
+    #[test]
+    fn should_format_valid_plain_tags_in_statement() {
+        let arguments: Vec<TargetValue> = vec![
+            TargetValue::Unencrypted("un".to_string()),
+            TargetValue::Unencrypted("un".to_string()),
+        ];
+        let condition = Operator::In(TagName::PlainTagName(vec![1,2,3]), arguments);
+        let (wallet_id, class) = setup_tag_search();
+
+        let (query, _arguments) = wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+
+        assert_eq!(query, "SELECT i.id, i.name, i.value, i.key, i.type FROM items as i INNER JOIN tags_plaintext as alias0 ON alias0.item_id = i.id AND alias0.wallet_id = $1 AND alias0.name = $2 AND alias0.value IN ($3,$4) WHERE i.type = $5 AND i.wallet_id = $6");
+        assert_eq!(_arguments.len(), 6)
+    }
+
+    #[test]
+    #[should_panic(expected = "Unencrypted tag value in $in for encrypted tag name")]
+    fn should_panic_when_invalid_encrypted_tags_in_arguments_used() {
+        let arguments: Vec<TargetValue> = vec![
+            TargetValue::Unencrypted("un".to_string()),
+            TargetValue::Encrypted(vec![4,5,6]),
+        ];
+        let condition = Operator::In(TagName::EncryptedTagName(vec![1,2,3]), arguments);
+        let (wallet_id, class) = setup_tag_search();
+
+        wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Encrypted tag value in $in for nonencrypted tag name")]
+    fn should_panic_when_invalid_plain_tags_in_arguments_used() {
+        let arguments: Vec<TargetValue> = vec![
+            TargetValue::Encrypted(vec![4,5,6]),
+            TargetValue::Unencrypted("un".to_string()),
+        ];
+        let condition = Operator::In(TagName::PlainTagName(vec![1,2,3]), arguments);
+        let (wallet_id, class) = setup_tag_search();
+
+        wql_to_sql(&class, &wallet_id, &condition, None).unwrap();
     }
 }
