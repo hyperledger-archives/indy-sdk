d84405022b2ae50fac5aae9e881fc987aee2d053
Vitalij Reicherdt <vitalij.reicherdt@commerzbank.com>
2022-06-22 15:41:31 +0000

    support socks_proxy config for indy_cli
    Signed-off-by: Vitalij Reicherdt <vitalij.reicherdt@commerzbank.com>

--- cli/src/commands/pool.rs
+++ cli/src/commands/pool.rs
@@ -81,11 +81,13 @@ pub mod connect_command {
                 .add_optional_param("extended-timeout", "Extended timeout for network request (in sec)")
                 .add_optional_param("pre-ordered-nodes", "Names of nodes which will have a priority during request sending")
                 .add_optional_param("number-read-nodes", "The number of nodes to send read requests (2 by default)")
+                .add_optional_param("socks_proxy", "ZMQ socks proxy host name and port")
                 .add_example("pool connect pool1")
                 .add_example("pool connect pool1 protocol-version=2")
                 .add_example("pool connect pool1 protocol-version=2 timeout=100")
                 .add_example("pool connect pool1 protocol-version=2 extended-timeout=100")
                 .add_example("pool connect pool1 protocol-version=2 pre-ordered-nodes=Node2,Node1")
+                .add_example("pool connect pool1 socks_proxy=proxy1.intranet.company.com:1080")
                 .finalize());
 
     fn execute(ctx: &CommandContext, params: &CommandParams) -> Result<(), ()> {
@@ -97,6 +99,7 @@ pub mod connect_command {
         let extended_timeout = get_opt_number_param::<i64>("extended-timeout", params).map_err(error_err!())?;
         let pre_ordered_nodes = get_opt_str_array_param("pre-ordered-nodes", params).map_err(error_err!())?;
         let number_read_nodes = get_opt_number_param::<u8>("number-read-nodes", params).map_err(error_err!())?;
+        let socks_proxy = get_opt_str_param("socks_proxy", params).map_err(error_err!())?;
 
         let config = {
             let mut json = JSONMap::new();
@@ -104,6 +107,7 @@ pub mod connect_command {
             update_json_map_opt_key!(json, "extended_timeout", extended_timeout);
             update_json_map_opt_key!(json, "preordered_nodes", pre_ordered_nodes);
             update_json_map_opt_key!(json, "number_read_nodes", number_read_nodes);
+            update_json_map_opt_key!(json, "socks_proxy", socks_proxy);
             JSONValue::from(json).to_string()
         };
 
